a57615b3d08d266a7b7db3d92ed5012e94aea8b6
Anthony Woods <awoods@raintank.io>
2015-02-23 13:55:30 +0000

    Update eventDefinition to support dynamic attributes
    Adds an 'Extra' field and json Marshaler,Unmarshaler functions
    to allow an event to contain any number of properties.  If the property
    is not one of the required properties, it is stored in Event.Extra[] when
    Unmarshaled from JSON. when Marshaling back to json, the Extra properties
    are returned to be top level properties.

--- eventdef/eventdef.go
+++ eventdef/eventdef.go
@@ -1,8 +1,10 @@
 package eventdef
 
 import (
+	"reflect"
 	"encoding/json"
 	"fmt"
+	"errors"
 	"github.com/codeskyblue/go-uuid"
 	elastigo "github.com/mattbaird/elastigo/lib"
 	"log"
@@ -11,13 +13,123 @@ import (
 )
 
 type EventDefinition struct {
-	ID        string `json:"id"`
-	EventType string `json:"event_type"`
-	AccountID int    `json:"account_id"`
-	Severity  string `json:"severity"` // enum "INFO" "WARN" "ERROR" "OK"
-	Source    string `json:"source"`
-	Timestamp int64  `json:"timestamp"`
-	Message   string `json:"message"`
+	Id        string                 `json:"id"`
+	EventType string                 `json:"event_type"`
+	AccountId int64                  `json:"account_id"`
+	Severity  string                 `json:"severity"` // enum "INFO" "WARN" "ERROR" "OK"
+	Source    string                 `json:"source"`
+	Timestamp int64                  `json:"timestamp"`
+	Message   string                 `json:"message"`
+	Extra     map[string]interface{} `json:"-"`
+}
+
+type RequiredField struct {
+	StructName string
+	Seen       bool
+}
+
+func (e *EventDefinition) UnmarshalJSON(raw []byte) error {
+	//lets start by unmashaling into a basic map datastructure
+	event := make(map[string]interface{})
+	err := json.Unmarshal(raw, &event)
+	if err != nil {
+			return err
+	}
+
+	//lets get a list of our required fields.
+	s := reflect.TypeOf(*e)
+	requiredFields := make(map[string]*RequiredField)
+
+	for i := 0; i < s.NumField(); i++ {
+		field := s.Field(i)
+		name := field.Name
+		// look at the field Tags to work out the property named used in the
+		// JSON document.
+		tag := field.Tag.Get("json")
+		if tag != "" && tag != "-" {
+			name = tag
+		}
+		//all fields except 'Extra' are required.
+		if (name != "Extra") {
+			requiredFields[name] = &RequiredField{
+				StructName: field.Name,
+				Seen: false,
+			}
+		}
+	}
+
+	e.Extra = make(map[string]interface{})
+	for k,v := range event {
+		def, ok := requiredFields[k]
+		// anything that is not a required field gets
+		// stored in our 'Extra' field.
+		if !ok {
+			e.Extra[k] = v
+		} else {
+			//coerce any float64 values to int64
+			if reflect.ValueOf(v).Type().Name() == "float64" {
+				v = int64(v.(float64))
+			}
+			value := reflect.ValueOf(v)
+			reflect.ValueOf(e).Elem().FieldByName(def.StructName).Set(value)
+			def.Seen = true
+		}
+	}
+
+	//make sure all required fields were present.
+	for _,v := range requiredFields {
+		if !v.Seen {
+			return errors.New("Required field missing")
+		}
+	}
+	return nil
+}
+
+func (e *EventDefinition) MarshalJSON() ([]byte, error) {
+	//convert our Event object to a map[string]
+	event := make(map[string]interface{})
+
+	value := reflect.ValueOf(*e)
+	for i := 0; i < value.Type().NumField(); i++ {
+		field := value.Type().Field(i)
+		name := field.Name
+		tag := field.Tag.Get("json")
+		if tag != "" && tag != "-" {
+			name = tag
+		}
+		if (name == "Extra") {
+			//anything that was in Extra[] becomes a toplevel property again.
+			for k,v := range e.Extra {
+				event[k] = v
+			}
+		} else {
+			v, err := encode(value.FieldByName(field.Name))
+			if err != nil {
+				return nil, err
+			}
+			event[name] = v
+		}
+	}
+	//Marshal our map[string] into a JSON string (byte[]).
+	raw, err := json.Marshal(&event)
+	if err != nil {
+			return nil, err
+	}
+	return raw, nil
+}
+
+// convert reflect.Value object to interface{}
+func encode(v reflect.Value) (interface{}, error) {
+	switch v.Type().Kind() {
+	case reflect.Bool:
+		return v.Bool(), nil
+	case reflect.String:
+		return v.String(), nil
+	case reflect.Int64:
+		return v.Int(), nil
+	default:
+		return nil, errors.New("Unsupported type")
+	}
 }
 
 var es *elastigo.Conn
@@ -32,9 +144,9 @@ func init() {
 }
 
 func (e *EventDefinition) Save() error {
-	if e.ID == "" {
+	if e.Id == "" {
 		u := uuid.NewRandom()
-		e.ID = u.String()
+		e.Id = u.String()
 	}
 	if e.Timestamp == 0 {
 		// looks like this expects timestamps in milliseconds
@@ -43,7 +155,7 @@ func (e *EventDefinition) Save() error {
 	if err := e.validate(); err != nil {
 		return err
 	}
-	resp, err := es.Index("events", e.EventType, e.ID, nil, e)
+	resp, err := es.Index("events", e.EventType, e.Id, nil, e)
 	log.Printf("response ok? %v", resp.Ok)
 	if err != nil {
 		return err
@@ -53,7 +165,7 @@ func (e *EventDefinition) Save() error {
 }
 
 func (e *EventDefinition) validate() error {
-	if e.EventType == "" || e.AccountID == 0 || e.Source == "" || e.Timestamp == 0 || e.Message == "" {
+	if e.EventType == "" || e.AccountId == 0 || e.Source == "" || e.Timestamp == 0 || e.Message == "" {
 		err := fmt.Errorf("event definition not valid")
 		return err
 	}
