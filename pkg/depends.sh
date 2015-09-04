b8146b3768ed113c7b9b96d7c71757e8f7173c87
ctdk <jbingham@gmail.com>
2015-09-03 17:06:13 -0700

    more dependencies

--- pkg/depends.sh
+++ pkg/depends.sh
@@ -19,19 +19,26 @@ bundle install
 
 mkdir -p ${GOPATH}/src/github.com/grafana/grafana
 mv ${GOPATH}/src/github.com/grafana/grafana ${GOPATH}/src/github.com/grafana/grafana-bak
-mkdir -p ${GOPATH}/src/github.com/raintank
-cd ${GOPATH}/src/github.com/raintank
-git clone https://github.com/raintank/grafana.git
-echo "raintank"
-ls ${GOPATH}/src/github.com/raintank
-echo "grafana"
-ls ${GOPATH}/src/github.com/grafana
+#mkdir -p ${GOPATH}/src/github.com/raintank
+#cd ${GOPATH}/src/github.com/raintank
+#git clone https://github.com/raintank/grafana.git
+#echo "raintank"
+#ls ${GOPATH}/src/github.com/raintank
+#echo "grafana"
+#ls ${GOPATH}/src/github.com/grafana
 ln -s ${GOPATH}/src/github.com/raintank/grafana ${GOPATH}/src/github.com/grafana/grafana
 # Only until this is done being in a different branch
 cd ${GOPATH}/src/github.com/raintank/grafana
 go run build.go setup
 godep restore
 
+# it, erm, seems to not be finding all of the dependencies right now
 go get github.com/nsqio/go-nsq
 go get github.com/tinylib/msgp/msgp
 go get github.com/ctdk/goas/v2/logger
+go get github.com/jessevdk/go-flags
+go get github.com/BurntSushi/toml
+go get github.com/mattbaird/elastigo/lib
+go get gopkg.in/redis.v2
+go get github.com/bitly/go-hostpool
+go get github.com/marpaia/graphite-golang
