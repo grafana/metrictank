2681632eed216570e336fe8957d9adb45385832e
ctdk <jbingham@gmail.com>
2015-09-03 17:26:54 -0700

    There's not a raintank-metric package anymore, of course

--- pkg/package.sh
+++ pkg/package.sh
@@ -5,7 +5,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 NAME=raintank-metric
-VERSION="$(../${NAME} -v | cut -f3 -d' ')"
+VERSION="0.1.0" # need an automatic way to do this again :-/
 BUILD="${DIR}/${NAME}-${VERSION}"
 ARCH="$(uname -m)"
 PACKAGE_NAME="${DIR}/artifacts/${NAME}-VERSION-ITERATION_ARCH.deb"
@@ -18,13 +18,13 @@ git tag $TAG
 mkdir -p ${BUILD}/usr/bin
 mkdir -p ${BUILD}/etc/init
 mkdir -p ${BUILD}/etc/raintank
-cp ${DIR}/config/ubuntu/trusty/etc/init/${NAME}.conf ${BUILD}/etc/init
-cp ${DIR}/config/ubuntu/trusty/etc/raintank/* ${BUILD}/etc/raintank
+#cp ${DIR}/config/ubuntu/trusty/etc/init/${NAME}.conf ${BUILD}/etc/init
+#cp ${DIR}/config/ubuntu/trusty/etc/raintank/* ${BUILD}/etc/raintank
 
-cp ../${NAME} ${BUILD}/usr/bin/
-cp -r ${DIR}/config/ubuntu/trusty/* ${BUILD}/
+#cp ../${NAME} ${BUILD}/usr/bin/
+#cp -r ${DIR}/config/ubuntu/trusty/* ${BUILD}/
 
-fpm -s dir -t deb \
+#fpm -s dir -t deb \
   -v ${VERSION} -n ${NAME} -a ${ARCH} --iteration $ITERATION --description "Raintank Metric" \
   --deb-upstart ${DIR}/config/ubuntu/trusty/etc/init/raintank-metric.conf \
   -C ${BUILD} -p ${PACKAGE_NAME} .
