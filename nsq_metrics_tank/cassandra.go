2c96714dfbbe3e3e190bda00076a8a0217173911
woodsaj <anthony@monkey.id.au>
2015-09-30 13:30:19 +0300

    add support for querying cassandra

--- nsq_metrics_tank/cassandra.go
+++ nsq_metrics_tank/cassandra.go
@@ -35,3 +35,83 @@ func InsertMetric(key string, ts uint32, data []byte) error {
 	row_key := fmt.Sprintf("%s_%d", key, (ts / 3600 / 24 / 28)) // "month number" based on unix timestamp (rounded down)
 	return cSession.Query(query, row_key, ts, data).Exec()
 }
+
+// Basic search of cassandra.
+// TODO:(Awoods) this needs to be refactored to run each query in a separate goroutine.
+// then concatenate the results at the end.
+func SearchCassandra(key string, start, end time.Time) ([]Point, error) {
+	start_epoch := uint32(start.Unix())
+	end_epoch := uint32(end.Unix())
+	points := make([]Point, 0)
+	//get row_keys we need to query.
+	row_ts, err := MonthsInRange(start, end)
+	if err != nil {
+		return points, err
+	}
+	// we need to send a query for each row holding data.
+	// this is 1 row per calender month.
+	num_rows := len(row_ts)
+
+	for i, ts := range row_ts {
+		row_key := fmt.Sprintf("%s_%s", key, ts)
+		var query string
+		params := make([]interface{}, 0)
+
+		// all queries need the row_key
+		params = append(params, row_key)
+		if num_rows == 1 {
+			// we need a selection of the row between startTs and endTs
+			query = "SELECT data FROM metric WHERE key = ? AND ts < ? AND ts >= ?"
+			params = append(params, end, start)
+		} else if i < 1 {
+			// we want from startTs to the end of the row.
+			query = "SELECT data FROM metric WHERE key = ? AND ts >= ?"
+			params = append(params, start)
+		} else {
+			// we want from start of the row till the endTs.
+			query = "SELECT data FROM metric WHERE key = ? AND ts < ?"
+			params = append(params, end)
+		}
+		var b []byte
+		result := cSession.Query(query, params...).Iter()
+		for result.Scan(&b) {
+			iter, err := tsz.NewIterator(b)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for iter.Next() {
+				ts, val := iter.Values()
+				if ts > start_epoch && ts <= end_epoch {
+					points = append(points, Point{val, ts})
+				}
+			}
+		}
+	}
+	return points, nil
+}
+
+func MonthsInRange(start, end time.Time) ([]string, error) {
+	if start.After(end) {
+		return nil, fmt.Errorf("start time after end time.")
+	}
+	months := make([]string, 0)
+
+	year := start.Year()
+	month := int(start.Month())
+
+	if year != end.Year() {
+		for month <= 12 {
+			months = append(months, fmt.Sprintf("%d%02d", year, month))
+			month++
+		}
+		//start new year.
+		month = 1
+		year++
+	}
+
+	for month <= int(end.Month()) {
+		months = append(months, fmt.Sprintf("%d%02d", year, month))
+		month++
+	}
+	return months, nil
+}
