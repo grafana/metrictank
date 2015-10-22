15829d8b3fbf7f5c39a7020c86a533b77b888044
Dieter Plaetinck <dieter@raintank.io>
2015-10-22 17:21:17 -0400

    deprecated in favor of graphite-watcher

--- nsq_metrics_tank/test/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget 'http://localhost:6063/get?render=litmus.fake_org_79_endpoint_2.dev1.http.ok_state' -q -O - | python -mjson.tool | grep -c Val
-echo 'GET http://localhost:6063/get?render=litmus.fake_org_79_endpoint_2.dev1.http.ok_state' | vegeta attack -rate 1000 -duration=10s | vegeta report
