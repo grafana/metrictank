5d968ee8f96b85b9611c714993fae8b4879e939d
ctdk <jbingham@gmail.com>
2015-11-11 13:40:58 -0800

    build nsq_metric_tank

--- pkg/build.sh
+++ pkg/build.sh
@@ -7,7 +7,7 @@ cd ${DIR}
 : ${GOPATH:="${HOME}/.go_workspace"}
 export PATH=$GOPATH/bin:$PATH
 
-for VAR in nsq_metrics_to_elasticsearch	nsq_metrics_to_kairos nsq_probe_events_to_elasticsearch; do
+for VAR in nsq_metrics_to_elasticsearch	nsq_metrics_to_kairos nsq_probe_events_to_elasticsearch nsq_metrics_tank; do
 	go get -u -f github.com/raintank/raintank-metric/$VAR
 	go install github.com/raintank/raintank-metric/$VAR
 	cp $(which $VAR) ${DIR}/artifacts
