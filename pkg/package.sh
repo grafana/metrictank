978a49fc29d692294fab77906b8356ff90011a89
woodsaj <anthony@monkey.id.au>
2015-11-13 17:23:14 +0800

    update pkg script to only add 1 init script

--- pkg/package.sh
+++ pkg/package.sh
@@ -35,11 +35,11 @@ for VAR in nsq_metrics_to_elasticsearch	nsq_metrics_to_kairos nsq_probe_events_t
 	mkdir -p ${NSQ_BUILD}/usr/sbin
 	mkdir -p ${NSQ_BUILD}/etc/init
 	mkdir -p ${NSQ_BUILD}/etc/raintank
-	cp ${DIR}/config/ubuntu/trusty/etc/init/${VAR}.conf ${NSQ_BUILD}/etc/init
+	#cp ${DIR}/config/ubuntu/trusty/etc/init/${VAR}.conf ${NSQ_BUILD}/etc/init
 	cp ${DIR}/config/ubuntu/trusty/etc/raintank/${VAR}.ini ${NSQ_BUILD}/etc/raintank
 	cp ${DIR}/artifacts/$VAR ${NSQ_BUILD}/usr/sbin
 	fpm -s dir -t deb \
 	  -v ${VERSION} -n ${VAR} -a ${ARCH} --iteration $ITERATION --description "Raintank Metric $VAR worker" \
-	  --deb-upstart ${DIR}/config/ubuntu/trusty/etc/init/${VAR}.conf \
+	  --deb-upstart ${DIR}/config/ubuntu/trusty/etc/init/${VAR} \
 	  -C ${NSQ_BUILD} -p ${NSQ_PACKAGE_NAME} .
 done
