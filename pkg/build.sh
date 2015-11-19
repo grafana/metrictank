2a9e845f63d11ebbfbe238acb91e7d1ae1cdfe30
woodsaj <anthony@monkey.id.au>
2015-11-19 22:58:09 +0800

    more fixes to build scripts

--- pkg/build.sh
+++ pkg/build.sh
@@ -7,6 +7,8 @@ cd ${DIR}
 : ${GOPATH:="${HOME}/.go_workspace"}
 export PATH=$GOPATH/bin:$PATH
 
+go get ./...
+
 for VAR in nsq_metrics_to_elasticsearch	nsq_metrics_to_kairos nsq_probe_events_to_elasticsearch metric_tank; do
 	#go get -u -f github.com/raintank/raintank-metric/$VAR
 	go install github.com/raintank/raintank-metric/$VAR
