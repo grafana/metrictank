c7c855a7f7fdb1f9b7caee00adcb56e1a7a9310d
Dieter Plaetinck <dieter@raintank.io>
2015-11-20 19:54:20 +1300

    submit metrics, not sure why i never enabled these

--- metric_tank/handler.go
+++ metric_tank/handler.go
@@ -4,6 +4,7 @@ import (
 	"github.com/grafana/grafana/pkg/log"
 	"github.com/nsqio/go-nsq"
 	"github.com/raintank/raintank-metric/msg"
+	"time"
 )
 
 type Handler struct {
@@ -22,14 +23,14 @@ func (h *Handler) HandleMessage(m *nsq.Message) error {
 		log.Error(0, "skipping message. %s", err)
 		return nil
 	}
-	//	msgsAge.Value(time.Now().Sub(ms.Produced).Nanoseconds() / 1000)
+	msgsAge.Value(time.Now().Sub(ms.Produced).Nanoseconds() / 1000)
 
 	err = ms.DecodeMetricData()
 	if err != nil {
 		log.Error(0, "skipping message. %s", err)
 		return nil
 	}
-	//	metricsPerMessage.Value(int64(len(ms.Metrics)))
+	metricsPerMessage.Value(int64(len(ms.Metrics)))
 
 	metricsReceived.Inc(int64(len(ms.Metrics)))
 	for _, metric := range ms.Metrics {
