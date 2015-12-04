05696ad3b67b97a1f71d502f76d2225af74b2a82
Dieter Plaetinck <dieter@raintank.io>
2015-12-04 13:27:47 +1300

    more consolidation tests

--- metric_tank/dataprocessor_test.go
+++ metric_tank/dataprocessor_test.go
@@ -12,7 +12,110 @@ type testCase struct {
 	out    []Point
 }
 
-func TestDataProcessor(t *testing.T) {
+func validate(cases []testCase, t *testing.T) {
+	for i, c := range cases {
+		out := consolidate(c.in, c.num, c.consol)
+		if len(out) != len(c.out) {
+			t.Fatalf("output for testcase %d mismatch: expected: %v, got: %v", i, c.out, out)
+
+		} else {
+			for j, p := range out {
+				if p.Val != c.out[j].Val || p.Ts != c.out[j].Ts {
+					t.Fatalf("output for testcase %d mismatch at point %d: expected: %v, got: %v", i, j, c.out[j], out[j])
+				}
+			}
+		}
+	}
+}
+
+func TestOddConsolidationAlignments(t *testing.T) {
+	cases := []testCase{
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+			consolidation.Avg,
+			1,
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+			consolidation.Avg,
+			3,
+			[]Point{
+				{2, 1449178151},
+				{4, 1449178161},
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+			consolidation.Avg,
+			5,
+			[]Point{
+				{float64(10) / float64(4), 1449178161},
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+			},
+			consolidation.Avg,
+			1,
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+			},
+			consolidation.Avg,
+			2,
+			[]Point{
+				{1.5, 1449178141},
+				{3, 1449178161}, // note: we choose the next ts here for even spacing (important for further processing/parsing/handing off), even though that point is missing
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+			},
+			consolidation.Avg,
+			3,
+			[]Point{
+				{2, 1449178151},
+			},
+		},
+	}
+	validate(cases, t)
+}
+func TestConsolidationFunctions(t *testing.T) {
 	cases := []testCase{
 		{
 			[]Point{
@@ -28,20 +131,78 @@ func TestDataProcessor(t *testing.T) {
 				{3.5, 1449178161},
 			},
 		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+			consolidation.Cnt,
+			2,
+			[]Point{
+				{2, 1449178141},
+				{2, 1449178161},
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+			consolidation.Last,
+			2,
+			[]Point{
+				{2, 1449178141},
+				{4, 1449178161},
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+			consolidation.Min,
+			2,
+			[]Point{
+				{1, 1449178141},
+				{3, 1449178161},
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+			consolidation.Max,
+			2,
+			[]Point{
+				{2, 1449178141},
+				{4, 1449178161},
+			},
+		},
+		{
+			[]Point{
+				{1, 1449178131},
+				{2, 1449178141},
+				{3, 1449178151},
+				{4, 1449178161},
+			},
+			consolidation.Sum,
+			2,
+			[]Point{
+				{3, 1449178141},
+				{7, 1449178161},
+			},
+		},
 	}
-	for i, c := range cases {
-		out := consolidate(c.in, c.num, c.consol)
-		if len(out) != len(c.out) {
-			t.Fatalf("output for testcase %d mismatch: expected: %v, got: %v", i, c.out, out)
-
-		} else {
-			for j, p := range out {
-				if p.Val != c.out[j].Val || p.Ts != c.out[j].Ts {
-					t.Fatalf("output for testcase %d mismatch at point %d: expected: %v, got: %v", i, j, c.out[j], out[j])
-				}
-			}
-		}
-	}
+	validate(cases, t)
 }
 
 type c struct {
