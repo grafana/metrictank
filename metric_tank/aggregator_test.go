2582a2fd0c68ac04b40323889447a8ee8ee8f76b
Dieter Plaetinck <dieter@raintank.io>
2015-12-04 14:44:45 +1300

    tests for rolling aggregators

--- metric_tank/aggregator_test.go
+++ metric_tank/aggregator_test.go
@@ -32,3 +32,52 @@ func TestAggBoundary(t *testing.T) {
 		}
 	}
 }
+
+// note that values don't get "committed" to the metric until the aggregation interval is complete
+func TestAggregator(t *testing.T) {
+	compare := func(key string, metric Metric, expected []Point) {
+		_, iters := metric.Get(0, 1000)
+		got := make([]Point, 0, len(expected))
+		for _, iter := range iters {
+			for iter.Next() {
+				ts, val := iter.Values()
+				got = append(got, Point{val, ts})
+			}
+		}
+		if len(got) != len(expected) {
+			t.Fatalf("output for testcase %s mismatch: expected: %v points, got: %v", key, len(expected), len(got))
+
+		} else {
+			for i, g := range got {
+				exp := expected[i]
+				if exp.Val != g.Val || exp.Ts != g.Ts {
+					t.Fatalf("output for testcase %s mismatch at point %d: expected: %v, got: %v", key, i, exp, g)
+				}
+			}
+		}
+	}
+	agg := NewAggregator("test", 60, 120, 10)
+	agg.Add(100, 123.4)
+	agg.Add(110, 5)
+	expected := []Point{}
+	compare("simple-min-unfinished", agg.minMetric, expected)
+
+	agg = NewAggregator("test", 60, 120, 10)
+	agg.Add(100, 123.4)
+	agg.Add(110, 5)
+	agg.Add(130, 130)
+	expected = []Point{
+		{5, 120},
+	}
+	compare("simple-min-one-block", agg.minMetric, expected)
+
+	agg = NewAggregator("test", 60, 120, 10)
+	agg.Add(100, 123.4)
+	agg.Add(110, 5)
+	agg.Add(120, 4)
+	expected = []Point{
+		{4, 120},
+	}
+	compare("simple-min-one-block-done-cause-last-point-just-right", agg.minMetric, expected)
+
+}
