a7675555cae40567a0b6cc8d2da3998fd4b2cf6e
woodsaj <anthony@monkey.id.au>
2016-01-14 16:13:07 +0800

    cleanup build scripts
    github/raintank/grafana is no longer a dependency so build scripts
    can be simplified.
    Also deleting legacy NSQ tools.

--- pkg/build.sh
+++ pkg/build.sh
@@ -7,13 +7,7 @@ cd ${DIR}
 : ${GOPATH:="${HOME}/.go_workspace"}
 export PATH=$GOPATH/bin:$PATH
 
-go get github.com/bmizerany/assert
-
 for VAR in nsq_probe_events_to_elasticsearch metric_tank; do
-	cd ${DIR}/../${VAR}
-	go get ./...
-	cd $DIR
-	#go get -u -f github.com/raintank/raintank-metric/$VAR
 	go install github.com/raintank/raintank-metric/$VAR
 	cp $(which $VAR) ${DIR}/artifacts
 done
--- pkg/depends.sh
+++ pkg/depends.sh
@@ -17,18 +17,14 @@ export PATH=$GOPATH/bin:$PATH
 mkdir -p artifacts
 bundle install
 
-mkdir -p ${GOPATH}/src/github.com/grafana/grafana
-mv ${GOPATH}/src/github.com/grafana/grafana ${GOPATH}/src/github.com/grafana/grafana-bak
-mkdir -p ${GOPATH}/src/github.com/raintank
-cd ${GOPATH}/src/github.com/raintank
-git clone https://github.com/raintank/grafana.git
-#echo "raintank"
-#ls ${GOPATH}/src/github.com/raintank
-#echo "grafana"
-#ls ${GOPATH}/src/github.com/grafana
-ln -s ${GOPATH}/src/github.com/raintank/grafana ${GOPATH}/src/github.com/grafana/grafana
-# Only until this is done being in a different branch
-#cd ${GOPATH}/src/github.com/raintank/grafana
 
-# link our code to our gopath.
-ln -s $CHECKOUT $GOPATH/src/github.com/raintank/raintank-metric
+# link our checked out code to our gopath.
+rm -rf $GOPATH/src/github.com/raintank/raintank-metric
+ln -s $(dirname $(readlink -e $CHECKOUT)) $GOPATH/src/github.com/raintank/raintank-metric
+
+
+for VAR in nsq_probe_events_to_elasticsearch metric_tank; do
+	$GOPATH/src/github.com/raintank/raintank-metric
+	go get -t -d ./...
+	cd ${DIR}
+done
