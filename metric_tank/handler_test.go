f1a5ec171c803e03b22c96b9adf5f6e6b3f34419
Dieter Plaetinck <dieter@raintank.io>
2016-02-08 13:07:41 +1300

    benchmark for message handler
    this comes with a mock ES implementation
    
    FTR, on my laptop, the current results are:
    
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  17594531 ns/op	 4818906 B/op	  145010 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	1.784s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  17851420 ns/op	 4818861 B/op	  145010 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	1.809s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  21504557 ns/op	 4818911 B/op	  145011 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	2.180s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  21263982 ns/op	 4818911 B/op	  145011 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	2.153s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  21514345 ns/op	 4818877 B/op	  145010 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	2.178s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	      50	  21286933 ns/op	 4819025 B/op	  145011 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	1.095s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  19810310 ns/op	 4818900 B/op	  145010 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	2.008s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  19837526 ns/op	 4818929 B/op	  145011 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	2.013s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  21493792 ns/op	 4818909 B/op	  145011 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	2.178s
    ~/g/s/g/r/r/metric_tank ❯❯❯ go test -run=XX -bench=HandleM -benchmem
    PASS
    BenchmarkHandler_HandleMessage1000-8	     100	  21292224 ns/op	 4818891 B/op	  145011 allocs/op
    ok  	github.com/raintank/raintank-metric/metric_tank	2.158s

---
+++ metric_tank/handler_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/nsqio/go-nsq"
+	"github.com/raintank/met/helper"
+	"github.com/raintank/raintank-metric/metricdef"
+	"github.com/raintank/raintank-metric/msg"
+	"github.com/raintank/raintank-metric/schema"
+	"testing"
+)
+
+func BenchmarkHandler_HandleMessage1000(b *testing.B) {
+	stats, _ := helper.New(false, "", "standard", "metrics_tank", "")
+	clusterStatus = NewClusterStatus("default", false)
+	initMetrics(stats)
+
+	store := NewDevnullStore()
+	aggmetrics := NewAggMetrics(store, 600, 10, 800, 8000, 10000, make([]aggSetting, 0))
+	defCache := NewDefCache(metricdef.NewDefsMock())
+	handler := NewHandler(aggmetrics, defCache)
+
+	metric := &schema.MetricData{
+		Id:         "some.id.of.a.metric",
+		OrgId:      500,
+		Name:       "some.id",
+		Metric:     "metric",
+		Interval:   60,
+		Value:      1234.567,
+		Unit:       "ms",
+		Time:       1234567890,
+		TargetType: "gauge",
+		Tags:       []string{"some_tag", "ok"},
+	}
+
+	metrics := make([]*schema.MetricData, 0)
+	for i := 0; i < 10; i++ {
+		metrics = append(metrics, metric)
+	}
+	id := nsq.MessageID{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 's', 'd', 'f', 'g', 'h'}
+	data, err := msg.CreateMsg(metrics, 1, msg.FormatMetricDataArrayMsgp)
+	if err != nil {
+		panic(err)
+	}
+	nsqmsg := nsq.NewMessage(id, data)
+
+	b.StartTimer()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < 1000; i++ {
+			err := handler.HandleMessage(nsqmsg)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
