6fec95f8d6f634292f279cc93c7e44987aa9e0ad
Dieter Plaetinck <dieter@raintank.io>
2016-02-13 11:21:20 +1100

    add benchmark for CreateMessage
    ~/g/s/g/r/r/msg ❯❯❯ go test -run=XX -bench=. -benchmem -memprofile=mem.prof -v
    testing: warning: no tests to run
    PASS
    Benchmark_CreateMessage-8
    messages:      1, system bytes needed:    3639544
    messages:    100, system bytes needed:    4720888
    messages:  10000, system bytes needed:   27711736
    messages: 300000, system bytes needed:  741918128
      300000	      3736 ns/op	    3856 B/op	       8 allocs/op
    ok  	github.com/raintank/raintank-metric/msg	1.231s
    ~/g/s/g/r/r/msg ❯❯❯ go test -run=XX -bench=. -benchmem -memprofile=mem.prof -v
    testing: warning: no tests to run
    PASS
    Benchmark_CreateMessage-8
    messages:      1, system bytes needed:    4983032
    messages:    100, system bytes needed:    4983032
    messages:  10000, system bytes needed:   27711736
    messages: 300000, system bytes needed:  741361000
      300000	      3729 ns/op	    3856 B/op	       8 allocs/op
    ok  	github.com/raintank/raintank-metric/msg	1.227s

---
+++ msg/msg_test.go
@@ -0,0 +1,53 @@
+package msg
+
+import (
+	"fmt"
+	"github.com/nsqio/go-nsq"
+	"github.com/raintank/raintank-metric/schema"
+	"runtime"
+	"testing"
+)
+
+func Benchmark_CreateMessage(b *testing.B) {
+	metrics := make([]*schema.MetricData, 10)
+	for i := 0; i < len(metrics); i++ {
+		metrics[i] = &schema.MetricData{
+			Id:         "some.id.of.a.metric",
+			OrgId:      500,
+			Name:       "some.id",
+			Metric:     "metric",
+			Interval:   60,
+			Value:      1234.567,
+			Unit:       "ms",
+			Time:       int64(i - len(metrics) + 1),
+			TargetType: "gauge",
+			Tags:       []string{"some_tag", "ok"},
+		}
+	}
+	// timestamps start at 1 and go up from there. (we can't use 0, see AggMetric.Add())
+	msgs := make([]*nsq.Message, b.N)
+	b.ResetTimer()
+	var id nsq.MessageID
+	for i := 0; i < b.N; i++ {
+		id = nsq.MessageID{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 's', 'd', 'f', 'g', 'h'}
+		for j := 0; j < len(metrics); j++ {
+			metrics[j].Time += 10
+		}
+		data, err := CreateMsg(metrics, 1, FormatMetricDataArrayMsgp)
+		if err != nil {
+			panic(err)
+		}
+		msgs[i] = nsq.NewMessage(id, data)
+	}
+	// prevents the go compiler from optimizing msgs away. presumably..
+	if msgs[len(msgs)-1].ID != id {
+		panic("bad id")
+	}
+	b.StopTimer()
+	var stats runtime.MemStats
+	runtime.ReadMemStats(&stats)
+	if b.N == 1 {
+		fmt.Println()
+	}
+	fmt.Printf("messages: %6d, system bytes needed: %10d\n", b.N, stats.Sys)
+}
