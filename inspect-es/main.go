70b2963db4e07f659fffca2397346efe356cdf61
Dieter Plaetinck <dieter@raintank.io>
2016-03-09 11:51:07 +1100

    consistent, disableable maxAge + fix buggy total

--- inspect-es/main.go
+++ inspect-es/main.go
@@ -42,25 +42,33 @@ func perror(err error) {
 var esAddr = flag.String("es-addr", "localhost:9200", "elasticsearch address")
 var esIndex = flag.String("es-index", "metrictank", "elasticsearch index to query")
 var format = flag.String("format", "list", "format: list|vegeta-graphite|vegeta-mt")
-var maxAge = flag.Int("max-age", 3600, "max age (last update diff with now) of metricdefs")
+var maxAge = flag.Int("max-age", 3600, "max age (last update diff with now) of metricdefs. use 0 to disable")
 var from = flag.String("from", "30min", "from. eg '30min', '5h', '14d', etc")
 var silent = flag.Bool("silent", false, "silent mode (don't print number of metrics loaded to stderr)")
 var fromS uint32
+var total int
 
 func showList(ds []*schema.MetricDefinition) {
 	for _, d := range ds {
-		fmt.Println(d.OrgId, d.Name)
+		if *maxAge != 0 && d.LastUpdate > time.Now().Unix()-int64(*maxAge) {
+			total += 1
+			fmt.Println(d.OrgId, d.Name)
+		}
 	}
 }
 func showVegetaGraphite(ds []*schema.MetricDefinition) {
 	for _, d := range ds {
-		fmt.Printf("GET http://localhost:8888/render?target=%s&from=-%s\nX-Org-Id: %d\n", d.Name, *from, d.OrgId)
+		if *maxAge != 0 && d.LastUpdate > time.Now().Unix()-int64(*maxAge) {
+			total += 1
+			fmt.Printf("GET http://localhost:8888/render?target=%s&from=-%s\nX-Org-Id: %d\n", d.Name, *from, d.OrgId)
+		}
 	}
 }
 func showVegetaMT(ds []*schema.MetricDefinition) {
 	from := time.Now().Add(-time.Duration(fromS) * time.Second)
 	for _, d := range ds {
-		if d.LastUpdate > time.Now().Unix()-int64(*maxAge) {
+		if *maxAge != 0 && d.LastUpdate > time.Now().Unix()-int64(*maxAge) {
+			total += 1
 			fmt.Printf("GET http://localhost:18763/get?target=%s&from=%d\n", d.Id, from.Unix())
 		}
 	}
@@ -86,13 +94,11 @@ func main() {
 	perror(err)
 	met, scroll_id, err := defs.GetMetrics("")
 	perror(err)
-	total := len(met)
 	show(met)
 	for scroll_id != "" {
 		met, scroll_id, err = defs.GetMetrics(scroll_id)
 		perror(err)
 		show(met)
-		total += len(met)
 	}
 	if !*silent {
 		fmt.Fprintf(os.Stderr, "listed %d metrics\n", total)
