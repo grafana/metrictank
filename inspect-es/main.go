8462d470c42516feb4cf1b6b0729ba4b6460e7f0
Dieter Plaetinck <dieter@raintank.io>
2016-03-27 23:08:37 -0400

    old es stuff no longer needed

--- inspect-es/main.go
+++ inspect-es/main.go
@@ -11,29 +11,6 @@ import (
 	"time"
 )
 
-type Hit struct {
-	Index  string                  `json:"_index"`
-	Type   string                  `json:"_type"`
-	Id     string                  `json:"_id"`
-	Score  float64                 `json:"_score"`
-	Source schema.MetricDefinition `json:"_source"`
-}
-
-type EsResult struct {
-	Took     int
-	TimedOut bool
-	_shards  struct {
-		total      int
-		successful int
-		failed     int
-	}
-	Hits struct {
-		Total    int
-		MaxScore int
-		Hits     []Hit
-	}
-}
-
 func perror(err error) {
 	if err != nil {
 		log.Fatal(err)
