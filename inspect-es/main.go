aff79aec1fbc92dc3a39e3140e3af0a397d1fd8f
Dieter Plaetinck <dieter@raintank.io>
2016-04-03 23:38:37 -0400

    update inspect-es for new graphite 'native' format for MT

--- inspect-es/main.go
+++ inspect-es/main.go
@@ -7,7 +7,9 @@ import (
 	"github.com/raintank/raintank-metric/metricdef"
 	"github.com/raintank/raintank-metric/schema"
 	"log"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -19,7 +21,7 @@ func perror(err error) {
 
 var esAddr = flag.String("es-addr", "localhost:9200", "elasticsearch address")
 var esIndex = flag.String("es-index", "metric", "elasticsearch index to query")
-var format = flag.String("format", "list", "format: list|vegeta-graphite|vegeta-mt")
+var format = flag.String("format", "list", "format: list|vegeta-graphite|vegeta-mt|vegeta-mt-graphite")
 var maxAge = flag.Int("max-age", 23400, "max age (last update diff with now) of metricdefs. defaults to 6.5hr. use 0 to disable")
 var from = flag.String("from", "30min", "from. eg '30min', '5h', '14d', etc")
 var silent = flag.Bool("silent", false, "silent mode (don't print number of metrics loaded to stderr)")
@@ -42,12 +44,41 @@ func showVegetaGraphite(ds []*schema.MetricDefinition) {
 		}
 	}
 }
+
 func showVegetaMT(ds []*schema.MetricDefinition) {
 	from := time.Now().Add(-time.Duration(fromS) * time.Second)
 	for _, d := range ds {
 		if *maxAge != 0 && d.LastUpdate > time.Now().Unix()-int64(*maxAge) {
 			total += 1
-			fmt.Printf("GET http://localhost:18763/get?target=%s&from=%d\n", d.Id, from.Unix())
+			fmt.Printf("GET http://localhost:6063/get?target=%s&from=%d\n", d.Id, from.Unix())
+		}
+	}
+}
+
+func showVegetaMTGraphite(ds []*schema.MetricDefinition) {
+	for _, d := range ds {
+		if *maxAge != 0 && d.LastUpdate > time.Now().Unix()-int64(*maxAge) {
+			total += 1
+			mode := rand.Intn(3)
+			name := d.Name
+			if mode == 0 {
+				// in this mode, replaces a node with a dot
+				which := rand.Intn(d.NodeCount)
+				parts := strings.Split(d.Name, ".")
+				parts[which] = "*"
+				name = strings.Join(parts, ".")
+			} else if mode == 1 {
+				// randomly replace chars with a *
+				// note that in 1/5 cases, nothing happens
+				// and otherwise, sometimes valid patterns are produced,
+				// but it's also possible to produce patterns that won't match anything (if '.' was taken out)
+				chars := rand.Intn(5)
+				pos := rand.Intn(len(d.Name) - chars)
+				name = name[0:pos] + "*" + name[pos+chars:]
+			}
+			// mode 3: do nothing :)
+
+			fmt.Printf("GET http://localhost:6063/render?target=%s&from=-%s\nX-Org-Id: %d\n\n", name, *from, d.OrgId)
 		}
 	}
 }
@@ -62,6 +93,8 @@ func main() {
 		show = showVegetaGraphite
 	case "vegeta-mt":
 		show = showVegetaMT
+	case "vegeta-mt-graphite":
+		show = showVegetaMTGraphite
 	default:
 		log.Fatal("invalid format")
 	}
