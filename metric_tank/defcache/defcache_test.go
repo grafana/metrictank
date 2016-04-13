57c9ac219a137ffa37c987398e85aaf8359d51b9
Dieter Plaetinck <dieter@raintank.io>
2016-04-13 18:17:40 -0400

    test case to make sure defcache doesn't clobber internal data

---
+++ metric_tank/defcache/defcache_test.go
@@ -0,0 +1,80 @@
+package defcache
+
+import (
+	"github.com/raintank/met/helper"
+	"github.com/raintank/raintank-metric/metricdef"
+	"github.com/raintank/raintank-metric/schema"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	stats, _ := helper.New(false, "", "standard", "metrics_tank", "")
+	defCache := New(metricdef.NewDefsMock(), stats)
+
+	assert := func(def *schema.MetricDefinition, id string, orgId int, name, metric string, interval int, unit, tt, tag1, tag2 string) {
+		if def.Id != id {
+			t.Fatalf("bad Id %q", def.Id)
+		}
+		if def.OrgId != orgId {
+			t.Fatalf("bad OrgId %q", def.OrgId)
+		}
+		if def.Name != name {
+			t.Fatalf("bad Name %q", def.Name)
+		}
+		if def.Metric != metric {
+			t.Fatalf("bad id %q", def.Metric)
+		}
+		if def.Interval != interval {
+			t.Fatalf("bad Interval %q", def.Interval)
+		}
+		if def.Unit != unit {
+			t.Fatalf("bad Unit %q", def.Unit)
+		}
+		if def.TargetType != tt {
+			t.Fatalf("bad TargetType %q", def.TargetType)
+		}
+		if len(def.Tags) != 2 || def.Tags[0] != tag1 || def.Tags[1] != tag2 {
+			t.Fatalf("bad Tags %q", def.Tags)
+		}
+	}
+
+	def, ok := defCache.Get("id")
+	if ok {
+		t.Fatalf("lookup for 'id' should return no results")
+	}
+	m := &schema.MetricData{
+		Id:         "id",
+		OrgId:      1,
+		Name:       "name",
+		Metric:     "metric",
+		Interval:   10,
+		Value:      1.5,
+		Unit:       "unit",
+		Time:       1234567890,
+		TargetType: "gauge",
+		Tags:       []string{"tag1", "tag2"},
+	}
+
+	defCache.Add(m)
+	def, ok = defCache.Get("id")
+	if !ok {
+		t.Fatalf("lookup for 'id' should return a result")
+	}
+	assert(def, "id", 1, "name", "metric", 10, "unit", "gauge", "tag1", "tag2")
+
+	m.Id = "id2"
+	m.Name = "name2"
+	m.Tags[0] = "taga"
+	m.Tags[1] = "tagb"
+	defCache.Add(m)
+	def, ok = defCache.Get("id2")
+	if !ok {
+		t.Fatalf("lookup for 'id2' should return a result")
+	}
+	assert(def, "id2", 1, "name2", "metric", 10, "unit", "gauge", "taga", "tagb")
+	def, ok = defCache.Get("id")
+	if !ok {
+		t.Fatalf("lookup for 'id' should return a result")
+	}
+	assert(def, "id", 1, "name", "metric", 10, "unit", "gauge", "tag1", "tag2")
+}
