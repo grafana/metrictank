19902d9489b1fb72af094f1217c43a4d6d2f1858
Dieter Plaetinck <dieter@raintank.io>
2016-05-10 12:57:11 -0400

    set GitHash through build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+GIT_HASH=$(git rev-parse HEAD)
 
 for i in $(grep 'func main' */*.go | sed 's#/.*##'); do
   cd $i
   echo ">>> $i"
-  go build
+  go build -ldflags "-X main.GitHash=$GIT_HASH"
   cd ..
 done
