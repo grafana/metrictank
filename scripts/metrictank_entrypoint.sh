bc6927db57cbba8952a068cefeba51c5b1975c29
woodsaj <anthony@monkey.id.au>
2016-08-05 10:37:55 +0800

    add timeout support when waiting for remote hosts

--- scripts/metrictank_entrypoint.sh
+++ scripts/metrictank_entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-hosts=$(echo $WAIT_HOSTS | tr "," "\n")
+WAIT_TIMEOUT=${WAIT_TIMEOUT:-10}
 
-for endpoint in $hosts; do
+for endpoint in $(echo $WAIT_HOSTS | tr "," "\n"); do
+  _start_time=$(date +%s)  
   while true; do
+    _now=$(date +%s)
+    _run_time=$(( $_now - $_start_time ))
+    if [ $_run_time -gt $WAIT_TIMEOUT ]; then
+        echo "timed out waiting for $endpoint"
+        break 
+    fi
     echo "waiting for $endpoint to become up..."
-    host=${endpoint#*:}
-    port=${endpoint%:*}
+    host=${endpoint%:*}
+    port=${endpoint#*:}
     nc -z $host $port && echo "$endpoint is up!" && break
     sleep 1
   done
