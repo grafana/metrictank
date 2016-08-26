096c197597cb078996ce1fa1f6d6bcc164cd3c43
woodsaj <anthony@monkey.id.au>
2016-08-26 22:58:17 +0800

    add cassandra backed metricDef index

---
+++ idx/cassandra/cassandra.go
@@ -0,0 +1,243 @@
+package cassandra
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gocql/gocql"
+	"github.com/raintank/met"
+	"github.com/raintank/metrictank/idx"
+	"github.com/raintank/metrictank/idx/memory"
+	"github.com/raintank/worldping-api/pkg/log"
+	"github.com/rakyll/globalconf"
+	"gopkg.in/raintank/schema.v1"
+)
+
+const keyspace_schema = `CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}  AND durable_writes = true`
+const table_schema = `CREATE TABLE IF NOT EXISTS %s.metric_def_idx (
+    id text PRIMARY KEY,
+    def blob,
+) WITH compaction = {'class': 'SizeTieredCompactionStrategy'}
+    AND compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}`
+
+var (
+	idxCasOk             met.Count
+	idxCasFail           met.Count
+	idxCasAddDuration    met.Timer
+	idxCasDeleteDuration met.Timer
+
+	Enabled        bool
+	keyspace       string
+	hosts          string
+	consistency    string
+	timeout        time.Duration
+	numConns       int
+	writeQueueSize int
+	protoVer       int
+)
+
+func ConfigSetup() {
+	casIdx := flag.NewFlagSet("cassandra-idx", flag.ExitOnError)
+	casIdx.BoolVar(&Enabled, "enabled", false, "")
+	casIdx.StringVar(&keyspace, "keyspace", "metric", "Cassandra keyspace to store metricDefinitions in.")
+	casIdx.StringVar(&hosts, "hosts", "localhost:9042", "comma separated list of cassandra addresses in host:port form")
+	casIdx.StringVar(&consistency, "consistency", "one", "write consistency (any|one|two|three|quorum|all|local_quorum|each_quorum|local_one")
+	casIdx.DurationVar(&timeout, "timeout", time.Second, "cassandra request timeout")
+	casIdx.IntVar(&numConns, "num-conns", 10, "number of concurrent connections to cassandra")
+	casIdx.IntVar(&writeQueueSize, "write-queue-size", 100000, "Max number of metricDefs allowed to be unwritten to cassandra")
+	casIdx.IntVar(&protoVer, "protocol-version", 4, "cql protocol version to use")
+	globalconf.Register("cassandra-idx", casIdx)
+}
+
+type writeReq struct {
+	def      *schema.MetricDefinition
+	recvTime time.Time
+}
+
+// Implements the the "MetricIndex" interface
+type CasIdx struct {
+	memory.MemoryIdx
+	cluster    *gocql.ClusterConfig
+	session    *gocql.Session
+	writeQueue chan writeReq
+	mu         sync.Mutex
+	shutdown   chan struct{}
+	wg         sync.WaitGroup
+}
+
+func New() *CasIdx {
+	cluster := gocql.NewCluster(strings.Split(hosts, ",")...)
+	cluster.Consistency = gocql.ParseConsistency(consistency)
+	cluster.Timeout = timeout
+	cluster.NumConns = numConns
+	cluster.ProtoVersion = protoVer
+
+	return &CasIdx{
+		MemoryIdx:  *memory.New(),
+		cluster:    cluster,
+		writeQueue: make(chan writeReq, writeQueueSize),
+		shutdown:   make(chan struct{}),
+	}
+}
+
+func (c *CasIdx) Init(stats met.Backend) error {
+	log.Info("initializing CasIdx. Hosts=%s", hosts)
+	if err := c.MemoryIdx.Init(stats); err != nil {
+		return err
+	}
+
+	var err error
+	tmpSession, err := c.cluster.CreateSession()
+	if err != nil {
+		log.Error(3, "failed to create cassandra session. %s", err)
+		return err
+	}
+
+	// ensure the keyspace and table exist.
+	err = tmpSession.Query(fmt.Sprintf(keyspace_schema, keyspace)).Exec()
+	if err != nil {
+		log.Error(3, "failed to initialize cassandra keyspace. %s", err)
+		return err
+	}
+	err = tmpSession.Query(fmt.Sprintf(table_schema, keyspace)).Exec()
+	if err != nil {
+		log.Error(3, "failed to initialize cassandra table. %s", err)
+		return err
+	}
+	tmpSession.Close()
+	c.cluster.Keyspace = keyspace
+	session, err := c.cluster.CreateSession()
+	if err != nil {
+		log.Error(3, "failed to create cassandra session. %s", err)
+		return err
+	}
+
+	c.session = session
+
+	idxCasOk = stats.NewCount("idx.cassandra.ok")
+	idxCasFail = stats.NewCount("idx.cassandra.fail")
+	idxCasAddDuration = stats.NewTimer("idx.cassandra.add_duration", 0)
+	idxCasDeleteDuration = stats.NewTimer("idx.cassandra.delete_duration", 0)
+
+	for i := 0; i < numConns; i++ {
+		c.wg.Add(1)
+		go c.processWriteQueue()
+	}
+	//Rebuild the in-memory index.
+
+	c.rebuildIndex()
+	return nil
+}
+
+func (c *CasIdx) Stop() {
+	log.Info("stopping Cassandra-idx")
+	c.MemoryIdx.Stop()
+	close(c.shutdown)
+	c.wg.Wait()
+	c.session.Close()
+}
+
+func (c *CasIdx) Add(data *schema.MetricData) {
+	existing, err := c.MemoryIdx.Get(data.Id)
+	inMemory := true
+	if err != nil {
+		if err == idx.DefNotFound {
+			inMemory = false
+		} else {
+			log.Error(3, "Failed to query Memory Index for %s. %s", data.Id, err)
+			return
+		}
+	}
+	if inMemory {
+		log.Debug("def already seen before. Just updating memory Index")
+		existing.LastUpdate = data.Time
+		c.MemoryIdx.AddDef(&existing)
+		return
+	}
+	def := schema.MetricDefinitionFromMetricData(data)
+	c.MemoryIdx.AddDef(def)
+	c.writeQueue <- writeReq{recvTime: time.Now(), def: def}
+}
+
+func (c *CasIdx) rebuildIndex() {
+	log.Info("Rebuilding Memory Index from metricDefinitions in Cassandra")
+	pre := time.Now()
+	defs := make([]schema.MetricDefinition, 0)
+	iter := c.session.Query("SELECT def from metric_def_idx").Iter()
+
+	var data []byte
+	for iter.Scan(&data) {
+		mdef, err := schema.MetricDefinitionFromJSON(data)
+		if err != nil {
+			log.Error(3, "Bad definition in index. %s - %s", data, err)
+		}
+		defs = append(defs, *mdef)
+	}
+	c.MemoryIdx.Load(defs)
+	log.Info("Rebuilding Memory Index Complete. Took %s", time.Since(pre).String())
+}
+
+func (c *CasIdx) processWriteQueue() {
+	log.Info("cassandra-idx writeQueue handler started.")
+	var data bytes.Buffer
+	encoder := json.NewEncoder(&data)
+	var success bool
+	var attempts int
+	var req writeReq
+	for {
+		select {
+		case req = <-c.writeQueue:
+			data.Reset()
+			err := encoder.Encode(req.def)
+			if err != nil {
+				log.Error(3, "Failed to Unmarshal metricDef. %s", err)
+				continue
+			}
+			success = false
+			attempts = 0
+			for !success {
+				if err := c.session.Query(`INSERT INTO metric_def_idx (id, def) VALUES (?, ?)`, req.def.Id, data.Bytes()).Exec(); err != nil {
+					idxCasFail.Inc(1)
+					if (attempts % 20) == 0 {
+						log.Warn("Failed to write def to cassandra. it will be retried. %s", err)
+					}
+					sleepTime := 100 * attempts
+					if sleepTime > 2000 {
+						sleepTime = 2000
+					}
+					time.Sleep(time.Duration(sleepTime) * time.Millisecond)
+					attempts++
+				} else {
+					success = true
+					idxCasAddDuration.Value(time.Since(req.recvTime))
+					idxCasOk.Inc(1)
+					log.Debug("metricDef saved to cassandra. %s", req.def.Id)
+				}
+			}
+		case <-c.shutdown:
+			break
+		}
+	}
+	log.Info("cassandra-idx writeQueue handler ended.")
+	c.wg.Done()
+}
+
+func (c *CasIdx) Delete(orgId int, pattern string) error {
+	ids, err := c.MemoryIdx.DeleteWithReport(orgId, pattern)
+	if err != nil {
+		return err
+	}
+	for _, id := range ids {
+		err := c.session.Query("DELETE FROM metric_def_idx where id=?", id).Exec()
+		if err != nil {
+			log.Error(3, "Failed to delete metricDef %s from cassandra. %s", id, err)
+			return err
+		}
+	}
+	return nil
+}
---
+++ idx/cassandra/cassandra_test.go
@@ -0,0 +1,236 @@
+package cassandra
+
+import (
+	"crypto/rand"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/raintank/met/helper"
+	. "github.com/smartystreets/goconvey/convey"
+	"gopkg.in/raintank/schema.v1"
+)
+
+func init() {
+	keyspace = "raintank"
+	hosts = ""
+	consistency = "one"
+	timeout = time.Second
+	numConns = 1
+	writeQueueSize = 1000
+	protoVer = 4
+}
+
+func getSeriesNames(depth, count int, prefix string) []string {
+	series := make([]string, count)
+	for i := 0; i < count; i++ {
+		ns := make([]string, depth)
+		for j := 0; j < depth; j++ {
+			ns[j] = getRandomString(4)
+		}
+		series[i] = prefix + "." + strings.Join(ns, ".")
+	}
+	return series
+}
+
+// source: https://github.com/gogits/gogs/blob/9ee80e3e5426821f03a4e99fad34418f5c736413/modules/base/tool.go#L58
+func getRandomString(n int, alphabets ...byte) string {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		if len(alphabets) == 0 {
+			bytes[i] = alphanum[b%byte(len(alphanum))]
+		} else {
+			bytes[i] = alphabets[b%byte(len(alphabets))]
+		}
+	}
+	return string(bytes)
+}
+
+func getMetricData(orgId, depth, count, interval int, prefix string) []*schema.MetricData {
+	data := make([]*schema.MetricData, count)
+	series := getSeriesNames(depth, count, prefix)
+	for i, s := range series {
+
+		data[i] = &schema.MetricData{
+			Name:     s,
+			Metric:   s,
+			OrgId:    orgId,
+			Interval: interval,
+		}
+		data[i].SetId()
+	}
+	return data
+}
+
+func TestGetAddKey(t *testing.T) {
+	ix := New()
+	stats, _ := helper.New(false, "", "standard", "metrictank", "")
+	ix.Init(stats)
+
+	publicSeries := getMetricData(-1, 2, 5, 10, "metric.public")
+	org1Series := getMetricData(1, 2, 5, 10, "metric.org1")
+	org2Series := getMetricData(2, 2, 5, 10, "metric.org2")
+
+	for _, series := range [][]*schema.MetricData{publicSeries, org1Series, org2Series} {
+		orgId := series[0].OrgId
+		Convey(fmt.Sprintf("When indexing metrics for orgId %d", orgId), t, func() {
+			for _, s := range series {
+				ix.Add(s)
+			}
+			Convey(fmt.Sprintf("Then listing metrics for OrgId %d", orgId), func() {
+				defs := ix.List(orgId)
+				numSeries := len(series)
+				if orgId != -1 {
+					numSeries += 5
+				}
+				So(defs, ShouldHaveLength, numSeries)
+
+			})
+		})
+	}
+
+	Convey("When adding metricDefs with the same series name as existing metricDefs", t, func() {
+		for _, series := range org1Series {
+			series.Interval = 60
+			series.SetId()
+			ix.Add(series)
+		}
+		Convey("then listing metrics", func() {
+			defs := ix.List(1)
+			So(defs, ShouldHaveLength, 15)
+		})
+	})
+}
+
+func TestFind(t *testing.T) {
+	ix := New()
+	stats, _ := helper.New(false, "", "standard", "metrictank", "")
+	ix.Init(stats)
+	for _, s := range getMetricData(-1, 2, 5, 10, "metric.demo") {
+		ix.Add(s)
+	}
+	for _, s := range getMetricData(1, 2, 5, 10, "metric.demo") {
+		ix.Add(s)
+	}
+	for _, s := range getMetricData(1, 1, 5, 10, "foo.demo") {
+		ix.Add(s)
+		s.Interval = 60
+		s.SetId()
+		ix.Add(s)
+	}
+	for _, s := range getMetricData(2, 2, 5, 10, "metric.foo") {
+		ix.Add(s)
+	}
+
+	Convey("When listing root nodes", t, func() {
+		Convey("root nodes for orgId 1", func() {
+			nodes, err := ix.Find(1, "*")
+			So(err, ShouldBeNil)
+			So(nodes, ShouldHaveLength, 2)
+			So(nodes[0].Path, ShouldBeIn, "metric", "foo")
+			So(nodes[1].Path, ShouldBeIn, "metric", "foo")
+			So(nodes[0].Leaf, ShouldBeFalse)
+		})
+		Convey("root nodes for orgId 2", func() {
+			nodes, err := ix.Find(2, "*")
+			So(err, ShouldBeNil)
+			So(nodes, ShouldHaveLength, 1)
+			So(nodes[0].Path, ShouldEqual, "metric")
+			So(nodes[0].Leaf, ShouldBeFalse)
+		})
+	})
+
+	Convey("When searching with GLOB", t, func() {
+		nodes, err := ix.Find(2, "metric.{f*,demo}.*")
+		So(err, ShouldBeNil)
+		So(nodes, ShouldHaveLength, 10)
+		for _, n := range nodes {
+			So(n.Leaf, ShouldBeFalse)
+		}
+	})
+
+	Convey("When searching with multiple wildcards", t, func() {
+		nodes, err := ix.Find(1, "*.*")
+		So(err, ShouldBeNil)
+		So(nodes, ShouldHaveLength, 2)
+		for _, n := range nodes {
+			So(n.Leaf, ShouldBeFalse)
+		}
+	})
+
+	Convey("When searching nodes not in public series", t, func() {
+		nodes, err := ix.Find(1, "foo.demo.*")
+		So(err, ShouldBeNil)
+		So(nodes, ShouldHaveLength, 5)
+		Convey("When searching for specific series", func() {
+			found, err := ix.Find(1, nodes[0].Path)
+			So(err, ShouldBeNil)
+			So(found, ShouldHaveLength, 1)
+			So(found[0].Path, ShouldEqual, nodes[0].Path)
+		})
+		Convey("When searching nodes that are children of a leaf", func() {
+			found, err := ix.Find(1, nodes[0].Path+".*")
+			So(err, ShouldBeNil)
+			So(found, ShouldHaveLength, 0)
+		})
+	})
+
+	Convey("When searching with multiple wildcards mixed leaf/branch", t, func() {
+		nodes, err := ix.Find(1, "*.demo.*")
+		So(err, ShouldBeNil)
+		So(nodes, ShouldHaveLength, 15)
+		for _, n := range nodes {
+			if strings.HasPrefix(n.Path, "foo.demo") {
+				So(n.Leaf, ShouldBeTrue)
+				So(n.Defs, ShouldHaveLength, 2)
+			} else {
+				So(n.Leaf, ShouldBeFalse)
+			}
+		}
+	})
+	Convey("When searching nodes for unkown orgId", t, func() {
+		nodes, err := ix.Find(4, "foo.demo.*")
+		So(err, ShouldBeNil)
+		So(nodes, ShouldHaveLength, 0)
+	})
+
+	Convey("When searching nodes that dont exist", t, func() {
+		nodes, err := ix.Find(1, "foo.demo.blah.*")
+		So(err, ShouldBeNil)
+		So(nodes, ShouldHaveLength, 0)
+	})
+
+}
+
+func BenchmarkIndexing(b *testing.B) {
+	keyspace = "raintank"
+	hosts = "localhost:9042"
+	consistency = "one"
+	timeout = time.Second
+	numConns = 10
+	writeQueueSize = 10
+	protoVer = 4
+	ix := New()
+	stats, _ := helper.New(false, "", "standard", "metrictank", "")
+	ix.Init(stats)
+
+	var series string
+	var data *schema.MetricData
+	b.ReportAllocs()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		series = "some.metric." + strconv.Itoa(n)
+		data = &schema.MetricData{
+			Name:     series,
+			Metric:   series,
+			Interval: 10,
+			OrgId:    1,
+		}
+		data.SetId()
+		ix.Add(data)
+	}
+}
