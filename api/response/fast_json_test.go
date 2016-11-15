5177fcf209077755ec86605341ee63a42a096c68
Dieter Plaetinck <dieter@raintank.io>
2016-11-15 15:02:37 +0200

    test integers vs floats separately, for Json and FastJson
    the previous benchmarks were "asymmetrical", it was weird to have 1
    bench measure encoding 2 series and another just 1, though it was
    helpful to compare to the code before httpRefactor.
    
    now though, this makes more sense.
    
    $ go test -v -run='^$' -bench=Json
    BenchmarkHttpRespFastJsonIntegers-8   	   10000	    176075 ns/op	  68.15 MB/s
    BenchmarkHttpRespFastJsonFloats-8     	    5000	    240288 ns/op	  49.94 MB/s
    BenchmarkHttpRespJsonIntegers-8       	    5000	    373856 ns/op	  32.10 MB/s
    BenchmarkHttpRespJsonFloats-8         	    3000	    424514 ns/op	  28.27 MB/s
    PASS
    ok  	github.com/raintank/metrictank/api/response	6.240s

--- api/response/fast_json_test.go
+++ api/response/fast_json_test.go
@@ -19,27 +19,43 @@ func TestFastJson(t *testing.T) {
 	}
 }
 
-func BenchmarkHttpRespFastJson(b *testing.B) {
-	pA := make([]schema.Point, 1000, 1000)
-	pB := make([]schema.Point, 1000, 1000)
+func BenchmarkHttpRespFastJsonIntegers(b *testing.B) {
+	points := make([]schema.Point, 1000, 1000)
 	baseTs := 1500000000
 	for i := 0; i < 1000; i++ {
-		pA[i] = schema.Point{Val: float64(10000 * i), Ts: uint32(baseTs + 10*i)}
-		pB[i] = schema.Point{Val: 12.34 * float64(i), Ts: uint32(baseTs + 10*i)}
+		points[i] = schema.Point{Val: float64(10000 * i), Ts: uint32(baseTs + 10*i)}
 	}
 	data := []models.Series{
 		{
 			Target:     "some.metric.with.a-whole-bunch-of.integers",
-			Datapoints: pA,
-			Interval:   10,
-		},
-		{
-			Target:     "some.metric.with.a-whole-bunch-of.floats",
-			Datapoints: pB,
+			Datapoints: points,
 			Interval:   10,
 		},
 	}
-	b.SetBytes(int64(len(pA) * 2 * 12))
+	b.SetBytes(int64(len(points) * 12))
+
+	b.ResetTimer()
+	var resp *FastJson
+	for n := 0; n < b.N; n++ {
+		resp = NewFastJson(200, models.SeriesByTarget(data))
+		resp.Body()
+		resp.Close()
+	}
+}
+func BenchmarkHttpRespFastJsonFloats(b *testing.B) {
+	points := make([]schema.Point, 1000, 1000)
+	baseTs := 1500000000
+	for i := 0; i < 1000; i++ {
+		points[i] = schema.Point{Val: 12.34 * float64(i), Ts: uint32(baseTs + 10*i)}
+	}
+	data := []models.Series{
+		{
+			Target:     "some.metric.with.a-whole-bunch-of.floats",
+			Datapoints: points,
+			Interval:   10,
+		},
+	}
+	b.SetBytes(int64(len(points) * 12))
 
 	b.ResetTimer()
 	var resp *FastJson
