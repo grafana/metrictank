1f8ebb78ac3694ba48ca8343a9d324c9a0394def
Dieter Plaetinck <dieter@raintank.io>
2016-11-15 15:10:11 +0200

    also benchmark encoding series with nulls
    apparently nulls are really fast. Who knew?
    
    $ go test -v -run='^$' -bench=Json
    BenchmarkHttpRespFastJsonIntegers-8   	   10000	    178158 ns/op	  67.36 MB/s
    BenchmarkHttpRespFastJsonFloats-8     	    5000	    239572 ns/op	  50.09 MB/s
    BenchmarkHttpRespFastJsonNulls-8      	   50000	     31473 ns/op	 381.28 MB/s
    BenchmarkHttpRespJsonIntegers-8       	    5000	    376646 ns/op	  31.86 MB/s
    BenchmarkHttpRespJsonFloats-8         	    3000	    426699 ns/op	  28.12 MB/s
    BenchmarkHttpRespJsonNulls-8          	   10000	    175048 ns/op	  68.55 MB/s
    PASS
    ok  	github.com/raintank/metrictank/api/response	10.871s

--- api/response/fast_json_test.go
+++ api/response/fast_json_test.go
@@ -1,6 +1,7 @@
 package response
 
 import (
+	"math"
 	"net/http/httptest"
 	"testing"
 
@@ -51,6 +52,29 @@ func BenchmarkHttpRespFastJsonFloats(b *testing.B) {
 	data := []models.Series{
 		{
 			Target:     "some.metric.with.a-whole-bunch-of.floats",
+			Datapoints: points,
+			Interval:   10,
+		},
+	}
+	b.SetBytes(int64(len(points) * 12))
+
+	b.ResetTimer()
+	var resp *FastJson
+	for n := 0; n < b.N; n++ {
+		resp = NewFastJson(200, models.SeriesByTarget(data))
+		resp.Body()
+		resp.Close()
+	}
+}
+func BenchmarkHttpRespFastJsonNulls(b *testing.B) {
+	points := make([]schema.Point, 1000, 1000)
+	baseTs := 1500000000
+	for i := 0; i < 1000; i++ {
+		points[i] = schema.Point{Val: math.NaN(), Ts: uint32(baseTs + 10*i)}
+	}
+	data := []models.Series{
+		{
+			Target:     "some.metric.with.a-whole-bunch-of.nulls",
 			Datapoints: points,
 			Interval:   10,
 		},
