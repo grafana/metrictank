a5d257438e793746b15689c1dca2524435512235
woodsaj <awoods@raintank.io>
2016-12-23 14:39:16 +0800

    add build scripts for tools

---
+++ scripts/build_tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+# Find the directory we exist within
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+GITVERSION=`git describe --always`
+SOURCEDIR=${DIR}/..
+BUILDDIR=$SOURCEDIR/build
+
+# Make dir
+mkdir -p $BUILDDIR
+
+# disable cgo
+export CGO_ENABLED=0
+
+# Build binary
+cd $GOPATH/src/github.com/raintank/metrictank/cmd
+for tool in *; do
+  cd $tool
+  go build -ldflags "-X main.GitHash=$GITVERSION" -o $BUILDDIR/$tool
+done
