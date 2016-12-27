6353f52c43543ce2c8bafaca77cd909e987d9e7e
Dieter Plaetinck <dieter@raintank.io>
2016-12-27 19:02:43 +0100

    just run launch.sh with env as argument, from anywhere

--- docker/extra/populate-grafana.sh
+++ docker/extra/populate-grafana.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
-basedir=$(dirname "$0")
-envsdir=$basedir/..
-env="$1"
-if [[ ! "$env" =~ ^docker- ]]; then
-	echo "env must start with docker-" >&2
-	exit 1
-fi
-
-if [ ! -d "$envsdir/$env" ]; then
-	echo -e "Could not find docker environment $envsdir/$env\n" >&2
-	echo -e "Known environments:\n" >&2
-	cd $envsdir
-	ls -1d docker-* >&2
-	exit 1
-fi
+env=$1
 
 WAIT_HOSTS=localhost:3000 ../../scripts/wait_for_endpoint.sh
 
-for file in $envsdir/$env/datasources/*; do
+for file in $env/datasources/*; do
   echo "> adding datasources $file"
   curl -u admin:admin -H "content-type: application/json" 'http://localhost:3000/api/datasources' -X POST --data-binary @$file
   echo
 done
 
-for file in $basedir/../../dashboard.json $basedir/dashboards/*; do
+for file in $env/../../dashboard.json $env/../extra/dashboards/*; do
   if grep -q "__inputs" $file; then
     echo "> importing dashboard $file"
     curl -u admin:admin \
@@ -52,4 +38,3 @@ for file in $basedir/../../dashboard.json $basedir/dashboards/*; do
   echo
   echo
 done
-
--- docker/launch.sh
+++ docker/launch.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+
+basedir=$(dirname "$0")
+env="$1"
+
+function knownEnvs () {
+	echo -e "Known environments:\n" >&2
+	cd $basedir
+	ls -1d docker-* >&2
+}
+
+if [[ ! "$env" =~ ^docker- ]]; then
+	echo "env must start with docker-" >&2
+	knownEnvs
+	exit 1
+fi
+
+if [ ! -d "$basedir/$env" ]; then
+	echo -e "Could not find docker environment $env\n" >&2
+	knownEnvs
+	exit 1
+fi
+
+cd $basedir/$env
+
 trap ctrl_c INT
 
 function ctrl_c() {
     docker-compose down
 }
 
-../extra/populate-grafana.sh "$(basename $(pwd))" &
+../extra/populate-grafana.sh $PWD &
 docker-compose up --force-recreate
