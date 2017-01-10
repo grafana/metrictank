4c9240ad9d3fef43666be786e359bc123aede722
Mauro Stettler <mauro.stettler@gmail.com>
2017-01-10 07:32:31 -0800

    adds two more unit tests which are testing:
    - does aggmetric call the cache push callback every time a metric gets
      evicted from the ring buffer?
    - does aggmetric add the chunk into the store if the node is primary?
    - makes the devnullStore count how many times it's Add() got called
    - does aggmetric not add the chunk into the store if the node is not primary?

--- mdata/store_devnull.go
+++ mdata/store_devnull.go
@@ -3,6 +3,7 @@ package mdata
 import "github.com/raintank/metrictank/mdata/chunk"
 
 type devnullStore struct {
+	AddCount uint32
 }
 
 func NewDevnullStore() *devnullStore {
@@ -11,6 +12,11 @@ func NewDevnullStore() *devnullStore {
 }
 
 func (c *devnullStore) Add(cwr *ChunkWriteRequest) {
+	c.AddCount++
+}
+
+func (c *devnullStore) Reset() {
+	c.AddCount = 0
 }
 
 func (c *devnullStore) Search(key string, start, end uint32) ([]chunk.IterGen, error) {
