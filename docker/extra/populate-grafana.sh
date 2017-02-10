1be9726729538fa6f05acfddb5f1a0665fac3059
Dieter Plaetinck <dieter@raintank.io>
2017-02-10 12:18:47 +0100

    fix support for different datasource arrangements

--- docker/extra/populate-grafana.sh
+++ docker/extra/populate-grafana.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# different docker stacks may have a different arrangement of datasources, for example
+# the standard / sample stacks use metrictank + graphite-api for the data "in metrictank" (e.g. fakemetrics, worldping, ..) as well as monitoring data (such as MT's perf stats)
+# fancier stacks such as the clustering one have a separate graphite server for monitoring data.
+# there needs to be an elegant way to make this seamless when importing dashboards: they should be setup so that they point to the correct datasource.
+# we could fix this by having different datasource settings, and in some cases pointing them to the exact same backend (e.g. a 'monitoring' and 'metrictank' datasource that for the standard stack would point to graphite-api backed by metrictank), but that could cause some confusion when using the UI.
+
+# so instead, we use the following convention for dashboards with an __inputs section::
+
+# 1) DS_GRAPHITE -> data in "graphite" for monitoring (could be backed by metrictank in simple setups. We could potentially change this to DS_MONITORING
+# 2) DS_GRAPHITENG -> data in metrictank, queried via graphite-api
+# [ in the future we may want to add:
+# 3) DS_METRICTANK -> querying metrictank directly (very uncommon) ]
+
+# the stacks then declare how they map these to concrete datasources, so that when we import the dashboard for a given stack, we know which datasource to use.
+
 env=$1
 
 WAIT_HOSTS=localhost:3000 ../../scripts/wait_for_endpoint.sh
 
-for file in $env/datasources/*; do
+for file in $env/datasources/*.json; do
   echo "> adding datasources $file"
   curl -u admin:admin -H "content-type: application/json" 'http://localhost:3000/api/datasources' -X POST --data-binary @$file
   echo
@@ -25,7 +40,13 @@ for file in $env/../../dashboard.json $env/../extra/dashboards/*; do
               \"name\": \"DS_GRAPHITE\",
               \"type\": \"datasource\",
               \"pluginId\": \"graphite\",
-              \"value\": \"graphite\"
+	      \"value\": \"$(cat $env/datasources/DS_GRAPHITE)\"
+            },
+            {
+              \"name\": \"DS_GRAPHITENG\",
+              \"type\": \"datasource\",
+              \"pluginId\": \"graphite\",
+	      \"value\": \"$(cat $env/datasources/DS_GRAPHITENG)\"
             }
           ]
         }"
