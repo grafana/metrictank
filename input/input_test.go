60c2ba66da177a8dad282fb178a331d9e34b5405
Dieter Plaetinck <dieter@raintank.io>
2017-03-09 11:36:33 +0100

    split up input Processing tests to compare same metrics vs new ones
    on my laptop:
    
    MASTER:
    BenchmarkProcess-8     2000000         718 ns/op
    PASS
    ok    github.com/raintank/metrictank/input  2.796s
    
    HEAD:
    go test -run='^$' -bench=.
    BenchmarkProcessUniqueMetrics-8      1000000        2388 ns/op
    BenchmarkProcessSameMetric-8         2000000         885 ns/op
    PASS
    ok    github.com/raintank/metrictank/input  6.081s
    
    So we're a bit slower but carbon input should be a lot faster
    (for which we don't have benchmarks) since it used to do regex matching
    all the time

--- input/input_test.go
+++ input/input_test.go
@@ -1,6 +1,7 @@
 package input
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -14,7 +15,7 @@ import (
 	"gopkg.in/raintank/schema.v1"
 )
 
-func BenchmarkProcess(b *testing.B) {
+func BenchmarkProcessUniqueMetrics(b *testing.B) {
 	cluster.Init("default", "test", time.Now(), "http", 6060)
 
 	store := mdata.NewDevnullStore()
@@ -32,11 +33,52 @@ func BenchmarkProcess(b *testing.B) {
 	// timestamps start at 10 and go up from there. (we can't use 0, see AggMetric.Add())
 	datas := make([]*schema.MetricData, b.N)
 	for i := 0; i < b.N; i++ {
+		name := fmt.Sprintf("fake.metric.%d", i)
 		metric := &schema.MetricData{
 			Id:       "some.id.of.a.metric",
 			OrgId:    500,
-			Name:     "some.id",
-			Metric:   "metric",
+			Name:     name,
+			Metric:   name,
+			Interval: 10,
+			Value:    1234.567,
+			Unit:     "ms",
+			Time:     int64((i + 1) * 10),
+			Mtype:    "gauge",
+			Tags:     []string{"some_tag", "ok"},
+		}
+		datas[i] = metric
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		in.Process(datas[i], 1)
+	}
+}
+
+func BenchmarkProcessSameMetric(b *testing.B) {
+	cluster.Init("default", "test", time.Now(), "http", 6060)
+
+	store := mdata.NewDevnullStore()
+
+	mdata.SetSingleSchema(whisper.NewRetentionMT(10, 10000, 600, 10, true))
+	mdata.SetOnlyDefaultAgg(whisper.Average, whisper.Min, whisper.Max)
+	mdata.Cache(time.Minute, time.Hour)
+
+	aggmetrics := mdata.NewAggMetrics(store, &cache.MockCache{}, 800, 8000, 0)
+	metricIndex := memory.New()
+	metricIndex.Init()
+	usage := usage.New(300, aggmetrics, metricIndex, clock.New())
+	in := NewDefaultHandler(aggmetrics, metricIndex, usage, "BenchmarkProcess")
+
+	// timestamps start at 10 and go up from there. (we can't use 0, see AggMetric.Add())
+	datas := make([]*schema.MetricData, b.N)
+	for i := 0; i < b.N; i++ {
+		name := "fake.metric.same"
+		metric := &schema.MetricData{
+			Id:       "some.id.of.a.metric",
+			OrgId:    500,
+			Name:     name,
+			Metric:   name,
 			Interval: 10,
 			Value:    1234.567,
 			Unit:     "ms",
