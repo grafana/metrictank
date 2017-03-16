e76de649ba8fd85f3ac36358c0936086202ca3e7
Dieter Plaetinck <dieter@raintank.io>
2017-03-16 12:51:12 +0100

    fix merge conflict

--- cmd/mt-whisper-importer-writer/main.go
+++ cmd/mt-whisper-importer-writer/main.go
@@ -175,11 +175,7 @@ func (s *Server) chunksHandler(w http.ResponseWriter, req *http.Request) {
 	avg := (whisper.AggregationMethod(metric.AggregationMethod) == whisper.AggregationAverage)
 
 	partition, err := s.Partitioner.Partition(&metric.MetricData, int32(*numPartitions))
-	err = s.Index.AddOrUpdate(&metric.MetricData, partition)
-	if err != nil {
-		throwError(fmt.Sprintf("Error updating metric index: %q", err))
-		return
-	}
+	s.Index.AddOrUpdate(&metric.MetricData, partition)
 
 	for archiveIdx, a := range metric.Archives {
 		archiveTTL := a.SecondsPerPoint * a.Points
