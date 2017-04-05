ae7ed849165c836a62e95b5cc71ef3e956916955
Dieter Plaetinck <dieter@raintank.io>
2017-04-05 21:02:51 +0200

    fix

--- cmd/mt-store-cat/full.go
+++ cmd/mt-store-cat/full.go
@@ -17,7 +17,7 @@ func chunkSummary(store *mdata.CassandraStore, tables []string, metrics []Metric
 		// actual TTL may be up to 2x what's in tablename. see mdata/store_cassandra.go for details
 		// we query up to 4x so that we also include data that should have been dropped already but still sticks around for whatever reason.
 		TTLHours, _ := strconv.Atoi(strings.Split(tbl, "_")[1])
-		start := now - uint32(4*TTLHours)
+		start := now - uint32(4*TTLHours*3600)
 		start_month := start - (start % mdata.Month_sec)
 		fmt.Println("## Table", tbl)
 		if len(metrics) == 0 {
@@ -26,7 +26,7 @@ func chunkSummary(store *mdata.CassandraStore, tables []string, metrics []Metric
 			showKeyTTL(iter, roundTTL)
 		} else {
 			for _, metric := range metrics {
-				for month := start_month; month <= end_month; month++ {
+				for month := start_month; month <= end_month; month += mdata.Month_sec {
 					row_key := fmt.Sprintf("%s_%d", metric.id, month/mdata.Month_sec)
 					query := fmt.Sprintf("select key, ttl(data) from %s where key=?", tbl)
 					iter := store.Session.Query(query, row_key).Iter()
