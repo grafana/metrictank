237fd005eba1e3ed0c5d407b688be72365d54b8a
Dieter Plaetinck <dieter@grafana.com>
2017-04-17 20:09:54 -0400

    properly handle failure to read request body

--- api/middleware/capturebody.go
+++ api/middleware/capturebody.go
@@ -3,10 +3,16 @@ package middleware
 import (
 	"bytes"
 	"io/ioutil"
+
+	"github.com/raintank/worldping-api/pkg/log"
 )
 
 func CaptureBody(c *Context) {
-	body, _ := ioutil.ReadAll(c.Req.Request.Body)
+	body, err := ioutil.ReadAll(c.Req.Request.Body)
+	if err != nil {
+		log.Error(3, "HTTP internal error: failed to read request body for proxying: %s", err)
+		c.PlainText(500, []byte("internal error: failed to read request body for proxying"))
+	}
 	c.Req.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 	c.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 }
