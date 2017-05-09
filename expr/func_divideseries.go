ab849b2f3d7f41a6c0bb9b063a9fccbb7a470172
Dieter Plaetinck <dieter@grafana.com>
2017-05-09 14:52:31 -0400

    add scale and divideSeries

---
+++ expr/func_divideseries.go
@@ -0,0 +1,69 @@
+package expr
+
+import (
+	"errors"
+	"fmt"
+	"math"
+
+	"github.com/raintank/metrictank/api/models"
+	"gopkg.in/raintank/schema.v1"
+)
+
+type FuncDivideSeries struct {
+	dividend GraphiteFunc
+	divisor  GraphiteFunc
+}
+
+func NewDivideSeries() GraphiteFunc {
+	return &FuncDivideSeries{}
+}
+
+func (s *FuncDivideSeries) Signature() ([]Arg, []Arg) {
+	return []Arg{
+		ArgSeriesList{val: &s.dividend},
+		ArgSeries{val: &s.divisor},
+	}, []Arg{ArgSeries{}}
+}
+
+func (s *FuncDivideSeries) NeedRange(from, to uint32) (uint32, uint32) {
+	return from, to
+}
+
+func (s *FuncDivideSeries) Exec(cache map[Req][]models.Series) ([]models.Series, error) {
+	dividends, err := s.dividend.Exec(cache)
+	if err != nil {
+		return nil, err
+	}
+	divisors, err := s.divisor.Exec(cache)
+	if err != nil {
+		return nil, err
+	}
+	if len(divisors) != 1 {
+		return nil, errors.New(fmt.Sprintf("need 1 divisor series, not %d", len(divisors)))
+	}
+	divisor := divisors[0]
+
+	var series []models.Series
+	for _, dividend := range dividends {
+		out := pointSlicePool.Get().([]schema.Point)
+		for i := 0; i < len(dividend.Datapoints); i++ {
+			p := schema.Point{
+				Ts: dividend.Datapoints[i].Ts,
+			}
+			if divisor.Datapoints[i].Val == 0 {
+				p.Val = math.NaN()
+			} else {
+				p.Val = dividend.Datapoints[i].Val / divisor.Datapoints[i].Val
+			}
+			out = append(out, p)
+		}
+		output := models.Series{
+			Target:     fmt.Sprintf("divideSeries(%s,%s)", dividend.Target, divisor.Target),
+			Datapoints: out,
+			Interval:   divisor.Interval,
+		}
+		cache[Req{}] = append(cache[Req{}], output)
+		series = append(series, output)
+	}
+	return series, nil
+}
---
+++ expr/func_scale.go
@@ -0,0 +1,52 @@
+package expr
+
+import (
+	"fmt"
+
+	"github.com/raintank/metrictank/api/models"
+	"gopkg.in/raintank/schema.v1"
+)
+
+type FuncScale struct {
+	in     GraphiteFunc
+	factor float64
+}
+
+func NewScale() GraphiteFunc {
+	return &FuncScale{}
+}
+
+func (s *FuncScale) Signature() ([]Arg, []Arg) {
+	return []Arg{
+			ArgSeriesList{val: &s.in},
+			ArgFloat{key: "factor", val: &s.factor},
+		}, []Arg{
+			ArgSeriesList{},
+		}
+}
+
+func (s *FuncScale) NeedRange(from, to uint32) (uint32, uint32) {
+	return from, to
+}
+
+func (s *FuncScale) Exec(cache map[Req][]models.Series) ([]models.Series, error) {
+	series, err := s.in.Exec(cache)
+	if err != nil {
+		return nil, err
+	}
+	var outputs []models.Series
+	for _, serie := range series {
+		out := pointSlicePool.Get().([]schema.Point)
+		for _, v := range serie.Datapoints {
+			out = append(out, schema.Point{Val: v.Val * s.factor, Ts: v.Ts})
+		}
+		s := models.Series{
+			Target:     fmt.Sprintf("scale(%s,%f)", serie.Target, s.factor),
+			Datapoints: out,
+			Interval:   serie.Interval,
+		}
+		outputs = append(outputs, s)
+		cache[Req{}] = append(cache[Req{}], s)
+	}
+	return outputs, nil
+}
