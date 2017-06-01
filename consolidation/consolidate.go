50b73d3a2b1663017c29f2abf9d355eb814a6b21
Dieter Plaetinck <dieter@grafana.com>
2017-06-01 19:02:56 +0300

    refactor nudging
    * move nudging-based consolidation to consolidation package
      this will make it easier to unit test, as opposed to having it in the
      plan.
    * since our aggregation returns points that are clean multiples of the
      new interval, and we don't want to move information into the past
      (which would be lying about the future) e.g. instead of moving a spike of data to
      an earlier timestamp, we move it to a later one; since we do this, we
      need to adjust the nudging logic to treat the point after a clean
      interval as the first one of any bucket.

--- consolidation/consolidate.go
+++ consolidation/consolidate.go
@@ -59,3 +59,69 @@ func AggEvery(numPoints, maxPoints uint32) uint32 {
 	}
 	return (numPoints + maxPoints - 1) / maxPoints
 }
+
+func ConsolidateStable(points []schema.Point, interval, maxDataPoints uint32, consolidator Consolidator) ([]schema.Point, uint32) {
+	aggNum := AggEvery(uint32(len(points)), maxDataPoints)
+	// nudging only really makes sense if we have enough points to strip (which is <= 1 postAggInterval's worth)
+	// or in the general case i would say we shouldn't make drastic time range alterations, e.g. only nudge
+	// if we have points > 2 * postAggInterval's worth
+	// this also assures that in the special case where people request MaxDataPoints=1 we will always consolidate
+	// all points together (because aggNum is set high enough) and don't trim a significant amount of the points
+	// that are expected to go into the aggregation
+	// e.g. consider a case where we have points with ts 130,140,150,160, aggNum=4, postAggInterval is 40,
+	// we shouldn't strip the first 3 points, so we only start stripping if we have more than 2*4=8 points
+	if len(points) > int(2*aggNum) {
+		_, num := nudge(points[0].Ts, interval, aggNum)
+		points = points[num:]
+	}
+	points = Consolidate(points, aggNum, consolidator)
+	interval *= aggNum
+	return points, interval
+}
+
+// Nudge computes the parameters for nudging.
+// let's say a series has points A,B,C,D and we must consolidate with numAgg=2.
+// if we wait a step, point E appears into the visible window and A will slide out of the window.
+// there's a few approaches you can take wrt such changes across refreshes:
+// 1) naive old approach:
+//    on first load return consolidate(A,B), consolidate(C,D)
+//    after a step, return consolidate(B,C), consolidate(D,E)
+//    => this looks weird across refreshes:
+//       both the values as well as the timestamps change everywhere, points jump around on the chart
+// 2) graphite-style nudging: trim a few of the first points away as needed, so that the first TS
+//    is always a multiple of the postConsolidationInterval (note: assumes input is quantized!)
+//    on first load return consolidate(A,B), consolidate(C,D)
+//    after a step, return consolidate(C,D), consolidate(E)
+//    => same points are always consolidated together, no jumping around.
+//    => simple to understand, but not the most performant (fetches/processes some points needlessly)
+//    => tends to introduce emptyness in graphs right after the requested start.
+//       (because Grafana plots from requested start, not returned start, and so there will be some empty space
+//       where we trimmed points)
+// 3) similar to 2, but don't trim, rather consolidate the leftovers both on the start and at the end.
+//    on first load return consolidate(A,B), consolidate(C,D)
+//    after a step, return consolidate(B), consolidate(C,D), consolidate(E)
+//    => same points are always consolidated together, no jumping around.
+//    => only datapoint up front and at the end may jump around, but not the vast majority
+//    => no discarding of points
+//    => requires a large code change though, as it makes it harder to honor MaxDataPoints.
+//       e.g. MDP=1, you have 5 points and aggNum is 5, if alignment is improper, it would cause
+//       2 output points, so we would have to rewrite a lot of code, no longer compute AggNum in advance etc
+//
+// note that with all 3 approaches, we always consolidate leftovers at the end together, so with any approach
+// the last point may jump around (see Consolidate function)
+// for now, and for simplicity we just implement the 2nd approach. it's also the only one that assures MDP is strictly
+// honored (see last point of approach 3, which also affects approach 1)
+func nudge(start, preAggInterval, aggNum uint32) (uint32, int) {
+	postAggInterval := preAggInterval * aggNum
+	var num int
+	var diff uint32
+	// move start until it maps to the first point of an aggregation bucket
+	// since clean multiples of the new postAggInterval are the last point to go into an aggregation
+	// we want a point that comes preAggInterval after it.
+	remainder := (start - preAggInterval) % postAggInterval
+	if remainder > 0 {
+		diff = postAggInterval - remainder
+		num = int(diff / preAggInterval)
+	}
+	return diff, num
+}
--- expr/plan.go
+++ expr/plan.go
@@ -223,69 +223,10 @@ func (p Plan) Run(input map[Req][]models.Series) ([]models.Series, error) {
 			if o.Consolidator == 0 {
 				o.Consolidator = consolidation.Avg
 			}
-			aggNum := consolidation.AggEvery(uint32(len(o.Datapoints)), p.MaxDataPoints)
-			// nudging only really makes sense if we have enough points to strip (which is <= 1 postAggInterval's worth)
-			// or in the general case i would say we shouldn't make drastic time range alterations, e.g. only nudge
-			// if we have points > 2 * postAggInterval's worth
-			// this also assures that in the special case where people request MaxDataPoints=1 we will always consolidate
-			// all points together (because aggNum is set high enough) and don't trim a significant amount of the points
-			// that are expected to go into the aggregation
-			// e.g. consider a case where we have points with ts 130,140,150,160, aggNum=4, postAggInterval is 40,
-			// we shouldn't strip the first 3 points, so we only start stripping if we have more than 2*4=8 points
-			if len(o.Datapoints) > int(2*aggNum) {
-				_, num := nudge(out[i].Datapoints[0].Ts, out[i].Interval, aggNum)
-				o.Datapoints = o.Datapoints[num:]
-			}
-			out[i].Datapoints = consolidation.Consolidate(o.Datapoints, aggNum, o.Consolidator)
-			out[i].Interval *= aggNum
+			out[i].Datapoints, out[i].Interval = consolidation.ConsolidateStable(o.Datapoints, o.Interval, p.MaxDataPoints, o.Consolidator)
 		}
 	}
 	return out, nil
-}
-
-// Nudge computes the parameters for nudging.
-// let's say a series has points A,B,C,D and we must consolidate with numAgg=2.
-// if we wait a step, point E appears into the visible window and A will slide out of the window.
-// there's a few approaches you can take wrt such changes across refreshes:
-// 1) naive old approach:
-//    on first load return consolidate(A,B), consolidate(C,D)
-//    after a step, return consolidate(B,C), consolidate(D,E)
-//    => this looks weird across refreshes:
-//       both the values as well as the timestamps change everywhere, points jump around on the chart
-// 2) graphite-style nudging: trim a few of the first points away as needed, so that the first TS
-//    is always a multiple of the postConsolidationInterval (note: assumes input is quantized!)
-//    on first load return consolidate(A,B), consolidate(C,D)
-//    after a step, return consolidate(C,D), consolidate(E)
-//    => same points are always consolidated together, no jumping around.
-//    => simple to understand, but not the most performant (fetches/processes some points needlessly)
-//    => tends to introduce emptyness in graphs right after the requested start.
-//       (because Grafana plots from requested start, not returned start, and so there will be some empty space
-//       where we trimmed points)
-// 3) similar to 2, but don't trim, rather consolidate the leftovers both on the start and at the end.
-//    on first load return consolidate(A,B), consolidate(C,D)
-//    after a step, return consolidate(B), consolidate(C,D), consolidate(E)
-//    => same points are always consolidated together, no jumping around.
-//    => only datapoint up front and at the end may jump around, but not the vast majority
-//    => no discarding of points
-//    => requires a large code change though, as it makes it harder to honor MaxDataPoints.
-//       e.g. MDP=1, you have 5 points and aggNum is 5, if alignment is improper, it would cause
-//       2 output points, so we would have to rewrite a lot of code, no longer compute AggNum in advance etc
-//
-// note that with all 3 approaches, we always consolidate leftovers at the end together, so with any approach
-// the last point may jump around (see Consolidate function)
-// for now, and for simplicity we just implement the 2nd approach. it's also the only one that assures MDP is strictly
-// honored (see last point of approach 3, which also affects approach 1)
-func nudge(start, preAggInterval, aggNum uint32) (uint32, int) {
-	postAggInterval := preAggInterval * aggNum
-	var num int
-	var diff uint32
-	// move start until it maps to first clean multiple of the new postAggInterval
-	remainder := start % postAggInterval
-	if remainder > 0 {
-		diff = postAggInterval - remainder
-		num = int(diff / preAggInterval)
-	}
-	return diff, num
 }
 
 // Clean returns all buffers (all input data + generated series along the way)
