9b6ac10e9de14f16c7f009b6b2135900deabd851
Dieter Plaetinck <dieter@grafana.com>
2017-08-11 11:20:28 +0200

    pull in latest carbon20. fix #668

--- vendor/github.com/metrics20/go-metrics20/carbon20/validate.go
+++ vendor/github.com/metrics20/go-metrics20/carbon20/validate.go
@@ -216,7 +216,8 @@ func ValidatePacket(buf []byte, levelLegacy ValidationLevelLegacy, levelM20 Vali
 
 	// graphite graciously allows a leading dot by pretending it's not there.
 	// (e.g. send ".foo" -> metric will become "foo") so we do the same.
-	// see #668
+	// see https://github.com/raintank/metrictank/issues/668 and
+	// https://github.com/raintank/metrictank/issues/694
 	if len(fields[0]) != 0 && fields[0][0] == '.' {
 		fields[0] = fields[0][1:]
 	}
@@ -229,17 +230,17 @@ func ValidatePacket(buf []byte, levelLegacy ValidationLevelLegacy, levelM20 Vali
 		err = ValidateKeyM20NoEqualsB(fields[0], levelM20)
 	}
 	if err != nil {
-		return empty, 0, 0, err
+		return fields[0], 0, 0, err
 	}
 
-	val, err := strconv.ParseFloat(string(fields[1]), 32)
+	val, err := strconv.ParseFloat(string(fields[1]), 64)
 	if err != nil {
-		return empty, 0, 0, errValNotNumber
+		return fields[0], 0, 0, errValNotNumber
 	}
 
-	ts, err := strconv.ParseUint(string(fields[2]), 10, 0)
+	ts, err := strconv.ParseFloat(string(fields[2]), 64)
 	if err != nil {
-		return empty, 0, 0, errTsNotTs
+		return fields[0], 0, 0, errTsNotTs
 	}
 
 	return fields[0], val, uint32(ts), nil
