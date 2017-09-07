72166d6913b378dca3d0d899e454ec374f2bb880
woodsaj <awoods@raintank.io>
2017-09-08 03:26:49 +0800

    refactor to use kafkaMdm input plugin
    - use the kafkaMdm input plugin to consume from kafka.  This consumes
      from explicit partitions, rather then using the high level
      sarama.cluster consumer groups
    - add metrics to track how things are running

--- cmd/mt-replicator-via-tsdb/metrics.go
+++ cmd/mt-replicator-via-tsdb/metrics.go
@@ -2,121 +2,88 @@ package main
 
 import (
 	"bytes"
-	"flag"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"strings"
 	"sync"
 	"time"
 
-	"github.com/Shopify/sarama"
-	"github.com/bsm/sarama-cluster"
 	"github.com/golang/snappy"
 	"github.com/jpillora/backoff"
+	"github.com/raintank/metrictank/cluster"
+	"github.com/raintank/metrictank/stats"
 	"github.com/raintank/worldping-api/pkg/log"
 	"gopkg.in/raintank/schema.v1"
 	"gopkg.in/raintank/schema.v1/msg"
 )
 
 var (
-	producerBatchSize = flag.Int("batch-size", 10000, "number of metrics to send in each batch.")
-	destinationURL    = flag.String("destination-url", "http://localhost/metrics", "tsdb-gw address to send metrics to")
-	destinationKey    = flag.String("destination-key", "admin-key", "admin-key of destination tsdb-gw server")
-
-	group                = flag.String("group", "mt-replicator", "Kafka consumer group")
-	clientID             = flag.String("client-id", "$HOSTNAME", "Kafka consumer group client id")
-	srcTopic             = flag.String("src-topic", "mdm", "metrics topic name on source cluster")
-	initialOffset        = flag.Int("initial-offset", -2, "initial offset to consume from. (-2=oldest, -1=newest)")
-	srcBrokerStr         = flag.String("src-brokers", "localhost:9092", "tcp address of source kafka cluster (may be be given multiple times as a comma-separated list)")
-	consumerFetchDefault = flag.Int("consumer-fetch-default", 32768, "number of bytes to try and fetch from consumer")
+	publishDuration  = stats.NewLatencyHistogram15s32("metrics.publish")
+	metricsPublished = stats.NewCounter32("metrics.published")
+	messagesSize     = stats.NewMeter32("metrics.message_size", false)
+	sendErrProducer  = stats.NewCounter32("metrics.send_error.producer")
 )
 
 type writeRequest struct {
-	data      []byte
-	count     int
-	offset    int64
-	topic     string
-	partition int32
+	data  []byte
+	count int
 }
 
 type MetricsReplicator struct {
-	consumer    *cluster.Consumer
-	tsdbClient  *http.Client
-	tsdbUrl     string
-	tsdbKey     string
-	writeQueue  chan *writeRequest
-	wg          sync.WaitGroup
-	shutdown    chan struct{}
-	flushBuffer chan []*schema.MetricData
+	tsdbClient    *http.Client
+	tsdbUrl       string
+	tsdbKey       string
+	writeQueue    map[int32]chan *writeRequest
+	wg            sync.WaitGroup
+	partitionChan map[int32]chan *schema.MetricData
 }
 
-func NewMetricsReplicator() (*MetricsReplicator, error) {
-	if *group == "" {
-		log.Fatal(4, "--group is required")
+func NewMetricsReplicator(destinationURL, destinationKey string) *MetricsReplicator {
+	r := &MetricsReplicator{
+		tsdbClient: &http.Client{
+			Timeout: time.Duration(10) * time.Second,
+		},
+		tsdbKey: destinationKey,
+		tsdbUrl: destinationURL,
 	}
 
-	if *srcBrokerStr == "" {
-		log.Fatal(4, "--src-brokers required")
-	}
-	if *srcTopic == "" {
-		log.Fatal(4, "--src-topic is required")
-	}
-
-	if *clientID == "$HOSTNAME" {
-		*clientID, _ = os.Hostname()
-	}
-
-	srcBrokers := strings.Split(*srcBrokerStr, ",")
-
-	config := cluster.NewConfig()
-	config.Consumer.Offsets.Initial = int64(*initialOffset)
-	config.ClientID = *clientID
-	config.Group.Return.Notifications = true
-	config.ChannelBufferSize = 1000
-	config.Consumer.Fetch.Min = 1
-	config.Consumer.Fetch.Default = int32(*consumerFetchDefault)
-	config.Consumer.MaxWaitTime = time.Second
-	config.Consumer.MaxProcessingTime = time.Second * time.Duration(5)
-	config.Config.Version = sarama.V0_10_0_0
-
-	err := config.Validate()
-	if err != nil {
-		return nil, err
-	}
-	consumer, err := cluster.NewConsumer(srcBrokers, *group, []string{*srcTopic}, config)
-	if err != nil {
-		log.Error(3, "failed to connect to source brokers %v.", srcBrokers)
-		return nil, err
-	}
-	tsdbClient := &http.Client{
-		Timeout: time.Duration(10) * time.Second,
-	}
-
-	return &MetricsReplicator{
-		consumer:   consumer,
-		tsdbClient: tsdbClient,
-		tsdbKey:    *destinationKey,
-		tsdbUrl:    *destinationURL,
-
-		shutdown:   make(chan struct{}),
-		writeQueue: make(chan *writeRequest, 10),
-	}, nil
+	return r
 }
 
 func (r *MetricsReplicator) Start() {
-	r.wg.Add(1)
-	go r.consume()
-	r.wg.Add(1)
-	go r.flush()
+	partitionChan := make(map[int32]chan *schema.MetricData)
+	writeQueue := make(map[int32]chan *writeRequest)
+	for _, part := range cluster.Manager.GetPartitions() {
+		partitionChan[part] = make(chan *schema.MetricData)
+		writeQueue[part] = make(chan *writeRequest, 10)
+	}
+
+	r.partitionChan = partitionChan
+	r.writeQueue = writeQueue
+
+	// as our goroutines read r.partitionChan and r.writeQueue we
+	// start them after these maps have been populated completely.
+	// Otherwise we would need to add synchronization to prevent
+	// concurrent read/writes.
+	for _, part := range cluster.Manager.GetPartitions() {
+		r.wg.Add(1)
+		go r.consume(part)
+		r.wg.Add(1)
+		go r.flush(part)
+	}
+
 }
 
 func (r *MetricsReplicator) Stop() {
-	r.consumer.Close()
-	close(r.shutdown)
+	for _, ch := range r.partitionChan {
+		// close consumer channels.  This will cause the consume() goroutines to end
+		// which will inturn cause the writeQueue chan to be closed leading to the
+		// flush() goroutines to end.
+		close(ch)
+	}
 	log.Info("Consumer closed.")
 	done := make(chan struct{})
 	go func() {
+		// wait for all of our goroutines (consume() and flush()) to end.
 		r.wg.Wait()
 		close(done)
 	}()
@@ -130,60 +97,54 @@ func (r *MetricsReplicator) Stop() {
 	return
 }
 
-func (r *MetricsReplicator) consume() {
-	buf := make([]*schema.MetricData, 0)
-	accountingTicker := time.NewTicker(time.Second * 10)
-	flushTicker := time.NewTicker(time.Second)
-	counter := 0
-	counterTs := time.Now()
-	msgChan := r.consumer.Messages()
-	notificationsChan := r.consumer.Notifications()
+func (r *MetricsReplicator) Process(metric *schema.MetricData, partition int32) {
+	// write the metric to the channel for this partition.  This map of chans is created
+	// at startup and never modified again, so it is safe to read without synchonization.
+	r.partitionChan[partition] <- metric
+}
 
-	flush := func(topic string, partition int32, offset int64) {
+func (r *MetricsReplicator) consume(partition int32) {
+	ch := r.partitionChan[partition]
+	flushTicker := time.NewTicker(time.Second)
+	buf := make([]*schema.MetricData, 0)
+
+	flush := func() {
 		mda := schema.MetricDataArray(buf)
 		data, err := msg.CreateMsg(mda, 0, msg.FormatMetricDataArrayMsgp)
 		if err != nil {
-			panic(err)
+			log.Fatal(4, err.Error())
 		}
 		// this will block when the writeQueue fills up. This will happen if
 		// we are consuming at a faster rate then we can publish, or if publishing
 		// is failing for some reason.
-		r.writeQueue <- &writeRequest{
-			data:      data,
-			topic:     topic,
-			partition: partition,
-			offset:    offset,
-			count:     len(buf),
+		r.writeQueue[partition] <- &writeRequest{
+			data:  data,
+			count: len(buf),
 		}
-		counter += len(buf)
 		buf = buf[:0]
 	}
 
-	var m *sarama.ConsumerMessage
+	var md *schema.MetricData
 	var ok bool
 	defer func() {
-		close(r.writeQueue)
+		close(r.writeQueue[partition])
 		r.wg.Done()
+		log.Info("consumer for partition %d has stopped", partition)
 	}()
+	log.Info("consumer for partition %d has started", partition)
 	for {
 		select {
-		case m, ok = <-msgChan:
+		case md, ok = <-ch:
 			if !ok {
+				// input chan has been closed.
 				if len(buf) != 0 {
-					flush(m.Topic, m.Partition, m.Offset)
+					flush()
 				}
 				return
 			}
-			md := &schema.MetricData{}
-			_, err := md.UnmarshalMsg(m.Value)
-			if err != nil {
-				log.Error(3, "kafka-mdm decode error, skipping message. %s", err)
-				continue
-			}
-
 			buf = append(buf, md)
 			if len(buf) > *producerBatchSize {
-				flush(m.Topic, m.Partition, m.Offset)
+				flush()
 				// reset our ticker
 				flushTicker.Stop()
 				flushTicker = time.NewTicker(time.Second)
@@ -192,41 +153,13 @@ func (r *MetricsReplicator) consume() {
 			if len(buf) == 0 {
 				continue
 			}
-			flush(m.Topic, m.Partition, m.Offset)
-		case t := <-accountingTicker.C:
-			log.Info("%d metrics processed in last %.1fseconds.", counter, t.Sub(counterTs).Seconds())
-			counter = 0
-			counterTs = t
-		case <-r.shutdown:
-			if len(buf) > 0 {
-				flush(m.Topic, m.Partition, m.Offset)
-			}
-			return
-		case msg := <-notificationsChan:
-			if len(msg.Claimed) > 0 {
-				for topic, partitions := range msg.Claimed {
-					log.Info("kafka consumer claimed %d partitions on topic: %s", len(partitions), topic)
-				}
-			}
-			if len(msg.Released) > 0 {
-				for topic, partitions := range msg.Released {
-					log.Info("kafka consumer released %d partitions on topic: %s", len(partitions), topic)
-				}
-			}
-
-			if len(msg.Current) == 0 {
-				log.Info("kafka consumer is no longer consuming from any partitions.")
-			} else {
-				log.Info("kafka Current partitions:")
-				for topic, partitions := range msg.Current {
-					log.Info("kafka Current partitions: %s: %v", topic, partitions)
-				}
-			}
+			flush()
 		}
 	}
 }
 
-func (r *MetricsReplicator) flush() {
+func (r *MetricsReplicator) flush(partition int32) {
+	q := r.writeQueue[partition]
 	b := &backoff.Backoff{
 		Min:    100 * time.Millisecond,
 		Max:    time.Minute,
@@ -235,7 +168,7 @@ func (r *MetricsReplicator) flush() {
 	}
 	body := new(bytes.Buffer)
 	defer r.wg.Done()
-	for wr := range r.writeQueue {
+	for wr := range q {
 		for {
 			pre := time.Now()
 			body.Reset()
@@ -250,15 +183,16 @@ func (r *MetricsReplicator) flush() {
 			resp, err := r.tsdbClient.Do(req)
 			diff := time.Since(pre)
 			if err == nil && resp.StatusCode >= 200 && resp.StatusCode < 300 {
-				// the payload has been successfully sent so lets mark our offset
-				r.consumer.MarkPartitionOffset(wr.topic, wr.partition, wr.offset, "")
-
 				b.Reset()
-				log.Info("GrafanaNet sent %d metrics in %s -msg size %d", wr.count, diff, body.Len())
+				log.Debug("GrafanaNet sent %d metrics in %s -msg size %d", wr.count, diff, body.Len())
 				resp.Body.Close()
 				ioutil.ReadAll(resp.Body)
+				publishDuration.Value(time.Since(pre))
+				metricsPublished.Add(wr.count)
+				messagesSize.Value(int(req.ContentLength))
 				break
 			}
+			sendErrProducer.Inc()
 			dur := b.Duration()
 			if err != nil {
 				log.Warn("GrafanaNet failed to submit data: %s will try again in %s (this attempt took %s)", err, dur, diff)
