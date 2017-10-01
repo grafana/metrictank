4db74f137693f39a4b5e317ccc36281df2d8caf2
Mauro Stettler <mauro.stettler@gmail.com>
2017-10-01 16:22:38 +0200

    reorganize tests

--- idx/memory/tag_query_test.go
+++ idx/memory/tag_query_test.go
@@ -1,11 +1,13 @@
 package memory
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/raintank/metrictank/idx"
+	"gopkg.in/raintank/schema.v1"
 )
 
 func getTestIndex() (TagIndex, map[string]*idx.Archive) {
@@ -109,5 +111,136 @@ func TestQueryByTagInvalidQuery(t *testing.T) {
 	_, err := NewTagQuery([]string{"key!=value1"})
 	if err != errInvalidQuery {
 		t.Fatalf("Expected an error, but didn't get it")
+	}
+}
+
+func TestGetByTag(t *testing.T) {
+	tagSupport := TagSupport
+	defer func() { TagSupport = tagSupport }()
+	TagSupport = true
+
+	ix := New()
+	ix.Init()
+
+	mds := make([]schema.MetricData, 20)
+	for i := range mds {
+		mds[i].Metric = fmt.Sprintf("metric.%d", i)
+		mds[i].Name = mds[i].Metric
+		mds[i].Id = mds[i].Metric
+		mds[i].OrgId = 1
+		mds[i].Interval = 1
+	}
+	mds[1].Tags = []string{"key1=value1", "key2=value2"}
+	mds[11].Tags = []string{"key1=value1"}
+	mds[18].Tags = []string{"key1=value2", "key2=value2"}
+	mds[3].Tags = []string{"key1=value1", "key3=value3"}
+
+	for _, md := range mds {
+		ix.AddOrUpdate(&md, 1)
+	}
+
+	type testCase struct {
+		expressions []string
+		expectation []string
+	}
+
+	testCases := []testCase{
+		{
+			expressions: []string{"key1=value1"},
+			expectation: []string{"metric.1", "metric.11", "metric.3"},
+		}, {
+			expressions: []string{"key1=value2"},
+			expectation: []string{"metric.18"},
+		}, {
+			expressions: []string{"key1=~value[0-9]"},
+			expectation: []string{"metric.1", "metric.11", "metric.18", "metric.3"},
+		}, {
+			expressions: []string{"key1=~value[23]"},
+			expectation: []string{"metric.18"},
+		}, {
+			expressions: []string{"key1=value1", "key2=value1"},
+			expectation: []string{},
+		}, {
+			expressions: []string{"key1=value1", "key2=value2"},
+			expectation: []string{"metric.1"},
+		}, {
+			expressions: []string{"key1=~value[12]", "key2=value2"},
+			expectation: []string{"metric.1", "metric.18"},
+		}, {
+			expressions: []string{"key1=~value1", "key1=value2"},
+			expectation: []string{},
+		}, {
+			expressions: []string{"key1=~value[0-9]", "key2=~", "key3!=value3"},
+			expectation: []string{"metric.11"},
+		}, {
+			expressions: []string{"key2=", "key1=value1"},
+			expectation: []string{"metric.11", "metric.3"},
+		},
+	}
+
+	for _, tc := range testCases {
+		tagQuery, err := NewTagQuery(tc.expressions)
+		if err != nil {
+			t.Fatalf("Got an unexpected error with query %s: %s", tc.expressions, err)
+		}
+		res := ix.IdsByTagQuery(1, tagQuery)
+		if len(res) != len(tc.expectation) {
+			t.Fatalf("Result does not match expectation for expressions %+v\nGot:\n%+v\nExpected:\n%+v\n", tc.expressions, res, tc.expectation)
+		}
+		expectationMap := make(map[string]struct{})
+		for _, v := range tc.expectation {
+			expectationMap[v] = struct{}{}
+		}
+		if !reflect.DeepEqual(res, expectationMap) {
+			t.Fatalf("Result does not match expectation\nGot:\n%+v\nExpected:\n%+v\n", res, tc.expectation)
+		}
+	}
+}
+
+func TestDeleteTaggedSeries(t *testing.T) {
+	tagSupport := TagSupport
+	defer func() { TagSupport = tagSupport }()
+	TagSupport = true
+
+	ix := New()
+	ix.Init()
+
+	orgId := 1
+
+	mds := getMetricData(orgId, 2, 50, 10, "metric.public")
+	mds[10].Tags = []string{"key1=value1", "key2=value2"}
+
+	for _, md := range mds {
+		ix.AddOrUpdate(md, 1)
+	}
+
+	tagQuery, _ := NewTagQuery([]string{"key1=value1", "key2=value2"})
+	res := ix.IdsByTagQuery(orgId, tagQuery)
+
+	if len(res) != 1 {
+		t.Fatalf("Expected to get 1 result, but got %d", len(res))
+	}
+
+	if len(ix.Tags[orgId]) != 2 {
+		t.Fatalf("Expected tag index to contain 2 keys, but it does not: %+v", ix.Tags)
+	}
+
+	deleted, err := ix.Delete(orgId, mds[10].Metric)
+	if err != nil {
+		t.Fatalf("Error deleting metric: %s", err)
+	}
+
+	if len(deleted) != 1 {
+		t.Fatalf("Expected 1 metric to get deleted, but got %d", len(deleted))
+	}
+
+	res = ix.IdsByTagQuery(orgId, tagQuery)
+
+	if len(res) != 0 {
+		t.Fatalf("Expected to get 0 results, but got %d", len(res))
+	}
+
+	if len(ix.Tags[orgId]) > 0 {
+		t.Fatalf("Expected tag index to be empty, but it is not: %+v", ix.Tags)
 	}
 }
