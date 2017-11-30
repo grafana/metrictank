0f4f39b33ba48ad52968cea692218ce452779e30
Sean Hanson <shanson7@bloomberg.net>
2017-11-30 15:53:06 -0500

    Move logic fo parsing and building series tags into Series struct

--- expr/func_get.go
+++ expr/func_get.go
@@ -1,8 +1,6 @@
 package expr
 
 import (
-	"strings"
-
 	"github.com/grafana/metrictank/api/models"
 )
 
@@ -26,30 +24,9 @@ func (s FuncGet) Context(context Context) Context {
 func (s FuncGet) Exec(cache map[Req][]models.Series) ([]models.Series, error) {
 	series := cache[s.req]
 
-	if len(series) == 0 {
-		return series, nil
+	for k := range series {
+		series[k].SetTags()
 	}
 
-	var out []models.Series
-
-	for _, serie := range series {
-		tagSplits := strings.Split(serie.Target, ";")
-
-		if serie.Tags == nil {
-			serie.Tags = make(map[string]string, len(tagSplits))
-		}
-
-		for _, tagPair := range tagSplits[1:] {
-			parts := strings.SplitN(tagPair, "=", 2)
-			if len(parts) != 2 {
-				// Shouldn't happen
-				continue
-			}
-			serie.Tags[parts[0]] = parts[1]
-		}
-		serie.Tags["name"] = tagSplits[0]
-		out = append(out, serie)
-	}
-
-	return out, nil
+	return series, nil
 }
