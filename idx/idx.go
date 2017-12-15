ed78e7baab25c8eedd6785f7d4c3db63ada07c9a
Mauro Stettler <mauro.stettler@gmail.com>
2017-12-15 16:15:45 +0900

    add comments

--- idx/idx.go
+++ idx/idx.go
@@ -147,7 +147,15 @@ type MetricIndex interface {
 	// LastUpdate time is >= the given value.
 	Tags(int, string, int64) ([]string, error)
 
+	// AutoCompleteTags is used to generate a list of possible values that could
+	// complete a given prefix. It also accepts additional conditions to further
+	// narrow down the result set.
 	AutoCompleteTags(int, string, []string, int64, uint) ([]string, error)
+
+	// AutoCompleteTagValues is used to generate a list of possible values that could
+	// complete a given value prefix. It requires a tag to be specified and only values
+	// of the given tag will be returned. It also accepts additional conditions to
+	// further narrow down the result set.
 	AutoCompleteTagValues(int, string, string, []string, int64, uint) ([]string, error)
 
 	// TagDetails returns a list of all values associated with a given tag key in the
