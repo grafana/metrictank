e5a344d3cdf2e6182d455f2bfa97dc276ff9adef
Dieter Plaetinck <dieter@grafana.com>
2017-12-26 16:11:13 +0100

    more info re ineffassign not yet working in circleCI

--- scripts/qa/ineffassign.sh
+++ scripts/qa/ineffassign.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-#TODO for some reason this doesn't work on circleci :+?
+
+# TODO for some reason this doesn't work on circleci :+?
+# it doesn't find the problem on line 64 of idx.go
+
 # find the dir we exist within...
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # and cd into root project dir
+echo $DIR
 cd ${DIR}/../..
-go get -u github.com/gordonklaus/ineffassign
-ineffassign .
+pwd
+go get -u -v github.com/dgnorton/ineffassign
+cat -n idx/idx.go
+echo "running now"
+ineffassign -v .
+rt=$?
+echo "return code is $rt"
+exit $rt
