c72cb5feeca1c815ad040d0f928c1772ab08e143
Sean Hanson <shanson7@bloomberg.net>
2017-12-28 13:29:49 -0500

    Put expected first in test error message

--- expr/func_groupbytags_test.go
+++ expr/func_groupbytags_test.go
@@ -211,7 +211,7 @@ func testGroupByTags(name string, in []models.Series, out []models.Series, agg s
 		if expectedErr == nil {
 			t.Fatalf("case %q: expected no error but got %q", name, err)
 		} else if err == nil || err.Error() != expectedErr.Error() {
-			t.Fatalf("case %q: err %q but expected %q", name, err, expectedErr)
+			t.Fatalf("case %q: expected error %q but got %q", name, err, expectedErr)
 		}
 	}
 	if len(got) != len(out) {
