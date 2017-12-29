d0316eff841ac3b3a5d1a97ae0cd58d659804c78
Dieter Plaetinck <dieter@grafana.com>
2017-12-29 22:09:47 +0100

    circleCi 2.0

--- scripts/depends.sh
+++ scripts/depends.sh
@@ -4,20 +4,6 @@ set -x
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-: ${GOPATH:="${HOME}/.go_workspace"}
-: ${ORG_PATH:="github.com/grafana"}
-: ${REPO_PATH:="${ORG_PATH}/metrictank"}
-
-if [ ! -z ${CIRCLECI} ] ; then
-  : ${CHECKOUT:="/home/ubuntu/${CIRCLE_PROJECT_REPONAME}"}
-else
-  : ${CHECKOUT:="${DIR}/.."}
-fi
-
-export GOPATH
-
+sudo apt install rubygems ruby-dev
+sudo gem install bundler
 bundle install
-
-echo "Linking ${GOPATH}/src/${REPO_PATH} to ${CHECKOUT}"
-mkdir -p ${GOPATH}/src/${ORG_PATH}
-ln -s ${CHECKOUT} ${GOPATH}/src/${REPO_PATH}
