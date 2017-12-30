d364a37d1b51b4a69464926800466ad69295e697
Dieter Plaetinck <dieter@grafana.com>
2017-12-30 12:14:59 +0100

    script now works fine in circleCI

--- scripts/qa/ineffassign.sh
+++ scripts/qa/ineffassign.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-
-# TODO for some reason this doesn't work on circleci :+?
-# it doesn't find the problem on line 64 of idx.go
-
 # find the dir we exist within...
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # and cd into root project dir
-echo $DIR
 cd ${DIR}/../..
-pwd
-go get -u -v github.com/dgnorton/ineffassign
-cat -n idx/idx.go
-echo "running now"
-ineffassign -v .
-rt=$?
-echo "return code is $rt"
-exit $rt
+go get -u -v github.com/gordonklaus/ineffassign
+ineffassign .
