c43ff22ab6b06518ad182dcb909700632dda3397
Dieter Plaetinck <dieter@grafana.com>
2018-01-02 11:23:31 +0100

    use sep dirs for tmp build data
    this way we can upload all build artifacts without all the tmp stuff
    also separate dir for packages (because it seems a bit cleaner)

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -x
-BASE=$(dirname $0)
-CODE_DIR=$(readlink -e "$BASE/../")
+BASE=$(dirname $0) # points to scripts directory
+CODE_DIR=$(readlink -e "$BASE/../") # project root
+BUILD_ROOT=$CODE_DIR/build # should have all binaries already inside
+BUILD_PKG=$CODE_DIR/build_pkg # will place packages here
+BUILD_TMP=$CODE_DIR/build_tmp # used for temporary data used to construct the packages
+mkdir $BUILD_TMP
+mkdir $BUILD_PKG
 
 sudo apt-get update # otherwise the below install command doesn't work
 sudo apt-get install rpm # to be able to make rpms
-
-BUILD_ROOT=$CODE_DIR/build
 
 ARCH="$(uname -m)"
 VERSION=$(git describe --long --always)
 
 ## debian wheezy
-BUILD=${BUILD_ROOT}/sysvinit
+BUILD=${BUILD_TMP}/sysvinit
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/etc/metrictank
 
@@ -21,7 +24,7 @@ cp ${BASE}/config/storage-schemas.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD}/metrictank-${VERSION}_${ARCH}.deb"
+PACKAGE_NAME="${BUILD_PKG}/metrictank-${VERSION}_${ARCH}.deb"
 fpm -s dir -t deb \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --deb-init ${BASE}/config/sysvinit/init.d/metrictank \
@@ -32,7 +35,7 @@ fpm -s dir -t deb \
   -C ${BUILD} -p ${PACKAGE_NAME} .
 
 ## ubuntu 14.04
-BUILD=${BUILD_ROOT}/upstart
+BUILD=${BUILD_TMP}/upstart
 
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/etc/init
@@ -43,7 +46,7 @@ cp ${BASE}/config/storage-schemas.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD}/metrictank-${VERSION}_${ARCH}.deb"
+PACKAGE_NAME="${BUILD_PKG}/metrictank-${VERSION}_${ARCH}.deb"
 fpm -s dir -t deb \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --deb-upstart ${BASE}/config/upstart/metrictank \
@@ -53,7 +56,7 @@ fpm -s dir -t deb \
   -C ${BUILD} -p ${PACKAGE_NAME} .
 
 ## ubuntu 16.04, Debian 8, CentOS 7
-BUILD=${BUILD_ROOT}/systemd
+BUILD=${BUILD_TMP}/systemd
 
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/lib/systemd/system/
@@ -66,14 +69,14 @@ cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/systemd/metrictank.service $BUILD/lib/systemd/system/
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD}/metrictank-${VERSION}_${ARCH}.deb"
+PACKAGE_NAME="${BUILD_PKG}/metrictank-${VERSION}_${ARCH}.deb"
 fpm -s dir -t deb \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --config-files /etc/metrictank/ \
   -m "Raintank Inc. <hello@grafana.com>" --vendor "grafana.com" \
   --license "Apache2.0" -C ${BUILD} -p ${PACKAGE_NAME} .
 
-BUILD=${BUILD_ROOT}/systemd-centos7
+BUILD=${BUILD_TMP}/systemd-centos7
 
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/lib/systemd/system/
@@ -86,7 +89,7 @@ cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/systemd/metrictank.service $BUILD/lib/systemd/system/
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD}/metrictank-${VERSION}.el7.${ARCH}.rpm"
+PACKAGE_NAME="${BUILD_PKG}/metrictank-${VERSION}.el7.${ARCH}.rpm"
 fpm -s dir -t rpm \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --config-files /etc/metrictank/ \
@@ -94,7 +97,7 @@ fpm -s dir -t rpm \
   --license "Apache2.0" -C ${BUILD} -p ${PACKAGE_NAME} .
 
 ## CentOS 6
-BUILD=${BUILD_ROOT}/upstart-0.6.5
+BUILD=${BUILD_TMP}/upstart-0.6.5
 
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/etc/init
@@ -106,7 +109,7 @@ cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/upstart-0.6.5/metrictank.conf $BUILD/etc/init
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD}/metrictank-${VERSION}.el6.${ARCH}.rpm"
+PACKAGE_NAME="${BUILD_PKG}/metrictank-${VERSION}.el6.${ARCH}.rpm"
 fpm -s dir -t rpm \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --replaces metric-tank --provides metric-tank \
