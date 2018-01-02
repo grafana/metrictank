dc4038b2fe4bebe0a78ed76dac77fd8d09c1b723
Dieter Plaetinck <dieter@grafana.com>
2018-01-02 14:33:01 +0100

    bugfix: create pkg dirs

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -14,17 +14,21 @@ sudo apt-get install rpm # to be able to make rpms
 ARCH="$(uname -m)"
 VERSION=$(git describe --long --always)
 
-## debian wheezy
+
+## debian wheezy ##
+
 BUILD=${BUILD_TMP}/sysvinit
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/etc/metrictank
+PKG=${BUILD_PKG}/sysvinit
+mkdir -p ${PKG}
 
 cp ${BASE}/config/metrictank-package.ini ${BUILD}/etc/metrictank/metrictank.ini
 cp ${BASE}/config/storage-schemas.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD_PKG}/sysvinit/metrictank-${VERSION}_${ARCH}.deb"
+PACKAGE_NAME="${PKG}/metrictank-${VERSION}_${ARCH}.deb"
 fpm -s dir -t deb \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --deb-init ${BASE}/config/sysvinit/init.d/metrictank \
@@ -34,19 +38,22 @@ fpm -s dir -t deb \
   --config-files /etc/metrictank/ \
   -C ${BUILD} -p ${PACKAGE_NAME} .
 
-## ubuntu 14.04
-BUILD=${BUILD_TMP}/upstart
 
+## ubuntu 14.04 ##
+
+BUILD=${BUILD_TMP}/upstart
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/etc/init
 mkdir -p ${BUILD}/etc/metrictank
+PKG=${BUILD_PKG}/upstart
+mkdir -p ${PKG}
 
 cp ${BASE}/config/metrictank-package.ini ${BUILD}/etc/metrictank/metrictank.ini
 cp ${BASE}/config/storage-schemas.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD_PKG}/upstart/metrictank-${VERSION}_${ARCH}.deb"
+PACKAGE_NAME="${PKG}/metrictank-${VERSION}_${ARCH}.deb"
 fpm -s dir -t deb \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --deb-upstart ${BASE}/config/upstart/metrictank \
@@ -55,13 +62,16 @@ fpm -s dir -t deb \
   --config-files /etc/metrictank/ \
   -C ${BUILD} -p ${PACKAGE_NAME} .
 
-## ubuntu 16.04, Debian 8, CentOS 7
-BUILD=${BUILD_TMP}/systemd
 
+## ubuntu 16.04, Debian 8, CentOS 7 ##
+
+BUILD=${BUILD_TMP}/systemd
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/lib/systemd/system/
 mkdir -p ${BUILD}/etc/metrictank
 mkdir -p ${BUILD}/var/run/metrictank
+PKG=${BUILD_PKG}/systemd
+mkdir -p ${PKG}
 
 cp ${BASE}/config/metrictank-package.ini ${BUILD}/etc/metrictank/metrictank.ini
 cp ${BASE}/config/storage-schemas.conf ${BUILD}/etc/metrictank/
@@ -69,19 +79,23 @@ cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/systemd/metrictank.service $BUILD/lib/systemd/system/
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD_PKG}/systemd/metrictank-${VERSION}_${ARCH}.deb"
+PACKAGE_NAME="${PKG}/metrictank-${VERSION}_${ARCH}.deb"
 fpm -s dir -t deb \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --config-files /etc/metrictank/ \
   -m "Raintank Inc. <hello@grafana.com>" --vendor "grafana.com" \
   --license "Apache2.0" -C ${BUILD} -p ${PACKAGE_NAME} .
 
-BUILD=${BUILD_TMP}/systemd-centos7
 
+## centos 7 ##
+
+BUILD=${BUILD_TMP}/systemd-centos7
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/lib/systemd/system/
 mkdir -p ${BUILD}/etc/metrictank
 mkdir -p ${BUILD}/var/run/metrictank
+PKG=${BUILD_PKG}/systemd-centos7
+mkdir -p ${PKG}
 
 cp ${BASE}/config/metrictank-package.ini ${BUILD}/etc/metrictank/metrictank.ini
 cp ${BASE}/config/storage-schemas.conf ${BUILD}/etc/metrictank/
@@ -89,19 +103,22 @@ cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/systemd/metrictank.service $BUILD/lib/systemd/system/
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD_PKG}/systemd-centos7/metrictank-${VERSION}.el7.${ARCH}.rpm"
+PACKAGE_NAME="${PKG}/metrictank-${VERSION}.el7.${ARCH}.rpm"
 fpm -s dir -t rpm \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --config-files /etc/metrictank/ \
   -m "Raintank Inc. <hello@grafana.com>" --vendor "grafana.com" \
   --license "Apache2.0" -C ${BUILD} -p ${PACKAGE_NAME} .
 
-## CentOS 6
-BUILD=${BUILD_TMP}/upstart-0.6.5
 
+## CentOS 6 ##
+
+BUILD=${BUILD_TMP}/upstart-0.6.5
 mkdir -p ${BUILD}/usr/sbin
 mkdir -p ${BUILD}/etc/init
 mkdir -p ${BUILD}/etc/metrictank
+PKG=${BUILD_PKG}/upstart-0.6.5
+mkdir -p ${PKG}
 
 cp ${BASE}/config/metrictank-package.ini ${BUILD}/etc/metrictank/metrictank.ini
 cp ${BASE}/config/storage-schemas.conf ${BUILD}/etc/metrictank/
@@ -109,7 +126,7 @@ cp ${BASE}/config/storage-aggregation.conf ${BUILD}/etc/metrictank/
 cp ${BASE}/config/upstart-0.6.5/metrictank.conf $BUILD/etc/init
 cp ${BUILD_ROOT}/{metrictank,mt-*} ${BUILD}/usr/sbin/
 
-PACKAGE_NAME="${BUILD_PKG}/upstart-0.6.5/metrictank-${VERSION}.el6.${ARCH}.rpm"
+PACKAGE_NAME="${PKG}/metrictank-${VERSION}.el6.${ARCH}.rpm"
 fpm -s dir -t rpm \
   -v ${VERSION} -n metrictank -a ${ARCH} --description "metrictank, the gorilla-inspired timeseries database backend for graphite" \
   --replaces metric-tank --provides metric-tank \
