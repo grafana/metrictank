221a1f6bb63a7d91706c4837819a5ab4f65e94dd
Dieter Plaetinck <dieter@grafana.com>
2018-01-30 12:34:20 +0100

    once new gcr image is ready, tell qa cluster to run extra checks

---
+++ scripts/push/extra-qa.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+log () {
+        echo "$(date +'%Y/%m/%d %H:%M:%S') $@"
+}
+
+VERSION=`git describe --always`
+
+# try returns if did not succeed and is retryable
+# exits in all other cases
+try () {
+	log "trying to push $VERSION to qa server"
+	res=$(curl -q https://mt-qa.grafana.net/try -d secret=$MTQA_SECRET -d version=$VERSION)
+	echo $res
+	if grep -q '^OK$' <<< "$res"; then
+		exit 0
+	fi
+	if ! grep -q 'already has an active deployment, please try again' <<< "$res"; then
+		echo "error occurred and not sure how to handle"
+		exit 2
+	fi
+}
+
+n=0
+until [ $n -ge 6 ]
+do
+      try
+      n=$[$n+1]
+      sleep 10
+done
+
+log "giving up"
+exit 2
