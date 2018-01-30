c8d10ef68b493c69092fbc06083c68623d3f007e
Dieter Plaetinck <dieter@grafana.com>
2018-01-30 19:46:40 +0100

    better docker push logging + exit failure if first cmd fails

--- scripts/push/dockerhub.sh
+++ scripts/push/dockerhub.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -x
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}/../..
 
 VERSION=`git describe --always`
 
-docker push grafana/metrictank:$VERSION
+echo docker push grafana/metrictank:$VERSION
+docker push grafana/metrictank:$VERSION || exit 2
+echo docker push grafana/metrictank:latest
 docker push grafana/metrictank:latest
--- scripts/push/gcr.sh
+++ scripts/push/gcr.sh
@@ -6,5 +6,7 @@ cd ${DIR}/../..
 VERSION=`git describe --always`
 
 docker login -u _json_key -p "$GOOGLE_AUTH" https://us.gcr.io
+echo docker push us.gcr.io/metrictank-gcr/metrictank:$VERSION
+docker push us.gcr.io/metrictank-gcr/metrictank:$VERSION || exit 2
+echo docker push us.gcr.io/metrictank-gcr/metrictank:latest
 docker push us.gcr.io/metrictank-gcr/metrictank:latest
-docker push us.gcr.io/metrictank-gcr/metrictank:$VERSION
