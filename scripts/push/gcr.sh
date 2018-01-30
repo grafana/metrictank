f67f9384583b40d741ea8752fcbde8f657048b8c
Dieter Plaetinck <dieter@grafana.com>
2018-01-30 12:33:40 +0100

    use key directly

--- scripts/push/gcr.sh
+++ scripts/push/gcr.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -x
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}/../..
 
 VERSION=`git describe --always`
 
-docker login -u _json_key -p "$(cat $HOME/gcp-key.json)" https://us.gcr.io
+docker login -u _json_key -p "$GOOGLE_AUTH" https://us.gcr.io
 docker push us.gcr.io/metrictank-gcr/metrictank:$VERSION
