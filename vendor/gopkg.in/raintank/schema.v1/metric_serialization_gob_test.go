df850f7b3d3a90bc6ad70906b10cbb73598c02f6
Dieter Plaetinck <dieter@grafana.com>
2018-03-13 14:17:47 +0100

    refactor benchmarks
    * split different serializers into different files for easy file diffing
    * follow benchmark best practice of having each iteration represent 1
      metric to be serialized and rely on automated conversion instead of
      hardcoded amount of 3000
    * show size on per-metric basis, which is more useful
    * simplify error handling

---
+++ vendor/gopkg.in/raintank/schema.v1/metric_serialization_gob_test.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"bytes"
+	"encoding/gob"
+	"testing"
+)
+
+func BenchmarkSerializeMetricDataArrayGob(b *testing.B) {
+	metrics := getDifferentMetrics(b.N)
+	b.ResetTimer()
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(metrics)
+	checkErr(b, err)
+	b.Logf("with %10d metrics -> final size: %.1f bytes per metric", b.N, float64(buf.Len())/float64(b.N))
+}
+func BenchmarkDeSerializeMetricDataArrayGob(b *testing.B) {
+	metrics := getDifferentMetrics(b.N)
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(metrics)
+	checkErr(b, err)
+	var out []*MetricData
+	b.ResetTimer()
+	dec := gob.NewDecoder(&buf)
+	err = dec.Decode(&out)
+	checkErr(b, err)
+}
