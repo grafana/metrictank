e217ec5f3ab51a8bddfeedcef57e75aed2fddc63
Dieter Plaetinck <dieter@grafana.com>
2018-03-18 13:53:41 +0100

    input: use MKey

--- input/input.go
+++ input/input.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"time"
 
+	schema "gopkg.in/raintank/schema.v1"
+
 	"github.com/grafana/metrictank/idx"
 	"github.com/grafana/metrictank/mdata"
 	"github.com/grafana/metrictank/msg"
@@ -48,6 +50,7 @@ func NewDefaultHandler(metrics mdata.Metrics, metricIndex idx.MetricIndex, input
 // concurrency-safe.
 func (in DefaultHandler) Process(pointMsg msg.Point, partition int32) {
 	in.metricsReceived.Inc()
+	var mkey schema.MKey
 	var timestamp uint32
 	var value float64
 
@@ -55,13 +58,18 @@ func (in DefaultHandler) Process(pointMsg msg.Point, partition int32) {
 		err := pointMsg.Md.Validate()
 		if err != nil {
 			in.MetricInvalid.Inc()
-			log.Debug("in: Invalid metric %s %v", err, pointMsg.Md)
+			log.Debug("in: Invalid metric %v: %s", pointMsg.Md, err)
 			return
 		}
 		if pointMsg.Md.Time == 0 {
 			in.MetricInvalid.Inc()
 			log.Warn("in: invalid metric. metric.Time is 0. %s", pointMsg.Md.Id)
 			return
+		}
+
+		mkey, err = schema.MKeyFromString(pointMsg.Md.Id)
+		if err != nil {
+			log.Debug("in: Invalid metric %v: %s", pointMsg.Md, err)
 		}
 
 		timestamp = uint32(pointMsg.Md.Time)
@@ -71,6 +79,10 @@ func (in DefaultHandler) Process(pointMsg msg.Point, partition int32) {
 			in.MetricInvalid.Inc()
 			log.Debug("in: Invalid metric %v", pointMsg.Point)
 			return
+		}
+		mkey = schema.MKey{
+			Key: pointMsg.Point.MetricPointId1.Id,
+			Org: pointMsg.Point.Org,
 		}
 		timestamp = pointMsg.Point.MetricPointId1.Time
 		value = pointMsg.Point.MetricPointId1.Value
@@ -82,7 +94,7 @@ func (in DefaultHandler) Process(pointMsg msg.Point, partition int32) {
 	in.pressureIdx.Add(int(time.Since(pre).Nanoseconds()))
 
 	pre = time.Now()
-	m := in.metrics.GetOrCreate(key, archive.SchemaId, archive.AggId)
+	m := in.metrics.GetOrCreate(mkey, archive.SchemaId, archive.AggId)
 	m.Add(timestamp, value)
 	in.pressureTank.Add(int(time.Since(pre).Nanoseconds()))
 }
