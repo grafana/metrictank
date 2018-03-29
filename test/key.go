d7724be5c67e449ab85fb59a34734975aafee373
Dieter Plaetinck <dieter@grafana.com>
2018-03-29 10:10:56 +0200

    update test.Get[A]Mkey to support large ints
    we will need this in benchmarks etc

--- test/key.go
+++ test/key.go
@@ -2,14 +2,15 @@ package test
 
 import schema "gopkg.in/raintank/schema.v1"
 
-func GetAMKey(suffix uint8) schema.AMKey {
+func GetAMKey(suffix int) schema.AMKey {
 	return schema.AMKey{
 		MKey: GetMKey(suffix),
 	}
 }
 
-func GetMKey(suffix uint8) schema.MKey {
+func GetMKey(suffix int) schema.MKey {
+	s := uint32(suffix)
 	return schema.MKey{
-		Key: [16]byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, suffix},
+		Key: [16]byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, byte(s >> 24), byte(s >> 16), byte(s >> 8), byte(s)},
 	}
 }
