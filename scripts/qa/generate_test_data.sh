c948cad4bb546f15e17dad3c8879d8094e6cff2a
Dieter Plaetinck <dieter@grafana.com>
2018-04-11 09:28:21 +0300

    update fakemetrics call

--- scripts/qa/generate_test_data.sh
+++ scripts/qa/generate_test_data.sh
@@ -22,7 +22,7 @@ case "$ACTION" in
   "start")
     go get $FAKEMETRICS_REPO
     echo "generating test data..."
-    fakemetrics -keys-per-org $METRICS_PER_SEC -orgs 1 --carbon-tcp-address $HOST:$PORT &
+    fakemetrics feed --carbon-addr $HOST:$PORT --orgs 1 --mpo $METRICS_PER_SEC &
     echo $! > $FAKEMETRICS_PID
     ;;
   "stop")
