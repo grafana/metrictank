54ee5f1810a6ff41eb0591d7b064b2bd6b50a725
Mauro Stettler <mauro.stettler@gmail.com>
2018-04-18 11:44:45 -0400

    take advantage of docker image layer cache

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,5 +1,9 @@
-# build librdkafka and export the according pkg config so it will be found
+#!/bin/bash
 
+set -x
+set -e
+
+# build librdkafka and export the according pkg config so it will be found
 if [ -z $TMP_DIR ]
 then
   TMP_DIR=$(mktemp -d)
@@ -29,15 +33,18 @@ fi
 
 cd $OLDPWD
 
-# cleanup
-rm \
-	vendor/github.com/edenhill/librdkafka/Makefile.config \
-	vendor/github.com/edenhill/librdkafka/config.cache \
-	vendor/github.com/edenhill/librdkafka/config.h \
-	vendor/github.com/edenhill/librdkafka/config.log \
-	vendor/github.com/edenhill/librdkafka/config.log.old
+if [ "${NOCLEANUP}" != "yes" ]
+then
+	# cleanup
+	rm \
+		vendor/github.com/edenhill/librdkafka/Makefile.config \
+		vendor/github.com/edenhill/librdkafka/config.cache \
+		vendor/github.com/edenhill/librdkafka/config.h \
+		vendor/github.com/edenhill/librdkafka/config.log \
+		vendor/github.com/edenhill/librdkafka/config.log.old
 
-# this file gets modified when configuring the package, which then causes
-# tests to fail because it differs from what's committed in git. so we check
-# it out to not make the tests fail.
-git checkout vendor/github.com/edenhill/librdkafka/CONFIGURATION.md
+	# this file gets modified when configuring the package, which then causes
+	# tests to fail because it differs from what's committed in git. so we check
+	# it out to not make the tests fail.
+	git checkout vendor/github.com/edenhill/librdkafka/CONFIGURATION.md
+fi
