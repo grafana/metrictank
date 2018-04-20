a86afedf1703111407a0066727849bb56c8659c5
Mauro Stettler <mauro.stettler@gmail.com>
2018-04-20 13:05:40 -0400

    add test for 3600

--- vendor/gopkg.in/raintank/schema.v1/archive_test.go
+++ vendor/gopkg.in/raintank/schema.v1/archive_test.go
@@ -12,6 +12,7 @@ func TestArchive(t *testing.T) {
 	cases := []c{
 		{Avg, 15, 0x31, "avg_15"},
 		{Cnt, 120, 0x76, "cnt_120"},
+		{Cnt, 3600, 0xF6, "cnt_3600"},
 	}
 	for i, cas := range cases {
 		arch := NewArchive(cas.method, cas.span)
