0fe13dbe4da1f41d44c02a4fa41c0849891a0680
Dieter Plaetinck <dieter@grafana.com>
2018-06-12 14:27:21 +0200

    optimize AddRange
    taskset --cpu-list 5 go test -bench BenchmarkAdd . -benchmem
    goos: linux
    goarch: amd64
    pkg: github.com/grafana/metrictank/mdata/cache
    BenchmarkAddAsc         	   10000	   1223221 ns/op	   21684 B/op	       5 allocs/op
    BenchmarkAddDesc1       	   10000	   1451440 ns/op	   21720 B/op	       7 allocs/op
    BenchmarkAddDesc4       	   10000	   1458588 ns/op	   21729 B/op	       9 allocs/op
    BenchmarkAddDesc64      	    9984	   1454018 ns/op	   21701 B/op	       9 allocs/op
    BenchmarkAddRangeAsc    	 1000000	      1066 ns/op	     118 B/op	       1 allocs/op
    BenchmarkAddRangeDesc4  	   10000	    365530 ns/op	    5503 B/op	       2 allocs/op
    BenchmarkAddRangeDesc64 	   99968	    279275 ns/op	    3276 B/op	       1 allocs/op
    PASS
    ok  	github.com/grafana/metrictank/mdata/cache	90.490s
    
    benchcmp pre.txt post.txt
    benchmark                   old ns/op     new ns/op     delta
    BenchmarkAddAsc             1223930       1223221       -0.06%
    BenchmarkAddDesc1           1449968       1451440       +0.10%
    BenchmarkAddDesc4           1450317       1458588       +0.57%
    BenchmarkAddDesc64          1446361       1454018       +0.53%
    BenchmarkAddRangeAsc        1124          1066          -5.16%
    BenchmarkAddRangeDesc4      362446        365530        +0.85%
    BenchmarkAddRangeDesc64     278462        279275        +0.29%
    
    benchmark                   old allocs     new allocs     delta
    BenchmarkAddAsc             5              5              +0.00%
    BenchmarkAddDesc1           7              7              +0.00%
    BenchmarkAddDesc4           9              9              +0.00%
    BenchmarkAddDesc64          9              9              +0.00%
    BenchmarkAddRangeAsc        1              1              +0.00%
    BenchmarkAddRangeDesc4      2              2              +0.00%
    BenchmarkAddRangeDesc64     1              1              +0.00%
    
    benchmark                   old bytes     new bytes     delta
    BenchmarkAddAsc             21684         21684         +0.00%
    BenchmarkAddDesc1           21720         21720         +0.00%
    BenchmarkAddDesc4           21729         21729         +0.00%
    BenchmarkAddDesc64          21701         21701         +0.00%
    BenchmarkAddRangeAsc        118           118           +0.00%
    BenchmarkAddRangeDesc4      5503          5503          +0.00%
    BenchmarkAddRangeDesc64     3276          3276          +0.00%

--- mdata/cache/ccache_metric.go
+++ mdata/cache/ccache_metric.go
@@ -78,10 +78,17 @@ func (mc *CCacheMetric) AddRange(prev uint32, itergens []chunk.IterGen) {
 		return
 	}
 
+	if len(itergens) == 1 {
+		mc.Add(prev, itergens[0])
+		return
+	}
+
 	mc.Lock()
 	defer mc.Unlock()
 
-	ts := itergens[0].Ts
+	// handle the first one
+	itergen := itergens[0]
+	ts := itergen.Ts
 
 	// if previous chunk has not been passed we try to be smart and figure it out.
 	// this is common in a scenario where a metric continuously gets queried
@@ -93,46 +100,74 @@ func (mc *CCacheMetric) AddRange(prev uint32, itergens []chunk.IterGen) {
 		}
 	}
 
-	for _, itergen := range itergens {
-		ts = itergen.Ts
+	// if the previous chunk is cached, link it
+	if _, ok := mc.chunks[prev]; ok {
+		mc.chunks[prev].Next = ts
+	} else {
+		prev = 0
+	}
 
-		if _, ok := mc.chunks[ts]; ok {
-			// chunk is already present. no need to error on that, just ignore it
-			continue
-		}
-
+	// add chunk if we don't have it yet (most likely)
+	if _, ok := mc.chunks[ts]; !ok {
 		mc.chunks[ts] = &CCacheChunk{
 			Ts:    ts,
-			Prev:  0,
-			Next:  0,
+			Prev:  prev,
+			Next:  itergens[1].Ts,
 			Itgen: itergen,
 		}
+	}
 
-		// if the previous chunk is cached, link in both directions
-		if _, ok := mc.chunks[prev]; ok {
-			mc.chunks[prev].Next = ts
-			mc.chunks[ts].Prev = prev
+	prev = ts
+
+	// handle the 2nd until the last-but-one
+	for i := 1; i < len(itergens)-1; i++ {
+		itergen := itergens[i]
+		ts := itergen.Ts
+		// add chunk if we don't have it yet (most likely)
+		if _, ok := mc.chunks[ts]; !ok {
+			mc.chunks[ts] = &CCacheChunk{
+				Ts:    ts,
+				Prev:  prev,
+				Next:  itergens[i+1].Ts,
+				Itgen: itergen,
+			}
 		}
 		prev = ts
 	}
 
-	nextTs := mc.nextTs(ts)
+	// handle the last one
+	itergen = itergens[len(itergens)-1]
+	ts = itergen.Ts
 
 	// if nextTs() can't figure out the end date it returns ts
-	if nextTs > ts {
+	next := mc.nextTsCore(itergen, ts, prev, 0)
+	if next == ts {
+		next = 0
+	} else {
 		// if the next chunk is cached, link in both directions
-		if _, ok := mc.chunks[nextTs]; ok {
-			mc.chunks[nextTs].Prev = ts
-			mc.chunks[ts].Next = nextTs
+		if _, ok := mc.chunks[next]; ok {
+			mc.chunks[next].Prev = ts
+		} else {
+			next = 0
 		}
 	}
 
+	// add chunk if we don't have it yet (most likely)
+	if _, ok := mc.chunks[ts]; !ok {
+		mc.chunks[ts] = &CCacheChunk{
+			Ts:    ts,
+			Prev:  prev,
+			Next:  next,
+			Itgen: itergen,
+		}
+	}
 	// regenerate the list of sorted keys after adding a chunk
 	mc.generateKeys()
 
 	return
 }
 
+// Add adds a chunk to the cache
 func (mc *CCacheMetric) Add(prev uint32, itergen chunk.IterGen) {
 	ts := itergen.Ts
 
@@ -201,24 +236,29 @@ func (mc *CCacheMetric) generateKeys() {
 // assumes we already have at least a read lock
 func (mc *CCacheMetric) nextTs(ts uint32) uint32 {
 	chunk := mc.chunks[ts]
-	span := chunk.Itgen.Span
+	return mc.nextTsCore(chunk.Itgen, chunk.Ts, chunk.Prev, chunk.Next)
+}
+
+// nextTsCore returns the ts of the next chunk, given a chunks key properties
+// (to the extent we know them). It guesses if necessary.
+// assumes we already have at least a read lock
+func (mc *CCacheMetric) nextTsCore(itgen chunk.IterGen, ts, prev, next uint32) uint32 {
+	span := itgen.Span
 	if span > 0 {
 		// if the chunk is span-aware we don't need anything else
-		return chunk.Ts + span
+		return ts + span
 	}
 
-	if chunk.Next == 0 {
-		if chunk.Prev == 0 {
-			// if a chunk has no next and no previous chunk we have to assume it's length is 0
-			return chunk.Ts
-		} else {
-			// if chunk has no next chunk, but has a previous one, we assume the length of this one is same as the previous one
-			return chunk.Ts + (chunk.Ts - chunk.Prev)
-		}
-	} else {
-		// if chunk has a next chunk, then that's the ts we need
-		return chunk.Next
+	// if chunk has a next chunk, then that's the ts we need
+	if next != 0 {
+		return next
 	}
+	// if chunk has no next chunk, but has a previous one, we assume the length of this one is same as the previous one
+	if prev != 0 {
+		return ts + (ts - prev)
+	}
+	// if a chunk has no next and no previous chunk we have to assume it's length is 0
+	return ts
 }
 
 // lastTs returns the last Ts of this metric cache
