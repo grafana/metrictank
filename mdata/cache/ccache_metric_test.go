851f6ba93de62df2f3abf715bc87775e57461915
Dieter Plaetinck <dieter@grafana.com>
2018-06-12 13:15:22 +0200

    fix

--- mdata/cache/ccache_metric_test.go
+++ mdata/cache/ccache_metric_test.go
@@ -8,30 +8,29 @@ import (
 	"gopkg.in/raintank/schema.v1"
 )
 
-func generateChunks(b testing.TB, startAt, count, step int) []chunk.IterGen {
+func generateChunks(b testing.TB, startAt, count, step uint32) []chunk.IterGen {
 	res := make([]chunk.IterGen, 0, count)
 
 	values := make([]uint32, step)
-	// the metric is initialized with the first chunk at ts 1,
-	// so the generated slice should start at ts 2
-	for i := startAt; i < (step*count)+startAt; i += step {
-		c := getItgen(b, values, uint32(i), true)
+	for t0 := startAt; t0 < startAt+(step*uint32(count)); t0 += step {
+		c := getItgen(b, values, t0, true)
 		res = append(res, c)
 	}
 	return res
 }
 
-func initMetric(b testing.TB) (schema.MKey, *CCacheMetric) {
+func initMetric(b testing.TB, start, step uint32) (schema.MKey, *CCacheMetric) {
 	ccm := NewCCacheMetric()
 	mkey, _ := schema.MKeyFromString("1.12345678901234567890123456789012")
-	c := getItgen(b, []uint32{1}, uint32(1), true)
+	values := make([]uint32, step)
+	c := getItgen(b, values, start, true)
 	ccm.Init(mkey, 0, c)
 	return mkey, ccm
 }
 
 func BenchmarkAddingManyChunksOneByOne(b *testing.B) {
-	_, ccm := initMetric(b)
-	chunks := generateChunks(b, 2, b.N, 1)
+	_, ccm := initMetric(b, 10, 10)
+	chunks := generateChunks(b, 20, uint32(b.N), 10)
 	prev := uint32(1)
 	b.ResetTimer()
 	for _, chunk := range chunks {
@@ -41,17 +40,17 @@ func BenchmarkAddingManyChunksOneByOne(b *testing.B) {
 }
 
 func BenchmarkAddingManyChunksAtOnce(b *testing.B) {
-	_, ccm := initMetric(b)
-	chunks := generateChunks(b, 2, b.N, 1)
+	_, ccm := initMetric(b, 10, 10)
+	chunks := generateChunks(b, 20, uint32(b.N), 10)
 	prev := uint32(1)
 	b.ResetTimer()
 	ccm.AddSlice(prev, chunks)
 }
 
 func TestAddingChunksOneByOneAndQueryingThem(t *testing.T) {
-	mkey, ccm := initMetric(t)
+	mkey, ccm := initMetric(t, 10, 10)
 	amkey := schema.AMKey{MKey: mkey, Archive: 0}
-	chunks := generateChunks(t, 10, 100, 10)
+	chunks := generateChunks(t, 20, 100, 10)
 	prev := uint32(10)
 	for _, chunk := range chunks {
 		ccm.Add(prev, chunk)
@@ -72,11 +71,10 @@ func TestAddingChunksOneByOneAndQueryingThem(t *testing.T) {
 		t.Fatalf("Expected result to start at 40, but had %d", res.Start[len(res.Start)-1].Ts)
 	}
 }
-
 func TestAddingChunksAtOnceAndQueryingThem(t *testing.T) {
-	mkey, ccm := initMetric(t)
+	mkey, ccm := initMetric(t, 10, 10)
 	amkey := schema.AMKey{MKey: mkey, Archive: 0}
-	chunks := generateChunks(t, 10, 100, 10)
+	chunks := generateChunks(t, 20, 100, 10)
 	prev := uint32(10)
 	ccm.AddSlice(prev, chunks)
 
