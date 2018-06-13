3727dfe1b65a262311504db3c3adf414a9e28a94
Dieter Plaetinck <dieter@grafana.com>
2018-06-13 11:40:02 +0200

    adding more tests/benchmarks, making them more sense

--- mdata/cache/ccache_metric_test.go
+++ mdata/cache/ccache_metric_test.go
@@ -19,40 +19,74 @@ func generateChunks(b testing.TB, startAt, count, step uint32) []chunk.IterGen {
 	return res
 }
 
-func getCCM() (schema.MKey, *CCacheMetric) {
-	mkey, _ := schema.MKeyFromString("1.12345678901234567890123456789012")
-	ccm := NewCCacheMetric(mkey)
-	return mkey, ccm
+func getCCM() (schema.AMKey, *CCacheMetric) {
+	amkey, _ := schema.AMKeyFromString("1.12345678901234567890123456789012")
+	ccm := NewCCacheMetric(amkey.MKey)
+	return amkey, ccm
 }
 
-func BenchmarkAddingManyChunksOneByOne(b *testing.B) {
-	_, ccm := getCCM()
-	chunks := generateChunks(b, 10, uint32(b.N), 10)
-	prev := uint32(1)
-	b.ResetTimer()
-	for _, chunk := range chunks {
-		ccm.Add(prev, chunk)
-		prev = chunk.Ts
-	}
+// TestAddAsc tests adding ascending timestamp chunks individually
+func TestAddAsc(t *testing.T) {
+	testRun(t, func(ccm *CCacheMetric) {
+		chunks := generateChunks(t, 10, 6, 10)
+		prev := uint32(1)
+		for _, chunk := range chunks {
+			ccm.Add(prev, chunk)
+			prev = chunk.Ts
+		}
+	})
 }
 
-func BenchmarkAddingManyChunksAtOnce(b *testing.B) {
-	_, ccm := getCCM()
-	chunks := generateChunks(b, 10, uint32(b.N), 10)
-	prev := uint32(1)
-	b.ResetTimer()
-	ccm.AddRange(prev, chunks)
+// TestAddDesc1 tests adding chunks that are all descending
+func TestAddDesc1(t *testing.T) {
+	testRun(t, func(ccm *CCacheMetric) {
+		chunks := generateChunks(t, 10, 6, 10)
+		for i := len(chunks) - 1; i >= 0; i-- {
+			ccm.Add(0, chunks[i])
+		}
+	})
 }
 
-func TestAddingChunksOneByOneAndQueryingThem(t *testing.T) {
-	mkey, ccm := getCCM()
-	amkey := schema.AMKey{MKey: mkey, Archive: 0}
-	chunks := generateChunks(t, 10, 6, 10)
-	prev := uint32(1)
-	for _, chunk := range chunks {
-		ccm.Add(prev, chunk)
-		prev = chunk.Ts
-	}
+// TestAddDesc4 tests adding chunks that are globally descending
+// but in groups 4 are ascending
+func TestAddDesc4(t *testing.T) {
+	testRun(t, func(ccm *CCacheMetric) {
+		chunks := generateChunks(t, 10, 6, 10)
+		ccm.Add(0, chunks[2])
+		ccm.Add(0, chunks[3])
+		ccm.Add(0, chunks[4])
+		ccm.Add(0, chunks[5])
+		ccm.Add(0, chunks[0])
+		ccm.Add(0, chunks[1])
+	})
+}
+
+// TestAddRange tests adding a contiguous range at once
+func TestAddRange(t *testing.T) {
+	testRun(t, func(ccm *CCacheMetric) {
+		chunks := generateChunks(t, 10, 6, 10)
+		prev := uint32(10)
+		ccm.AddRange(prev, chunks)
+	})
+}
+
+// TestAddRangeDesc4 benchmarks adding chunks that are globally descending
+// but in groups of 4 are ascending. those groups are added via 1 AddRange.
+func TestAddRangeDesc4(t *testing.T) {
+	testRun(t, func(ccm *CCacheMetric) {
+		chunks := generateChunks(t, 10, 6, 10)
+		ccm.AddRange(0, chunks[2:6])
+		ccm.AddRange(0, chunks[0:2])
+	})
+}
+
+// test executes the run function
+// run should generate chunks and add them to the CCacheMetric however it likes,
+// but in a way so that the result will be as expected
+func testRun(t *testing.T, run func(*CCacheMetric)) {
+	amkey, ccm := getCCM()
+
+	run(ccm)
 
 	res := CCSearchResult{}
 	ccm.Search(test.NewContext(), amkey, &res, 25, 45)
@@ -68,24 +102,88 @@ func TestAddingChunksOneByOneAndQueryingThem(t *testing.T) {
 		t.Fatalf("Expected result to start at 40, but had %d", res.Start[len(res.Start)-1].Ts)
 	}
 }
-func TestAddingChunksAtOnceAndQueryingThem(t *testing.T) {
-	mkey, ccm := getCCM()
-	amkey := schema.AMKey{MKey: mkey, Archive: 0}
-	chunks := generateChunks(t, 10, 6, 10)
-	prev := uint32(10)
+
+// BenchmarkAddAsc benchmarks adding ascending timestamp chunks individually
+func BenchmarkAddAsc(b *testing.B) {
+	_, ccm := getCCM()
+	chunks := generateChunks(b, 10, uint32(b.N), 10)
+	prev := uint32(1)
+	b.ResetTimer()
+	for _, chunk := range chunks {
+		ccm.Add(prev, chunk)
+		prev = chunk.Ts
+	}
+}
+
+// BenchmarkAddDesc1 benchmarks adding chunks that are all descending
+func BenchmarkAddDesc1(b *testing.B) {
+	_, ccm := getCCM()
+	chunks := generateChunks(b, 10, uint32(b.N), 10)
+	b.ResetTimer()
+	for i := len(chunks) - 1; i >= 0; i-- {
+		ccm.Add(0, chunks[i])
+	}
+}
+
+// BenchmarkAddDesc4 benchmarks adding chunks that are globally descending
+// but in groups 4 are ascending
+func BenchmarkAddDesc4(b *testing.B) {
+	_, ccm := getCCM()
+	b.N = b.N - (b.N % 4)
+	chunks := generateChunks(b, 10, uint32(b.N), 10)
+	b.ResetTimer()
+	for i := len(chunks) - 4; i >= 0; i -= 4 {
+		ccm.Add(0, chunks[i])
+		ccm.Add(0, chunks[i+1])
+		ccm.Add(0, chunks[i+2])
+		ccm.Add(0, chunks[i+3])
+	}
+}
+
+// BenchmarkAddDesc64 benchmarks adding chunks that are globally descending
+// but in groups 64 are ascending
+func BenchmarkAddDesc64(b *testing.B) {
+	_, ccm := getCCM()
+	b.N = b.N - (b.N % 64)
+	chunks := generateChunks(b, 10, uint32(b.N), 10)
+	b.ResetTimer()
+	for i := len(chunks) - 64; i >= 0; i -= 64 {
+		for offset := 0; offset < 64; offset += 1 {
+			ccm.Add(0, chunks[i+offset])
+		}
+	}
+
+}
+
+// BenchmarkAddRangeAsc benchmarks adding a contiguous range at once
+func BenchmarkAddRangeAsc(b *testing.B) {
+	_, ccm := getCCM()
+	chunks := generateChunks(b, 10, uint32(b.N), 10)
+	prev := uint32(1)
+	b.ResetTimer()
 	ccm.AddRange(prev, chunks)
+}
 
-	res := CCSearchResult{}
-	ccm.Search(test.NewContext(), amkey, &res, 25, 45)
-	if res.Complete != true {
-		t.Fatalf("Expected result to be complete, but it was not")
+// BenchmarkAddRangeDesc4 benchmarks adding chunks that are globally descending
+// but in groups 4 are ascending. those groups are added via 1 AddRange.
+func BenchmarkAddRangeDesc4(b *testing.B) {
+	_, ccm := getCCM()
+	b.N = b.N - (b.N % 4)
+	chunks := generateChunks(b, 10, uint32(b.N), 10)
+	b.ResetTimer()
+	for i := len(chunks) - 4; i >= 0; i -= 4 {
+		ccm.AddRange(0, chunks[i:i+4])
 	}
+}
 
-	if res.Start[0].Ts != 20 {
-		t.Fatalf("Expected result to start at 20, but had %d", res.Start[0].Ts)
-	}
-
-	if res.Start[len(res.Start)-1].Ts != 40 {
-		t.Fatalf("Expected result to start at 40, but had %d", res.Start[len(res.Start)-1].Ts)
+// BenchmarkAddRangeDesc64 benchmarks adding chunks that are globally descending
+// but in groups 64 are ascending. those groups are added via 1 AddRange.
+func BenchmarkAddRangeDesc64(b *testing.B) {
+	_, ccm := getCCM()
+	b.N = b.N - (b.N % 64)
+	chunks := generateChunks(b, 10, uint32(b.N), 10)
+	b.ResetTimer()
+	for i := len(chunks) - 64; i >= 0; i -= 64 {
+		ccm.AddRange(0, chunks[i:i+64])
 	}
 }
