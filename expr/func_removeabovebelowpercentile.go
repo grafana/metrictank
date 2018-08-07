691e61b8afc8791f0426eaedc4edce922763205e
Stiven Deleur <sdeleur@bloomberg.net>
2018-08-07 11:07:39 -0400

    wip

---
+++ expr/func_removeabovebelowpercentile.go
@@ -0,0 +1,88 @@
+package expr
+
+import (
+	"fmt"
+	"math"
+	"sort"
+
+	schema "gopkg.in/raintank/schema.v1"
+
+	"github.com/grafana/metrictank/api/models"
+)
+
+type FuncRemoveAboveBelowPercentile struct {
+	in    GraphiteFunc
+	n     float64
+	above bool
+}
+
+func NewRemoveAboveBelowPercentileConstructor(above bool) func() GraphiteFunc {
+	return func() GraphiteFunc {
+		return &FuncRemoveAboveBelowPercentile{above: above}
+	}
+}
+
+func (s *FuncRemoveAboveBelowPercentile) Signature() ([]Arg, []Arg) {
+
+	return []Arg{
+		ArgSeriesList{val: &s.in},
+		ArgFloat{key: "n", val: &s.n},
+	}, []Arg{ArgSeriesList{}}
+}
+
+func (s *FuncRemoveAboveBelowPercentile) Context(context Context) Context {
+	return context
+}
+
+func (s *FuncRemoveAboveBelowPercentile) Exec(cache map[Req][]models.Series) ([]models.Series, error) {
+	series, err := s.in.Exec(cache)
+	if err != nil {
+		return nil, err
+	}
+
+	var output []models.Series
+	for _, serie := range series {
+		if s.above {
+			serie.Target = fmt.Sprintf("removeAbovePercentile(%s, %g)", serie.Target, s.n)
+		} else {
+			serie.Target = fmt.Sprintf("removeBelowPercentile(%s, %g)", serie.Target, s.n)
+		}
+		serie.QueryPatt = serie.Target
+
+		out := pointSlicePool.Get().([]schema.Point)
+		for _, p := range serie.Datapoints {
+			if s.above {
+				if p.Val > s.n {
+					p.Val = math.NaN()
+				}
+			} else {
+				if p.Val < s.n {
+					p.Val = math.NaN()
+				}
+			}
+			out = append(out, p)
+		}
+		serie.Datapoints = out
+		output = append(output, serie)
+	}
+
+	cache[Req{}] = append(cache[Req{}], output...)
+
+	return output, nil
+}
+
+func getPercentileValue(datapoints []schema.Point, n float64) float64 {
+	sortedDatapoints := make([]schema.Point, len(datapoints))
+	for _, p := range datapoints {
+		if !math.IsNaN(p.Val) {
+			sortedDatapoints = append(sortedDatapoints, p)
+		}
+	}
+
+	sort.Slice(sortedDatapoints, func(i, j int) bool {
+		return sortedDatapoints[i].Val < sortedDatapoints[j].Val
+	})
+
+	index := math.Ceil(n/100.0*float64(len(sortedDatapoints)+1)) - 1
+	return sortedDatapoints[int(index)].Val
+}
