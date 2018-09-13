861350b11ac8c70160a8df6fc2a80e3dba154786
Dieter Plaetinck <dieter@grafana.com>
2018-09-13 17:28:52 +0300

    make http stress workload more consistent over different iterations
    use 1 simple pattern instead of a mix of various patterns. we simply
    don't do enough requests to have that become a consistent pattern.
    
    Also, we have to filter for which metrics to use.
    Without filtering we would get all metrics back, e.g.:
    
    1 some.id.of.a.metric.1
    1 some.id.of.a.metric.10
    1 some.id.of.a.metric.100
    1 some.id.of.a.metric.1000
    1 some.id.of.a.metric.10000
    1 some.id.of.a.metric.100000
    ...
    1 some.id.of.a.metric.99999
    
    This meant replacing a single char with a '*' could match many more
    series then we meant to. e.g. the top one, replacing 1 with * would
    match ALL series, resulting in very unfair benchmark runs.
    Now we know that we only ever query for 1 or 10 series.

--- benchmarks/realistic-workload-single-tenant.sh
+++ benchmarks/realistic-workload-single-tenant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 trap 'kill $(jobs -p)' EXIT
 
-# this script aims to put a realistic workload on a metrictank instance or cluster
+# this script aims to put a somewhat realistic workload on a metrictank instance or cluster
 # (in terms of proportions, not absolute volume)
 # the goal is not to test the limits of MT here or get close to production workloads,
 # but rather use a workload that it can definitely handle
@@ -17,6 +17,9 @@ fakemetrics backfill --kafka-mdm-addr localhost:9092 --offset 5h --period 10s --
 fakemetrics feed --kafka-mdm-addr localhost:9092 --period 10s --mpo 100000 &
 sleep 30
 # now request series:
-# some passthrough (requesting 1 series), some 1wildcard (upto 10 series), others 2wildcards (upto 100 series), others 3wildcards (upto 1000 series)
-./build/mt-index-cat -addr http://localhost:6063 -from 60min cass -hosts localhost:9042 -schema-file scripts/config/schema-idx-cassandra.toml 'GET http://localhost:6063/render?target={{.Name | patternCustom 20 "pass" 30 "3rccw" 30 "2rccw" 20 "1rccw" }}&from=-1h\nX-Org-Id: 1\n\n' | vegeta attack -rate 20 -duration 300s | vegeta report
+# this selects exactly 10k series that will match the regex, out of which we randomly replace 1 char with a wildcard, resulting in queries usually for 1 series, and sometimes for 10 series (depending on whether the replaced char falls within the dynamic part an the end of the name or not)
+# so 20/25 chances for 1 series, 5/25 chances for 10
+# as we execute 100Hz*300s=30k requests, this should give us a plenty high cache hit rate (while still testing the uncached code path)
+# in practice, the cache rate sometimes looks fairly low and i'm not sure why. but anyway (seeing about 15% hit partial and the rest are misses), and only in the last minute
+./build/mt-index-cat -addr http://localhost:6063 -from 60min -regex 'some\.id\.of\.a\.metric\.9....' cass -hosts localhost:9042 -schema-file scripts/config/schema-idx-cassandra.toml 'GET http://localhost:6063/render?target={{.Name | patternCustom 100 "1rccw" }}&from=-1h\nX-Org-Id: 1\n\n' | vegeta attack -rate 100 -duration 300s | vegeta report
 sleep 20
