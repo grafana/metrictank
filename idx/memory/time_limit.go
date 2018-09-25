958b9f07e76209b3ff572d2d5db3101dcdf2327c
Dieter Plaetinck <dieter@grafana.com>
2018-09-25 13:15:47 +0300

    typos, simplifications, clarifications and an allocation tweak

--- idx/memory/time_limit.go
+++ idx/memory/time_limit.go
@@ -6,28 +6,39 @@ import (
 	"time"
 )
 
-// TimeLimiter provides a means limit the amount of time spent working.TimeLimiter
+// TimeLimiter limits the rate of a set of operations.
+// It does this by slowing down further operations as soon
+// as one Add() is called informing it the per-window allowed budget has been exceeded.
+// Limitations:
+// * concurrently running operations can all exceed the budget,
+//   so it works best for serial operations.
+// * for serial operations, the last operation is allowed to exceed the budget
+// * when an operation takes very long (e.g. 10 seconds, with a 100ms limit per second), it
+//   is counted as exceeding the 100ms budget, but no other provisions are being made.
+//
+// Thus, TimeLimiter is designed for, and works best with, serially running operations,
+// each of which takes a fraction of the limit.
 type TimeLimiter struct {
 	sync.Mutex
-	ctx          context.Context
-	lockDuration time.Duration
-	window       time.Duration
-	limit        time.Duration
-	accountCh    chan time.Duration
-	queryCh      chan chan struct{}
+	ctx       context.Context
+	timeSpent time.Duration
+	window    time.Duration
+	limit     time.Duration
+	addCh     chan time.Duration
+	queryCh   chan chan struct{}
 }
 
-// NewTimeLimiter creates a new TimeLimiter.  A background thread will run until the
+// NewTimeLimiter creates a new TimeLimiter.  A background goroutine will run until the
 // provided context is done.  When the amount of time spent on task (the time is determined
 // by calls to "Add()") every "window" duration is more then "limit",  then calls to
 // Wait() will block until the start if the next window period.
 func NewTimeLimiter(ctx context.Context, window, limit time.Duration) *TimeLimiter {
 	l := &TimeLimiter{
-		ctx:       ctx,
-		window:    window,
-		limit:     limit,
-		accountCh: make(chan time.Duration),
-		queryCh:   make(chan chan struct{}),
+		ctx:     ctx,
+		window:  window,
+		limit:   limit,
+		addCh:   make(chan time.Duration),
+		queryCh: make(chan chan struct{}),
 	}
 	go l.run()
 	return l
@@ -44,19 +55,17 @@ func (l *TimeLimiter) run() {
 			for _, ch := range blockedQueries {
 				close(ch)
 			}
-			blockedQueries = nil
 			return
 		case <-ticker.C:
-			// reset lockDuration to 0
-			l.lockDuration = time.Duration(0)
+			l.timeSpent = 0
 			for _, ch := range blockedQueries {
 				close(ch)
 			}
-			blockedQueries = nil
-		case d := <-l.accountCh:
-			l.lockDuration += d
+			blockedQueries = blockedQueries[:0]
+		case d := <-l.addCh:
+			l.timeSpent += d
 		case respCh := <-l.queryCh:
-			if l.lockDuration < l.limit {
+			if l.timeSpent < l.limit {
 				close(respCh)
 			} else {
 				// rate limit exceeded.  On the next tick respCh will be closed
@@ -67,14 +76,13 @@ func (l *TimeLimiter) run() {
 	}
 }
 
-// Add increments the counter of time spent doing something by "d"
+// Add increments the "time spent" counter by "d"
 func (l *TimeLimiter) Add(d time.Duration) {
-	l.accountCh <- d
+	l.addCh <- d
 }
 
-// Wait will return immediately if we are not rate limited, otherwise it will
-// block until we are no longer limited.  The longest we will block for is
-// the size of the defined time window.
+// Wait returns when we are not rate limited, which may be
+// anywhere between immediately or after the window.
 func (l *TimeLimiter) Wait() {
 	respCh := make(chan struct{})
 	l.queryCh <- respCh
