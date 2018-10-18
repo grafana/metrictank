827b74e291ae0d7ef1f49b77c121f5c6f0b0ae52
Dieter Plaetinck <dieter@grafana.com>
2018-10-18 19:42:36 +0200

    clarify

--- idx/memory/time_limit_test.go
+++ idx/memory/time_limit_test.go
@@ -34,10 +34,12 @@ func TestTimeLimiter(t *testing.T) {
 	shouldTake(t, tl, 10*time.Millisecond, 0, "window 1: work done: 15ms")
 	shouldTake(t, tl, 80*time.Millisecond, 0, "window 1: work done: 95ms")
 	shouldTake(t, tl, 4*time.Millisecond, 0, "window 1: work done: 99ms")
+	// we should spend at least 1020ms to do 102ms of work
 	shouldTake(t, tl, 3*time.Millisecond, (1020-102)*time.Millisecond, "window 1: work done: 102ms")
 
 	// TEST 2 : Now that we waited until a full window, should be able to up to limit work again
 	shouldTake(t, tl, 50*time.Millisecond, 0, "window 2: work done: 50ms")
 	shouldTake(t, tl, 40*time.Millisecond, 0, "window 2: work done: 90ms")
+	// we should spend at least 1300ms to do 130ms of work
 	shouldTake(t, tl, 40*time.Millisecond, (1300-130)*time.Millisecond, "window 2: work done: 130ms")
 }
