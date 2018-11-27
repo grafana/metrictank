b3e49c6f119ba92446d2c30d04928c43351ae563
Dieter Plaetinck <dieter@grafana.com>
2018-11-26 22:31:06 -0500

    add Series encoding benchmark

--- mdata/chunk/tsz/tsz_test.go
+++ mdata/chunk/tsz/tsz_test.go
@@ -8,6 +8,38 @@ import (
 var T uint32
 var V float64
 
+func BenchmarkPushSeries4h(b *testing.B) {
+	s := NewSeries4h(0)
+	N := uint32(b.N)
+	for i := uint32(1); i <= N; i++ {
+		if i%10 == 0 {
+			s.Push(i, 0)
+		} else if i%10 == 1 {
+			s.Push(i, 1)
+		} else {
+			s.Push(i, float64(i)+123.45)
+		}
+	}
+	s.Finish()
+	b.Logf("Series4h size: %dB", len(s.Bytes()))
+}
+
+func BenchmarkPushSeriesLong(b *testing.B) {
+	s := NewSeriesLong(0)
+	N := uint32(b.N)
+	for i := uint32(1); i <= N; i++ {
+		if i%10 == 0 {
+			s.Push(i, 0)
+		} else if i%10 == 1 {
+			s.Push(i, 1)
+		} else {
+			s.Push(i, float64(i)+123.45)
+		}
+	}
+	s.Finish()
+	b.Logf("SeriesLong size: %dB", len(s.Bytes()))
+}
+
 func BenchmarkIterSeries4h(b *testing.B) {
 	s := NewSeries4h(0)
 	N := uint32(b.N)
