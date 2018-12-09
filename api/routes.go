794509aa10457c17dba0292d4ba3b2410496416c
deniszh <denis.zhdanov@gmail.com>
2018-12-09 21:59:59 +0100

    taming linter

--- api/routes.go
+++ api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/grafana/metrictank/api/models"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/raintank/gziper"
-	macaron "gopkg.in/macaron.v1"
+	"gopkg.in/macaron.v1"
 )
 
 func (s *Server) RegisterRoutes() {
