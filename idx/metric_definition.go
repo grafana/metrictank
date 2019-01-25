c7010d177b56779dd420e4007a66cf9b02df0994
Robert Milan <robert.p.milan@gmail.com>
2019-01-25 16:37:48 -0300

    add new MetricDefinition for use with interning
    Make the iterning layer global to the index so that we can
    reuse it throughout. For now, if a MetricDefinition leaves the index
    it will need to be converted back into a schema.Metricdefinition.

---
+++ idx/metric_definition.go
@@ -0,0 +1,224 @@
+package idx
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+	"unsafe"
+
+	"github.com/raintank/schema"
+	log "github.com/sirupsen/logrus"
+)
+
+type mType uint8
+
+const (
+	MTypeUndefined mType = iota
+	MTypeGauge
+	MTypeRate
+	MTypeCount
+	MTypeCounter
+	MTypeTimestamp
+)
+
+//go:generate msgp
+type MetricName struct {
+	nodes []uintptr
+}
+
+func (mn *MetricName) String() string {
+	if len(mn.nodes) == 0 {
+		return ""
+	}
+
+	bld := strings.Builder{}
+	return mn.string(&bld)
+}
+
+func (mn *MetricName) string(bld *strings.Builder) string {
+	// get []int of the lengths of all of the mn.nodes
+	lns, ok := IdxIntern.LenNoCprsn(mn.nodes)
+	if !ok {
+		// this should never happen, do what now?
+		return ""
+	}
+
+	// should be faster than calling IdxIntern.SetStringNoCprsn in a tight loop
+	var tmpSz string
+	szHeader := (*reflect.StringHeader)(unsafe.Pointer(&tmpSz))
+	first, _ := IdxIntern.ObjString(mn.nodes[0])
+	bld.WriteString(first)
+	for idx, nodePtr := range mn.nodes[1:] {
+		szHeader.Data = nodePtr
+		szHeader.Len = lns[idx]
+		bld.WriteString(".")
+		bld.WriteString(tmpSz)
+	}
+
+	return bld.String()
+}
+
+// TagKeyValue holds interned versions of the Tag Key And Value
+type TagKeyValue struct {
+	Key   string
+	Value string
+}
+
+func (t *TagKeyValue) String() string {
+	bld := strings.Builder{}
+
+	bld.WriteString(t.Key)
+	bld.WriteString("=")
+	bld.WriteString(t.Value)
+
+	return bld.String()
+}
+
+// TagKeyValues stores a slice of all of the Tag K/V combinations for a MetricDefinition
+type TagKeyValues []TagKeyValue
+
+// Strings returns a slice containing all of the Tag K/V combinations for a MetricDefinition
+func (t TagKeyValues) Strings() []string {
+	tags := make([]string, len(t))
+	for i, tag := range t {
+		tags[i] = tag.String()
+	}
+	return tags
+}
+
+type MetricDefinition struct {
+	Id       schema.MKey `json:"mkey"`
+	OrgId    uint32      `json:"org_id"`
+	Name     MetricName  `json:"name"`
+	Interval int         `json:"interval"`
+	Unit     string      `json:"unit"`
+	mtype    mType       `json:"mtype"`
+
+	// some users of MetricDefinition (f.e. MetricTank) might add a "name" tag
+	// to this slice which allows querying by name as a tag. this special tag
+	// should not be stored or transmitted over the network, otherwise it may
+	// just get overwritten by the receiver.
+	Tags       TagKeyValues `json:"tags"`
+	LastUpdate int64        `json:"lastUpdate"` // unix timestamp
+	Partition  int32        `json:"partition"`
+}
+
+func (md *MetricDefinition) NameWithTags() string {
+	bld := strings.Builder{}
+
+	bld.WriteString("name=")
+	bld.WriteString(md.Name.string(&bld))
+	for _, tag := range md.Tags {
+		bld.WriteString(";")
+		bld.WriteString(tag.String())
+	}
+	return bld.String()
+}
+
+func (md *MetricDefinition) SetMType(mtype string) {
+	switch mtype {
+	case "gauge":
+		md.mtype = MTypeGauge
+	case "rate":
+		md.mtype = MTypeRate
+	case "count":
+		md.mtype = MTypeCount
+	case "counter":
+		md.mtype = MTypeCounter
+	case "timestamp":
+		md.mtype = MTypeTimestamp
+	default:
+		// for values "" and other unknown/corrupted values
+		md.mtype = MTypeUndefined
+	}
+}
+
+func (md *MetricDefinition) Mtype() string {
+	switch md.mtype {
+	case MTypeGauge:
+		return "gauge"
+	case MTypeRate:
+		return "rate"
+	case MTypeCount:
+		return "count"
+	case MTypeCounter:
+		return "counter"
+	case MTypeTimestamp:
+		return "timestamp"
+	default:
+		// case of MTypeUndefined and also default for unknown/corrupted values
+		return ""
+	}
+}
+
+func (md *MetricDefinition) SetUnit(unit string) {
+	sz, err := IdxIntern.AddOrGetSzNoCprsn([]byte(unit))
+	if err != nil {
+		md.Unit = unit
+	}
+	md.Unit = sz
+}
+
+func (md *MetricDefinition) SetMetricName(name string) {
+	nodes := strings.Split(name, ".")
+	md.Name.nodes = make([]uintptr, len(nodes))
+	for i, node := range nodes {
+		// TODO: add error checking? Fail somehow
+		nodePtr, _ := IdxIntern.AddOrGet([]byte(node))
+		md.Name.nodes[i] = nodePtr
+	}
+}
+
+func (md *MetricDefinition) SetTags(tags []string) {
+	md.Tags = make([]TagKeyValue, len(tags))
+	sort.Strings(tags)
+	for i, tag := range tags {
+		splits := strings.Split(tag, "=")
+		if len(splits) < 2 {
+			invalidTag.Inc()
+			log.Errorf("idx: Tag %q has an invalid format, ignoring", tag)
+			continue
+		}
+		keySz, err := IdxIntern.AddOrGetSzNoCprsn([]byte(splits[0]))
+		if err != nil {
+			keyTmpSz := splits[0]
+			md.Tags[i].Key = keyTmpSz
+		} else {
+			md.Tags[i].Key = keySz
+		}
+
+		valueSz, err := IdxIntern.AddOrGetSzNoCprsn([]byte(splits[1]))
+		if err != nil {
+			valueTmpSz := splits[1]
+			md.Tags[i].Value = valueTmpSz
+		} else {
+			md.Tags[i].Value = valueSz
+		}
+	}
+}
+
+func MetricDefinitionFromMetricDataWithMkey(mkey schema.MKey, d *schema.MetricData) *MetricDefinition {
+	md := &MetricDefinition{
+		Id:         mkey,
+		OrgId:      uint32(d.OrgId),
+		Interval:   d.Interval,
+		LastUpdate: d.Time,
+	}
+
+	md.SetMetricName(d.Name)
+	md.SetMType(d.Mtype)
+	md.SetTags(d.Tags)
+	md.SetUnit(d.Unit)
+
+	return md
+}
+
+func MetricDefinitionFromMetricData(d *schema.MetricData) (*MetricDefinition, error) {
+	mkey, err := schema.MKeyFromString(d.Id)
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing ID: %s", err)
+	}
+
+	return MetricDefinitionFromMetricDataWithMkey(mkey, d), nil
+}
