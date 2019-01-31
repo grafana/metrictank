9296f9d44d6aee6c27346e778a05c145d1ddae95
Robert Milan <robert.p.milan@gmail.com>
2019-01-30 22:43:09 -0300

    update mt-index-cat

--- cmd/mt-index-cat/out/out.go
+++ cmd/mt-index-cat/out/out.go
@@ -7,33 +7,37 @@ import (
 	"text/template"
 
 	"github.com/davecgh/go-spew/spew"
-
-	"github.com/raintank/schema"
+	"github.com/grafana/metrictank/idx"
 )
 
 var QueryTime int64
 
-func Dump(d schema.MetricDefinition) {
+func Dump(d idx.MetricDefinition) {
 	spew.Dump(d)
 }
 
+<<<<<<< HEAD
 func List(d schema.MetricDefinition) {
 	fmt.Println(d.OrgId, d.NameWithTags())
+=======
+func List(d idx.MetricDefinition) {
+	fmt.Println(d.OrgId, d.Name.String())
+>>>>>>> update mt-index-cat
 }
 
-func GetVegetaRender(addr, from string) func(d schema.MetricDefinition) {
-	return func(d schema.MetricDefinition) {
-		fmt.Printf("GET %s/render?target=%s&from=-%s\nX-Org-Id: %d\n\n", addr, d.Name, from, d.OrgId)
+func GetVegetaRender(addr, from string) func(d idx.MetricDefinition) {
+	return func(d idx.MetricDefinition) {
+		fmt.Printf("GET %s/render?target=%s&from=-%s\nX-Org-Id: %d\n\n", addr, d.Name.String(), from, d.OrgId)
 	}
 }
 
-func GetVegetaRenderPattern(addr, from string) func(d schema.MetricDefinition) {
-	return func(d schema.MetricDefinition) {
-		fmt.Printf("GET %s/render?target=%s&from=-%s\nX-Org-Id: %d\n\n", addr, pattern(d.Name), from, d.OrgId)
+func GetVegetaRenderPattern(addr, from string) func(d idx.MetricDefinition) {
+	return func(d idx.MetricDefinition) {
+		fmt.Printf("GET %s/render?target=%s&from=-%s\nX-Org-Id: %d\n\n", addr, pattern(d.Name.String()), from, d.OrgId)
 	}
 }
 
-func Template(format string) func(d schema.MetricDefinition) {
+func Template(format string) func(d idx.MetricDefinition) {
 	funcs := make(map[string]interface{})
 	funcs["pattern"] = pattern
 	funcs["patternCustom"] = patternCustom
@@ -45,7 +49,7 @@ func Template(format string) func(d schema.MetricDefinition) {
 
 	tpl := template.Must(template.New("format").Funcs(funcs).Parse(format))
 
-	return func(d schema.MetricDefinition) {
+	return func(d idx.MetricDefinition) {
 		err := tpl.Execute(os.Stdout, &d)
 		if err != nil {
 			panic(err)
