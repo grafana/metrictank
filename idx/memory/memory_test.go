01e92ecd2465b07250f2e517a2aaf447f03eed94
Robert Milan <robert.p.milan@gmail.com>
2019-01-30 23:00:04 -0300

    fix memory test

--- idx/memory/memory_test.go
+++ idx/memory/memory_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/grafana/metrictank/conf"
 	"github.com/grafana/metrictank/idx"
+	"github.com/grafana/metrictank/mdata"
 	"github.com/grafana/metrictank/test"
 	"github.com/raintank/schema"
 	. "github.com/smartystreets/goconvey/convey"
@@ -979,44 +980,44 @@ func BenchmarkPruneLongSeriesNames(b *testing.B) {
 	}
 }
 
-// func TestMatchSchemaWithTags(t *testing.T) {
-// 	_tagSupport := TagSupport
-// 	_schemas := mdata.Schemas
-// 	defer func() { TagSupport = _tagSupport }()
-// 	defer func() { mdata.Schemas = _schemas }()
+func TestMatchSchemaWithTags(t *testing.T) {
+	_tagSupport := TagSupport
+	_schemas := mdata.Schemas
+	defer func() { TagSupport = _tagSupport }()
+	defer func() { mdata.Schemas = _schemas }()
 
-// 	TagSupport = true
-// 	mdata.Schemas = conf.NewSchemas([]conf.Schema{
-// 		{
-// 			Name:       "tag1_is_value3_or_value5",
-// 			Pattern:    regexp.MustCompile(".*;tag1=value[35](;.*|$)"),
-// 			Retentions: conf.Retentions([]conf.Retention{conf.NewRetentionMT(1, 3600*24*1, 600, 2, 0)}),
-// 		},
-// 	})
+	TagSupport = true
+	mdata.Schemas = conf.NewSchemas([]conf.Schema{
+		{
+			Name:       "tag1_is_value3_or_value5",
+			Pattern:    regexp.MustCompile(".*;tag1=value[35](;.*|$)"),
+			Retentions: conf.Retentions([]conf.Retention{conf.NewRetentionMT(1, 3600*24*1, 600, 2, 0)}),
+		},
+	})
 
-// 	ix := New()
-// 	ix.Init()
+	ix := New()
+	ix.Init()
 
-// 	data := make([]*idx.MetricDefinition, 10)
-// 	archives := make([]idx.Archive, 10)
-// 	for i := 0; i < 10; i++ {
-// 		name := fmt.Sprintf("some.id.of.a.metric.%d", i)
-// 		data[i] = &idx.MetricDefinition{
-// 			Id: MKeyFromString()
-// 			Name:     name,
-// 			OrgId:    1,
-// 			Interval: 1,
-// 			Tags:     []string{fmt.Sprintf("tag1=value%d", i), "tag2=othervalue"},
-// 		}
-// 		data[i].SetId()
-// 		archives[i] = ix.add(data[i])
-// 	}
+	data := make([]*idx.MetricDefinition, 10)
+	archives := make([]idx.Archive, 10)
+	for i := 0; i < 10; i++ {
+		name := fmt.Sprintf("some.id.of.a.metric.%d", i)
+		data[i] = &idx.MetricDefinition{
+			OrgId:    1,
+			Interval: 1,
+		}
+		tags := []string{fmt.Sprintf("tag1=value%d", i), "tag2=othervalue"}
+		data[i].SetTags(tags)
+		data[i].SetMetricName(name)
+		data[i].SetId()
+		archives[i] = ix.add(data[i])
+	}
 
-// 	// only those MDs with tag1=value3 or tag1=value5 should get the first schema id
-// 	expectedSchemas := []uint16{1, 1, 1, 0, 1, 0, 1, 1, 1, 1}
-// 	for i := 0; i < 10; i++ {
-// 		if archives[i].SchemaId != expectedSchemas[i] {
-// 			t.Fatalf("Expected schema of archive %d to be %d, but it was %d", i, expectedSchemas[i], archives[i].SchemaId)
-// 		}
-// 	}
-// }
+	// only those MDs with tag1=value3 or tag1=value5 should get the first schema id
+	expectedSchemas := []uint16{1, 1, 1, 0, 1, 0, 1, 1, 1, 1}
+	for i := 0; i < 10; i++ {
+		if archives[i].SchemaId != expectedSchemas[i] {
+			t.Fatalf("Expected schema of archive %d to be %d, but it was %d", i, expectedSchemas[i], archives[i].SchemaId)
+		}
+	}
+}
