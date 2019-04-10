ee77e8846cb972a6494b3a7c8a74716cee77d1c3
Dieter Plaetinck <dieter@grafana.com>
2019-04-10 15:36:48 +0200

    remove log-min-dur
    introduced via 929513c / #174
    became obsolete in b3d3831 / #575 / 0.7.1-61-gbe64803e
    
    we no longer really log requests ourselves anymore.
    i imagine at some point we probably will, at which point we can
    re-introduce this setting
    
    (our qa tool `unused` notified that this variable wasn't being read
    anywhere)

--- api/config.go
+++ api/config.go
@@ -16,8 +16,6 @@ var (
 	maxPointsPerReqSoft int
 	maxPointsPerReqHard int
 	maxSeriesPerReq     int
-	logMinDurStr        string
-	logMinDur           uint32
 
 	Addr             string
 	UseSSL           bool
@@ -41,8 +39,6 @@ func ConfigSetup() {
 	apiCfg.IntVar(&maxPointsPerReqSoft, "max-points-per-req-soft", 1000000, "lower resolution rollups will be used to try and keep requests below this number of datapoints. (0 disables limit)")
 	apiCfg.IntVar(&maxPointsPerReqHard, "max-points-per-req-hard", 20000000, "limit of number of datapoints a request can return. Requests that exceed this limit will be rejected. (0 disables limit)")
 	apiCfg.IntVar(&maxSeriesPerReq, "max-series-per-req", 250000, "limit of number of series a request can operate on. Requests that exceed this limit will be rejected. (0 disables limit)")
-	apiCfg.StringVar(&logMinDurStr, "log-min-dur", "5min", "only log incoming requests if their timerange is at least this duration. Use 0 to disable")
-
 	apiCfg.StringVar(&Addr, "listen", ":6060", "http listener address.")
 	apiCfg.BoolVar(&UseSSL, "ssl", false, "use HTTPS")
 	apiCfg.BoolVar(&useGzip, "gzip", true, "use GZIP compression of all responses")
@@ -58,8 +54,6 @@ func ConfigSetup() {
 }
 
 func ConfigProcess() {
-	logMinDur = dur.MustParseDuration("log-min-dur", logMinDurStr)
-
 	//validate the addr
 	_, err := net.ResolveTCPAddr("tcp", Addr)
 	if err != nil {
