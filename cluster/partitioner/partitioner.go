7d883af2b065722f68ec63bdc4166f321d419063
Dieter Plaetinck <dieter@grafana.com>
2019-04-18 15:38:41 +0200

    settle with xxhash, remove others and comparison framework

--- cluster/partitioner/partitioner.go
+++ cluster/partitioner/partitioner.go
@@ -1,15 +1,11 @@
 package partitioner
 
 import (
-	"encoding/binary"
 	"fmt"
-	"hash/fnv"
 
 	"github.com/Shopify/sarama"
 	"github.com/cespare/xxhash"
-	"github.com/dchest/siphash"
 	jump "github.com/dgryski/go-jump"
-	metro "github.com/dgryski/go-metro"
 	"github.com/raintank/schema"
 )
 
@@ -50,82 +46,9 @@ func (k *Kafka) Partition(m schema.PartitionedMetric, numPartitions int32) (int3
 	return k.Partitioner.Partition(&sarama.ProducerMessage{Key: sarama.ByteEncoder(key)}, numPartitions)
 }
 
-type jumpPartitionerMauro struct{}
+type jumpPartitioner struct{}
 
-func (p jumpPartitionerMauro) Partition(message *sarama.ProducerMessage, numPartitions int32) (int32, error) {
-	key, err := message.Key.Encode()
-	if err != nil {
-		return 0, err
-	}
-
-	// we need to get from a []byte key to a uint64 key, because jump requires that as input
-	// so we keep adding slices of 8 bytes to the jump key as uint64 values
-	// if the result wraps around the uin64 boundary it doesn't matter because
-	// this only needs to be fast and consistent
-	key = append(key, make([]byte, 8-len(key)%8)...)
-	var jumpKey uint64
-	for pos := 8; pos <= len(key); pos += 8 {
-		jumpKey += binary.BigEndian.Uint64(key[pos-8 : pos])
-	}
-
-	return jump.Hash(jumpKey, int(numPartitions)), nil
-}
-
-func (p jumpPartitionerMauro) RequiresConsistency() bool {
-	return true
-}
-
-type jumpPartitionerFnv struct{}
-
-func (p jumpPartitionerFnv) Partition(message *sarama.ProducerMessage, numPartitions int32) (int32, error) {
-	key, err := message.Key.Encode()
-	if err != nil {
-		return 0, err
-	}
-	hf := fnv.New64a()
-	_, _ = hf.Write(key)
-	return jump.Hash(hf.Sum64(), int(numPartitions)), nil
-}
-
-func (p jumpPartitionerFnv) RequiresConsistency() bool {
-	return true
-}
-
-type jumpPartitionerMetro struct{}
-
-func (p jumpPartitionerMetro) Partition(message *sarama.ProducerMessage, numPartitions int32) (int32, error) {
-	key, err := message.Key.Encode()
-	if err != nil {
-		return 0, err
-	}
-	jumpKey := metro.Hash64(key, 0)
-	return jump.Hash(jumpKey, int(numPartitions)), nil
-
-}
-
-func (p jumpPartitionerMetro) RequiresConsistency() bool {
-	return true
-}
-
-type jumpPartitionerSip struct{}
-
-func (p jumpPartitionerSip) Partition(message *sarama.ProducerMessage, numPartitions int32) (int32, error) {
-	key, err := message.Key.Encode()
-	if err != nil {
-		return 0, err
-	}
-	jumpKey := siphash.Hash(0, 0, key)
-	return jump.Hash(jumpKey, int(numPartitions)), nil
-
-}
-
-func (p jumpPartitionerSip) RequiresConsistency() bool {
-	return true
-}
-
-type jumpPartitionerXxhash struct{}
-
-func (p jumpPartitionerXxhash) Partition(message *sarama.ProducerMessage, numPartitions int32) (int32, error) {
+func (p jumpPartitioner) Partition(message *sarama.ProducerMessage, numPartitions int32) (int32, error) {
 	key, err := message.Key.Encode()
 	if err != nil {
 		return 0, err
@@ -135,6 +58,6 @@ func (p jumpPartitionerXxhash) Partition(message *sarama.ProducerMessage, numPar
 
 }
 
-func (p jumpPartitionerXxhash) RequiresConsistency() bool {
+func (p jumpPartitioner) RequiresConsistency() bool {
 	return true
 }
--- cluster/partitioner/partitioner_test.go
+++ cluster/partitioner/partitioner_test.go
@@ -1,122 +1,15 @@
 package partitioner
 
 import (
-	"fmt"
-	"math"
 	"testing"
 
 	"github.com/Shopify/sarama"
 )
 
-// note: lots of output here, to make sense of it, here's some tips:
-// * generally, COV should go down as totalMetrics/n increases.
-//   in particular, low metric counts on high number of shards has bad scores
-//   but is also less relevant.
-// * fnv = medium (32 partitions)
-// * ops = medium (32 partitions)
-// * rtm = small (8 partitions)
-// * id = don't remember which instance this is
-
-func TestSaramaPartitioner(t *testing.T) {
-	testPartitioner("sarama", sarama.NewHashPartitioner(""), t)
-}
-
-func TestJumpPartitionerMauro(t *testing.T) {
-	testPartitioner("jump-mauro", jumpPartitionerMauro{}, t)
-}
-func TestJumpPartitionerFnv(t *testing.T) {
-	testPartitioner("jump-fnv", jumpPartitionerFnv{}, t)
-}
-func TestJumpPartitionerMetro(t *testing.T) {
-	testPartitioner("jump-metro", jumpPartitionerMetro{}, t)
-}
-func TestJumpPartitionerSip(t *testing.T) {
-	testPartitioner("jump-sip", jumpPartitionerSip{}, t)
-}
-func TestJumpPartitionerXxhash(t *testing.T) {
-	testPartitioner("jump-xxhash", jumpPartitionerXxhash{}, t)
-}
-
-func testPartitioner(partitionerName string, p sarama.Partitioner, t *testing.T) {
-	test := func(datasetName string, shards int, dataset chan []byte, t *testing.T) {
-		distributions := make([]int, shards)
-		var total int
-		for metric := range dataset {
-			msg := &sarama.ProducerMessage{Key: sarama.ByteEncoder(metric)}
-			p, err := p.Partition(msg, int32(shards))
-			if err != nil {
-				t.Fatal(err)
-			}
-			distributions[int(p)]++
-			total++
-		}
-		cov, pctDiff := stats(distributions, t)
-		fmt.Printf("%20s/%-4d %10s %9d -> cov=%.3f, diff=%.2f%%\n", partitionerName, shards, datasetName, total, cov, pctDiff)
-	}
-
-	for _, shards := range []int{8, 32, 128} {
-		test("rtm", shards, dataset("rtm"), t)
-		test("id", shards, dataset("id"), t)
-		test("ops", shards, dataset("ops"), t)
-		test("fng", shards, dataset("fng"), t)
-		for _, tags := range []bool{false, true} {
-			for _, fakes := range []int{10 * 1000, 100 * 1000, 1000 * 1000, 10 * 1000 * 1000} {
-				datasetName := fmt.Sprintf("fake-%t", tags)
-				test(datasetName, shards, fakeMetrics(fakes, tags), t)
-			}
-		}
-	}
-}
-
-// stats computes the stats of the distribution:
-// 1) coefficient of variation, which is an excellent measure of relative variation.
-// 2) percent difference between highest and lowest partition
-func stats(distributions []int, t *testing.T) (float64, float64) {
-	var sum int
-	min := math.MaxInt64
-	max := math.MinInt64
-	for i := 0; i < len(distributions); i++ {
-		// fmt.Printf("%6d", distributions[i])
-		size := distributions[i]
-		if size < min {
-			min = size
-		}
-		if size > max {
-			max = size
-		}
-		sum += size
-	}
-	mean := float64(sum) / float64(len(distributions))
-
-	var stdev float64
-	for i := 0; i < len(distributions); i++ {
-		stdev += math.Pow(float64(distributions[i])-mean, 2)
-	}
-	stdev = math.Sqrt(stdev / float64(len(distributions)))
-	cov := stdev / mean
-	pctdiff := float64(100*(max-min)) / float64(min)
-	return cov, pctdiff
-}
-
 var p int32
 
-func BenchmarkPartitionerSarama(b *testing.B) {
-	benchmarkPartitioner(sarama.NewHashPartitioner(""), b)
-}
-func BenchmarkJumpPartitionerMauro(b *testing.B) {
-	benchmarkPartitioner(jumpPartitionerMauro{}, b)
-}
-func BenchmarkJumpPartitionerFnv(b *testing.B) {
-	benchmarkPartitioner(jumpPartitionerFnv{}, b)
-}
-func BenchmarkJumpPartitionerMetro(b *testing.B) {
-	benchmarkPartitioner(jumpPartitionerMetro{}, b)
-}
-func BenchmarkJumpPartitionerSip(b *testing.B) {
-	benchmarkPartitioner(jumpPartitionerSip{}, b)
-}
-func BenchmarkJumpPartitionerXxhash(b *testing.B) {
-	benchmarkPartitioner(jumpPartitionerXxhash{}, b)
+func BenchmarkJumpPartitioner(b *testing.B) {
+	benchmarkPartitioner(jumpPartitioner{}, b)
 }
 
 // note: jumphash is so efficient that trying to test more different metrics would cause slowdowns due to cpu cache misses
