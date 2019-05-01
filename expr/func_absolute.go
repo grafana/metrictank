4d67e048720c9694a42126b568f7ca4d9c071955
Stiven Deleur <deleur@usc.edu>
2019-05-01 15:05:29 -0700

    finished absolute function and added tests

--- expr/func_absolute.go
+++ expr/func_absolute.go
@@ -34,8 +34,8 @@ func (s *FuncAbsolute) Exec(cache map[Req][]models.Series) ([]models.Series, err
 	out := make([]models.Series, len(series))
 	for i, serie := range series {
 		transformed := &out[i]
-		transformed.Target = fmt.Sprintf("absolutes(%s)", serie.Target)
-		transformed.QueryPatt = fmt.Sprintf("absolutes(%s)", serie.QueryPatt)
+		transformed.Target = fmt.Sprintf("absolute(%s)", serie.Target)
+		transformed.QueryPatt = fmt.Sprintf("absolute(%s)", serie.QueryPatt)
 		transformed.Tags = make(map[string]string, len(serie.Tags)+1)
 		transformed.Datapoints = pointSlicePool.Get().([]schema.Point)
 		transformed.Interval = serie.Interval
@@ -45,7 +45,7 @@ func (s *FuncAbsolute) Exec(cache map[Req][]models.Series) ([]models.Series, err
 		for k, v := range serie.Tags {
 			transformed.Tags[k] = v
 		}
-		transformed.Tags["absolutes"] = "1"
+		transformed.Tags["absolute"] = "1"
 		for _, p := range serie.Datapoints {
 			p.Val = math.Abs(p.Val)
 			transformed.Datapoints = append(transformed.Datapoints, p)
