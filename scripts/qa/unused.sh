3d0c5abe05a87fcffeecc27e619f6db324840fab
Mauro Stettler <mauro.stettler@gmail.com>
2019-05-30 21:51:38 +0000

    use new tool staticcheck instead of unused

--- scripts/qa/unused.sh
+++ scripts/qa/unused.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# runs the tools varcheck, structcheck and deadcode
+# runs the tools staticcheck, varcheck, structcheck and deadcode
 # see their websites for more info.
 
 # find the dir we exist within...
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # and cd into root project dir
 cd ${DIR}/../..
+go get -u honnef.co/go/tools/cmd/staticcheck
 go get -u github.com/opennota/check/cmd/varcheck
 go get -u github.com/opennota/check/cmd/structcheck
 # for https://github.com/remyoudompheng/go-misc/pull/14
 go get -u github.com/Dieterbe/go-misc/deadcode
 
 ret=0
+
+echo "## running staticcheck"
+staticcheck -checks U1000 ./...
+r=$?
+[ $r -gt $ret ] && ret=$r
 
 echo "## running varcheck"
 varcheck ./...
