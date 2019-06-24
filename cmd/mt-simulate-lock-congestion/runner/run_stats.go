53d135890a049420df90653d498642542a476776
Mauro Stettler <mauro.stettler@gmail.com>
2019-06-24 17:45:02 +0000

    fix query statistics

--- cmd/mt-simulate-lock-congestion/runner/run_stats.go
+++ cmd/mt-simulate-lock-congestion/runner/run_stats.go
@@ -3,24 +3,33 @@ package runner
 import (
 	"log"
 	"sync/atomic"
+	"time"
 )
 
 type runStats struct {
-	addsCompleted    uint32   // how many adds have been executed
-	queriesCompleted uint32   // how many queries have been executed
-	queryTimes       []uint32 // slice of query execution durations in ms
-	queryTimeCursor  uint32   // tracking position of the last write to queryTimes
+	addsCompleted    uint32          // how many adds have been executed
+	queriesCompleted uint32          // how many queries have been executed
+	queryTimes       []time.Duration // slice of query execution durations in ms
+	queryTimeCursor  uint32          // tracking position of the last write to queryTimes
+}
+
+func newRunStats(queryCount uint32) runStats {
+	return runStats{
+		queryTimes: make([]time.Duration, queryCount),
+	}
 }
 
 func (r *runStats) Print(runSeconds uint32) {
-	log.Printf("Adds Completed: %d (%d / sec)", r.addsCompleted, r.addsCompleted/runSeconds)
-	log.Printf("Queries Completed: %d (%d / sec)", r.queriesCompleted, r.queriesCompleted/runSeconds)
+	log.Printf("Adds Completed: %d (%f / sec)", r.addsCompleted, float32(r.addsCompleted)/float32(runSeconds))
+	log.Printf("Queries Completed: %d (%f / sec)", r.queriesCompleted, float32(r.queriesCompleted)/float32(runSeconds))
 
 	var queryTimeSum uint64
-	for _, queryTime := range r.queryTimes {
-		queryTimeSum += uint64(queryTime)
+	queriesCompleted := atomic.LoadUint32(&r.queriesCompleted)
+	for i := uint32(0); i < queriesCompleted; i++ {
+		queryTimeSum += uint64(r.queryTimes[i].Nanoseconds())
 	}
-	log.Printf("Average query time: %d ns", queryTimeSum/uint64(r.queryTimeCursor))
+
+	log.Printf("Average query time: %d ms", queryTimeSum/uint64(queriesCompleted)/1000000)
 }
 
 func (r *runStats) incAddsCompleted() uint32 {
@@ -31,9 +40,10 @@ func (r *runStats) incQueriesCompleted() uint32 {
 	return atomic.AddUint32(&r.queriesCompleted, 1)
 }
 
-func (r *runStats) addQueryTime(time uint32) {
+func (r *runStats) addQueryTime(time time.Duration) {
 	cursor := atomic.AddUint32(&r.queryTimeCursor, 1)
-	if cursor < uint32(len(r.queryTimes)) {
-		r.queryTimes[cursor] = time
+	if cursor >= uint32(len(r.queryTimes)) {
+		log.Fatal("Exhausted slice to collect query times")
 	}
+	r.queryTimes[cursor] = time
 }
