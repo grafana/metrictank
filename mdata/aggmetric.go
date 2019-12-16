37b7734eb6e5e14994bdacfbe0e3fc6256fdbc22
Mauro Stettler <mauro.stettler@gmail.com>
2019-12-16 18:16:57 -0300

    add comment

--- mdata/aggmetric.go
+++ mdata/aggmetric.go
@@ -451,6 +451,9 @@ func (a *AggMetric) Add(ts uint32, val float64) {
 	// need to check if ts > futureTolerance to prevent that we reject a datapoint
 	// because the ts value has wrapped around the uint32 boundary
 	if ts > a.futureTolerance && int64(ts-a.futureTolerance) > time.Now().Unix() {
+		// we increase this counter in any case, even if the enforcement of the future tolerance
+		// is disabled. this is useful to predict whether data points would get rejected once
+		// the feature gets enabled.
 		discardedSampleTooFarAhead.Inc()
 
 		if enforceFutureTolerance {
