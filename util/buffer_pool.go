85e9c4faf3122f1d6802da47c0994333f223ff3f
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-01-15 14:45:57 -0500

    import BufferPool33 struct from raintank/tsdb-gw to fix compilation errors

--- util/buffer_pool.go
+++ util/buffer_pool.go
@@ -21,3 +21,23 @@ func (b *BufferPool) Get() []byte {
 func (b *BufferPool) Put(buf []byte) {
 	b.pool.Put(buf[:0])
 }
+
+
+// BufferPool33 is a pool that returns cap=33 len=0 byte slices
+type BufferPool33 struct {
+	pool sync.Pool
+}
+
+func NewBufferPool33() *BufferPool33 {
+	return &BufferPool33{pool: sync.Pool{
+		New: func() interface{} { return make([]byte, 0, 33) },
+	}}
+}
+
+func (b *BufferPool33) Get() []byte {
+	return b.pool.Get().([]byte)
+}
+
+func (b *BufferPool33) Put(buf []byte) {
+	b.pool.Put(buf[:0])
+}
