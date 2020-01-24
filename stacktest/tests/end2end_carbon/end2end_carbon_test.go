e2a1a001cdeb6b9cda941fd09fc0b57d06a268e0
Robert Milan <robert@grafana.com>
2020-01-24 15:00:59 +0100

    increase stack tests startup wait times

--- stacktest/tests/end2end_carbon/end2end_carbon_test.go
+++ stacktest/tests/end2end_carbon/end2end_carbon_test.go
@@ -105,7 +105,7 @@ func TestStartup(t *testing.T) {
 	case <-tracker.Match(matchers):
 		log.Println("stack now running.")
 		log.Println("Go to http://localhost:3000 (and login as admin:admin) to see what's going on")
-	case <-time.After(time.Second * 100):
+	case <-time.After(time.Second * 180):
 		grafana.PostAnnotation("TestStartup:FAIL")
 		t.Fatal("timed out while waiting for all metrictank instances to come up")
 	}
