620a080f6bbadd90df14d0725e0364ac05b7b892
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-02-03 13:35:10 -0500

    remove dead code

--- cmd/fakemetrics/cmd/root.go
+++ cmd/fakemetrics/cmd/root.go
@@ -21,7 +21,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/grafana/metrictank/cmd/fakemetrics/out"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/raintank/met"
 	"github.com/raintank/worldping-api/pkg/log"
@@ -89,7 +88,6 @@ var (
 	period     int // in s
 
 	// global vars
-	outs          []out.Out
 	stats         met.Backend
 	flushDuration met.Timer
 )
