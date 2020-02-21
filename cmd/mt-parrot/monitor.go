8c917ceb83901a5ae96c9760871bd03484d37e3f
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-02-20 22:47:30 -0500

    more logging

--- cmd/mt-parrot/monitor.go
+++ cmd/mt-parrot/monitor.go
@@ -52,6 +52,7 @@ func monitor() {
 					continue
 				}
 				if diff := dp.Val - float64(dp.Ts); diff != 0 {
+					log.Debugf("partition=%d dp.Val=%f dp.Ts=%d diff=%f", partition, dp.Val, dp.Ts, diff)
 					serStats.lastSeen = dp.Ts
 					serStats.deltaSum += diff
 					serStats.numNonMatching += 1
