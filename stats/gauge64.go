b2e735e96da3265c3b8b26a919c6ce769f7035df
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-02-24 16:17:29 -0500

    Update stats/Gauge64 to use a struct like all other stats implementations.

--- stats/gauge64.go
+++ stats/gauge64.go
@@ -5,27 +5,28 @@ import (
 	"time"
 )
 
-type Gauge64 uint64
+type Gauge64 struct {
+	val uint64
+}
 
 func NewGauge64(name string) *Gauge64 {
-	u := Gauge64(0)
-	return registry.getOrAdd(name, &u).(*Gauge64)
+	return registry.getOrAdd(name, &Gauge64{}).(*Gauge64)
 }
 
 func (g *Gauge64) Inc() {
-	atomic.AddUint64((*uint64)(g), 1)
+	atomic.AddUint64(&g.val, 1)
 }
 
 func (g *Gauge64) Dec() {
-	atomic.AddUint64((*uint64)(g), ^uint64(0))
+	atomic.AddUint64(&g.val, ^uint64(0))
 }
 
 func (g *Gauge64) AddUint64(val uint64) {
-	atomic.AddUint64((*uint64)(g), val)
+	atomic.AddUint64(&g.val, val)
 }
 
 func (g *Gauge64) DecUint64(val uint64) {
-	atomic.AddUint64((*uint64)(g), ^uint64(val-1))
+	atomic.AddUint64(&g.val, ^uint64(val-1))
 }
 
 func (g *Gauge64) Add(val int) {
@@ -41,19 +42,19 @@ func (g *Gauge64) Add(val int) {
 }
 
 func (g *Gauge64) Set(val int) {
-	atomic.StoreUint64((*uint64)(g), uint64(val))
+	atomic.StoreUint64(&g.val, uint64(val))
 }
 
 func (g *Gauge64) SetUint64(val uint64) {
-	atomic.StoreUint64((*uint64)(g), val)
+	atomic.StoreUint64(&g.val, val)
 }
 
 func (g *Gauge64) ReportGraphite(prefix, buf []byte, now time.Time) []byte {
-	val := atomic.LoadUint64((*uint64)(g))
+	val := atomic.LoadUint64(&g.val)
 	buf = WriteUint64(buf, prefix, []byte("gauge64"), val, now)
 	return buf
 }
 
 func (g *Gauge64) Peek() uint64 {
-	return atomic.LoadUint64((*uint64)(g))
+	return atomic.LoadUint64(&g.val)
 }
