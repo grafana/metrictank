c19b30457a8b55859454784eec794fbc36f3deda
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-02-27 14:11:43 -0500

    bring decode error back

--- cmd/mt-parrot/monitor.go
+++ cmd/mt-parrot/monitor.go
@@ -14,6 +14,7 @@ import (
 
 var (
 	httpError    = stats.NewCounter32("parrot.monitoring.error;error=http")
+	decodeError = stats.NewCounter32("parrot.monitoring.error;error=decode")
 	invalidError = stats.NewCounter32("parrot.monitoring.error;error=invalid")
 )
 
@@ -49,7 +50,7 @@ func monitor() {
 			httpError.Inc()
 		}
 		if query.DecodeErr != nil {
-			invalidError.Inc()
+			decodeError.Inc()
 		}
 
 		for _, s := range query.Decoded {
