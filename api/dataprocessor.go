e14e6affba3b18a332d3ba1c09d633a73c6f2b85
Dieter Plaetinck <dieter@grafana.com>
2020-03-20 21:34:35 +0200

    remove todo. is fixed now

--- api/dataprocessor.go
+++ api/dataprocessor.go
@@ -239,7 +239,6 @@ func (s *Server) getTargetsRemote(ctx context.Context, ss *models.StorageStats, 
 	for _, nodeReqs := range remoteReqs {
 		shardID := nodeReqs[0].Node.GetPartitions()[0]
 		requiredPeers[shardID] = allPeers[shardID]
-		// TODO - Sometimes two peers from the same shard have different responses here...very strange
 		shardReqs[shardID] = append(shardReqs[shardID], nodeReqs...)
 	}
 
