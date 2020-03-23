3a0bbe2364d26767a5a91f497e03c500a3bcfd60
Amanda Gao <agao48@bloomberg.net>
2020-03-23 10:07:57 -0400

    Remove check for reqs.count == 0

--- api/graphite.go
+++ api/graphite.go
@@ -757,10 +757,7 @@ func (s *Server) executePlan(ctx context.Context, orgId uint32, plan expr.Plan) 
 	}
 
 	reqRenderSeriesCount.ValueUint32(reqs.cnt)
-	if reqs.cnt == 0 {
-		return nil, meta, nil
-	}
-
+	
 	meta.RenderStats.SeriesFetch = reqs.cnt
 
 	// note: if 1 series has a movingAvg that requires a long time range extension, it may push other reqs into another archive. can be optimized later
