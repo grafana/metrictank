142b3a6bf64384470767a6318f2c647e494fbcfd
Dieter Plaetinck <dieter@grafana.com>
2020-03-31 11:38:13 +0300

    remove duplicate code

--- stats/out_devnull.go
+++ stats/out_devnull.go
@@ -2,11 +2,13 @@ package stats
 
 import (
 	"time"
+
+	"github.com/grafana/metrictank/clock"
 )
 
 func NewDevnull() {
 	go func() {
-		ticker := tick(time.Second)
+		ticker := clock.AlignedTickLossy(time.Second)
 		buf := make([]byte, 0)
 		for now := range ticker {
 			for _, metric := range registry.list() {
