abaa269dfad08434aed624160b3b47f7daca9faf
Amanda Gao <agao48@bloomberg.net>
2020-04-16 17:28:02 -0400

    Implement invert

--- expr/func_invert.go
+++ expr/func_invert.go
@@ -1,7 +1,11 @@
 package expr
 
 import (
+	"fmt"
+	"math"
+
 	"github.com/grafana/metrictank/api/models"
+	"github.com/grafana/metrictank/schema"
 )
 
 type FuncInvert struct {
@@ -26,5 +30,22 @@ func (s *FuncInvert) Exec(dataMap DataMap) ([]models.Series, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	for i, serie := range series {
+		series[i].Target = fmt.Sprintf("invert(%s)", serie.Target)
+		series[i].Tags = serie.CopyTagsWith("invert", "1")
+		series[i].QueryPatt = fmt.Sprintf("invert(%s)", serie.QueryPatt)
+		series[i].Datapoints = pointSlicePool.Get().([]schema.Point)
+		for _, p := range serie.Datapoints {
+			if !math.IsNaN(p.Val) && p.Val != 0 {
+				p.Val = math.Pow(p.Val, -1)
+			} else {
+				p.Val = math.NaN()
+			}
+			series[i].Datapoints = append(series[i].Datapoints, p)
+		}
+	}
+
+	dataMap.Add(Req{}, series...)
 	return series, nil
 }
