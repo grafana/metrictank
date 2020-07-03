a3893e7777ef679089ba84ca6ab22a59b4f4144d
Dieter Plaetinck <dieter@grafana.com>
2020-07-03 12:49:19 +0300

    utility function for the pool to get appropriately sized slice

--- expr/pool.go
+++ expr/pool.go
@@ -1,6 +1,10 @@
 package expr
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/grafana/metrictank/schema"
+)
 
 var pointSlicePool *sync.Pool
 
@@ -11,4 +15,18 @@ var pointSlicePool *sync.Pool
 // the output from this package's processing is no longer needed.
 func Pool(p *sync.Pool) {
 	pointSlicePool = p
+}
+
+// pointSlicePoolGet returns a pointslice of at least minCap capacity.
+// similar code lives also in api.Fix(). at some point we should really clean up our pool code.
+func pointSlicePoolGet(minCap int) []schema.Point {
+	// 3 attempts to get a sufficiently sized slice from the pool. if it fails, allocate a new one
+	for attempt := 1; attempt < 4; attempt++ {
+		candidate := pointSlicePool.Get().([]schema.Point)
+		if cap(candidate) >= minCap {
+			return candidate
+		}
+		pointSlicePool.Put(candidate)
+	}
+	return make([]schema.Point, 0, minCap)
 }
