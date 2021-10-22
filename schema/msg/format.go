36ad4c5b15598fe350c66d1b25f16c0c436f9a1a
Sean Hanson <shanson7@bloomberg.net>
2021-10-22 11:59:17 +0100

    Add MetricPointArray format

--- schema/msg/format.go
+++ schema/msg/format.go
@@ -11,4 +11,5 @@ const (
 	FormatMetricPoint
 	FormatMetricPointWithoutOrg
 	FormatIndexControlMessage
+	FormatMetricPointArray
 )
--- schema/msg/msg_test.go
+++ schema/msg/msg_test.go
@@ -74,6 +74,105 @@ func TestWriteReadPointMsgWithoutOrg(t *testing.T) {
 	}
 }
 
+func TestWriteReadPointMsgArray(t *testing.T) {
+	mp := []schema.MetricPoint{
+		{
+			MKey: schema.MKey{
+				Org: 123,
+			},
+			Time:  math.MaxUint32,
+			Value: 123.45,
+		},
+		{
+			MKey: schema.MKey{
+				Org: 1,
+			},
+			Time:  math.MaxUint32,
+			Value: 1,
+		},
+		{
+			MKey: schema.MKey{
+				Org: 2,
+			},
+			Time:  math.MaxUint32,
+			Value: 12,
+		},
+	}
+	buf := make([]byte, 0, 2+32*3)
+	out, err := WritePointMsgArray(mp, buf, FormatMetricPoint)
+	if err != nil {
+		t.Fatalf("%s", err.Error())
+	}
+
+	_, ok := IsPointMsg(out)
+	if !ok {
+		t.Fatal("IsPointMsg: exp true, got false")
+	}
+
+	var outPoints []schema.MetricPoint
+	iter := NewMetricPointIter(out, 6)
+	for iter.Next() {
+		outPoints = append(outPoints, iter.Value())
+	}
+
+	if iter.Err() != nil {
+		t.Fatalf("%s", iter.Err().Error())
+	}
+
+	if !reflect.DeepEqual(mp, outPoints) {
+		t.Fatalf("expected point %v, got %v", mp, outPoints)
+	}
+}
+
+func TestWriteReadPointMsgArrayWithoutOrg(t *testing.T) {
+	mp := []schema.MetricPoint{
+		{
+			MKey:  schema.MKey{},
+			Time:  math.MaxUint32,
+			Value: 123.45,
+		},
+		{
+			MKey:  schema.MKey{},
+			Time:  math.MaxUint32,
+			Value: 1,
+		},
+		{
+			MKey:  schema.MKey{},
+			Time:  math.MaxUint32,
+			Value: 12,
+		},
+	}
+	buf := make([]byte, 0, 2+28*3)
+	out, err := WritePointMsgArray(mp, buf, FormatMetricPointWithoutOrg)
+	if err != nil {
+		t.Fatalf("%s", err.Error())
+	}
+
+	_, ok := IsPointMsg(out)
+	if !ok {
+		t.Fatal("IsPointMsg: exp true, got false")
+	}
+
+	// iter will have to set the default org and we want to check it
+	for i := range mp {
+		mp[i].MKey.Org = 6
+	}
+
+	var outPoints []schema.MetricPoint
+	iter := NewMetricPointIter(out, 6)
+	for iter.Next() {
+		outPoints = append(outPoints, iter.Value())
+	}
+
+	if iter.Err() != nil {
+		t.Fatalf("%s", iter.Err().Error())
+	}
+
+	if !reflect.DeepEqual(mp, outPoints) {
+		t.Fatalf("expected point %v, got %v", mp, outPoints)
+	}
+}
+
 func TestWriteReadIndexControlMsg(t *testing.T) {
 	mp := schema.ControlMsg{
 		Op:   schema.OpArchive,
